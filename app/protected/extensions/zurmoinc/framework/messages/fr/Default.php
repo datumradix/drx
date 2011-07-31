96ec7af2acda20113e18653b47f944128e4969f4
Jason <Jason@BERN>
2011-07-30 21:11:53 -0500

    french language pack

---
+++ app/protected/extensions/zurmoinc/framework/messages/fr/Default.php
@@ -0,0 +1,143 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Advanced Search'
+            => 'Recherche avancée',
+        'All'
+            => 'Tous',
+        'by'
+            => 'par',
+        'Cell Settings'
+            => 'Paramètres des cellules',
+        'Celsius'
+            => 'Celsius',
+        'Change Log'
+            => 'Log de modifications',
+        'Changed'
+            => 'Modifié',
+        'Clear Search'
+            => 'Effacer',
+        'Created Date and User'
+            => '?? Crée',
+        'Created On'
+            => 'Crée',
+        'Delete'
+            => 'Supprimer',
+        'Detail View Only'
+            => 'Vue détaillée uniquement',
+        'Details'
+            => 'Détails',
+        'Edit'
+            => 'Modifier',
+        'Edit Portlet'
+            => 'Modifier le portlet',
+        'Fahrenheit'
+            => 'Fahrenheit',
+        'Filtered List'
+            => '?? filtered list',
+        'from'
+            => 'de',
+        'Full Name'
+            => 'Nom',
+        'Go to Details'
+            => 'Au détails',
+        'Invalid'
+            => 'Invalide',
+        'Last Modified'
+            => 'Dernière modification',
+        'Last Modified Date and User'
+            => '?? Dernière modification par',
+        'List'
+            => 'Liste',
+        'Name'
+            => 'Nom',
+        'No'
+            => 'Non',
+        'None'
+            => 'Aucun',
+        'Opted Out'
+            => 'Opt out',
+        'Panel'
+            => 'Panel',
+        'Panel Settings'
+            => 'Paramètres du panel',
+        'Panel Title'
+            => 'Titre du panel',
+        'Perform Action'
+            => 'Sélectionner',
+        'record'
+            => 'donnée',
+        'records'
+            => 'données',
+        'Remove'
+            => 'Supprimer',
+        'Return to List'
+            => 'Retour à la liste',
+        'Row'
+            => '[Ajouter une ligne]',
+        'Save'
+            => 'Sauvegarder',
+        'Search'
+            => 'Rechercher',
+        'Select'
+            => 'Sélectionner',
+        'selected for updating.'
+            => 'sélectionnée(s)',
+        'Some values cannot be removed because they are currently in use. Try changing the records that use them first.'
+            => '?? some values cannot be removed...',
+        'This module doesn\'t have anything to configure.'
+            => 'Il n\'y a rien a configurer',
+        'This pick-list is used by more than one module.'
+            => 'Cette liste déroulante est utilisée dans les modules suivants:',
+        'Title/First/LastName'
+            => 'Titre/Prénom/Nom',
+        'to'
+            => 'à',
+        'Today'
+            => 'Aujourd\'hui',
+        'Tomorrow'
+            => 'Demain',
+        'Unknown'
+            => 'Inconnu',
+        'Update Selected'
+            => 'Actualiser les données',
+        'Yes'
+            => 'Oui',
+        'Yesterday'
+            => 'Hier',
+        'You must enter a value'
+            => 'Ce champ est obligatoire',
+        'You must specify a value.'
+            => '?? you must specify a value',
+        '{attribute} is too precise (maximum decimal places is {precision}).'
+            => '?? attribute too precise',
+    );
+?>
---
+++ app/protected/modules/zurmo/messages/fr/Default.php
@@ -0,0 +1,229 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(Unknown)'
+            => '(Inconnu)',
+        '(Unnamed)'
+            => '(Anonyme)',
+        'About'
+            => 'À propos de zurmo',
+        'Access Failure'
+            => 'Échec de l\'accès',
+        'Admin'
+            => 'Configuration',
+        'Alert'
+            => 'Alerte',
+        'Allow'
+            => 'Autorisé',
+        'An error has occurred. Please click'
+            => 'Il y a une erreur. Veuillez cliquer.',
+        'Any City'
+            => 'Ville',
+        'Any Country'
+            => 'Pays',
+        'Any Email Address'
+            => 'Email',
+        'Any Invalid Email'
+            => 'Email invalide',
+        'Any Opted Out Email'
+            => 'Opt-out',
+        'Any Postal Code'
+            => 'Code postale',
+        'Any State'
+            => 'État/Dépt',
+        'Any Street'
+            => 'Rue',
+        'are required.'
+            => 'sont obligatoires',
+        'Are you sure you want to remove this group?'
+            => 'Êtes-vous sûr(e) de vouloir supprimer ce groupe?',
+        'Are you sure you want to remove this role?'
+            => 'Êtes-vous sûr(e) de vouloir supprimer ce rôle?',
+        'Cancel'
+            => 'Annuler',
+        'Configuration'
+            => 'Configuration',
+        'Convert'
+            => 'Convertir',
+        'Copyright &#169; Zurmo Inc., 2011. All Rights reserved.'
+            => 'Copyright &#169; Zurmo Inc., 2011. Tous droits réservés.',
+        'Create Group'
+            => 'Nouveau Groupe',
+        'Create New'
+            => 'Crée',
+        'Currency Configuration'
+            => 'Devises',
+        'days'
+            => 'jours',
+        'Delete Group'
+            => 'Supprimer le Groupe',
+        'Delete Role'
+            => 'Supprimer le Rôle',
+        'Deny'
+            => 'Interdit',
+        'every'
+            => 'chaque',
+        'Everyone'
+            => 'Tous',
+        'Fields with'
+            => 'Champs avec',
+        'Global Configuration'
+            => 'Configuration globale',
+        'Go to Search'
+            => 'à la Recherche',
+        'Group'
+            => 'Groupe',
+        'Group Module Permissions'
+            => 'Permissions',
+        'Group Policies'
+            => 'Règles de sécurit',
+        'Group Rights'
+            => 'Droits de sécurité',
+        'Group User Membership'
+            => 'Membres du groupe',
+        'Groups'
+            => 'Groupes',
+        'here'
+            => 'ici',
+        'Home'
+            => 'Accueil',
+        'Incorrect username or password.'
+            => 'Le nom d\'utilisateur ou le mot de passe est incorrect.',
+        'Inherited - All'
+            => '??inherited-tous',
+        'Inherited - Allow'
+            => '??inherited-allow',
+        'Label must be all lowercase.'
+            => 'Veuillez utiliser des lettres miniscules',
+        'Label must not be empty.'
+            => 'L\'étiquette est obligatoire',
+        'Label must not contain any special characters.'
+            => 'L\'étiquette ne peut pas contenir de caractères spéciaux',
+        'Language'
+            => 'Langue',
+        'List page size'
+            => 'Dimension de la liste',
+        'Load'
+            => '??',
+        'Login'
+            => 'Connexion',
+        'Logout'
+            => 'Déconnexion',
+        'Manage Currency Configuration'
+            => 'Gestion des devises',
+        'Manage Global Configuration'
+            => 'Gestion de la configuration globale',
+        'Manage Groups'
+            => 'Gestion des groupes',
+        'Manage Roles'
+            => 'Gestion des rôles',
+        'Module Name - Plural'
+            => 'Nom du module - au pluriel',
+        'Module Name - Plural (lowercase)'
+            => 'Nom du module - au pluriel (miniscule)',
+        'Module Name - Singular'
+            => 'Nom du module - singluier',
+        'Module Name - Singular (lowercase)'
+            => 'Nom du module - au singulier (miniscule)',
+        'Module Permissions'
+            => 'Permissions',
+        'Not Set'
+            => 'Par défaut',
+        'Not Set - Ad Hoc / Owner'
+            => '?? not set adhoc',
+        'Office Fax'
+            => 'Fax de Bureau',
+        'Office Phone'
+            => 'Téléphone de Bureau',
+        'Oh no!'
+            => 'Zut!',
+        'Owner Only'
+            => 'Propriétaire',
+        'Parent Group'
+            => 'Groupe mère',
+        'Password'
+            => 'Mot de passe',
+        'Please use FireFox, Chrome, or Internet Explorer.'
+            => 'Veuillez utiliser FireFox, Chrome, ou Internet Explorer.',
+        'Policies'
+            => 'Règles',
+        'Popup list page size'
+            => 'Dimensions de la liste pop-up',
+        'Remember me next time'
+            => 'Retenir le nom d\'utilisateur',
+        'Return to Admin Menu'
+            => 'Retour au menu administrateur',
+        'Rights'
+            => 'Droits',
+        'Role'
+            => 'Rôle',
+        'Roles'
+            => 'Rôles',
+        'Search'
+            => 'Rechercher',
+        'Security'
+            => 'Sécurité',
+        'Select a Parent Group'
+            => '?? un groupe',
+        'Select a Parent Role'
+            => '?? un rôle',
+        'Select a Role'
+            => 'Selectionner un rôle',
+        'Sorry! Your browser is not supported.'
+            => 'Désolé! Votre navigateur n\'est pas supporté',
+        'Sublist page size'
+            => 'Dimensions de la sous-liste',
+        'Successfully updated'
+            => 'Mise à jour réussie',
+        'Super Administrators'
+            => 'Super Administrateur',
+        'This name is reserved. Please pick a different name.'
+            => '?? error:name reserved, pick a different one',
+        'Time zone'
+            => 'Fuseau horaire',
+        'to continue to the home page. If the error persists please do something or other.'
+            => 'pour continuer à l\'accueil. ??' ,
+        'User Membership'
+            => 'Membres du groupe',
+        'Username'
+            => 'Nom d\'utilisateur',
+        'Welcome'
+            => 'Bienvenue',
+        'Your Profile'
+            => 'Votre Profil',
+        'Your RedBean version is currentVersion and it should be acceptableVersion.'
+            => 'Votre version de RedBean est currentVersion et doit être acceptableVersion.',
+        'Your Yii version is currentVersion and it should be acceptableVersion.'
+            => 'Votre version de Yii est currentVersion et doit être acceptableVersion.',
+        'ZurmoCRM'
+            => 'ZurmoCRM',
+    );
+?>
