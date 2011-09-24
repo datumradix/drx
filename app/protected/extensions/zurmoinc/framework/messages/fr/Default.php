b87461694431e06d5db0f88f99efb52c8a3b370d
zurmo <zurmo@localhost>
2011-09-24 00:30:09 -0400

    License update

---
+++ app/protected/extensions/zurmoinc/framework/messages/fr/Default.php
@@ -0,0 +1,193 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(None)'
+            => '--Aucun--',
+        'Advanced Search'
+            => 'Recherche avancée',
+        'All'
+            => 'Tous',
+        'by'
+            => 'par',
+        'Cancel'
+            => 'Annuler',
+        'Cell Settings'
+            => 'Paramètres des cellules',
+        'Celsius'
+            => 'Celsius',
+        'Change Log'
+            => 'Log de modifications',
+        'Changed'
+            => 'Modifié',
+        'Clear Search'
+            => 'Effacer',
+        'Created By User'
+            => 'Crée par',
+        'Created Date and User'
+            => 'Date Crée',
+        'Created Date Time'
+            => 'Date Crée',
+        'Created On'
+            => 'Crée',
+        'Date'
+            => 'Date',
+        'Date Time'
+            => 'Date/Heure',
+        'Delete'
+            => 'Supprimer',
+        'Detail View Only'
+            => 'Vue détaillée uniquement',
+        'Details'
+            => 'Détails',
+        'Edit'
+            => 'Modifier',
+        'Edit Portlet'
+            => 'Modifier le portlet',
+        'Error'
+            => 'Erreur',
+        'Fahrenheit'
+            => 'Fahrenheit',
+        'Filtered List'
+            => 'Liste Filtrée',
+        'firstDateTime must occur after  secondDateTime'
+            => 'firstDateTime doit être après secondDateTime',
+        'firstDateTime must occur before secondDateTime'
+            => 'firstDateTime doit être avant secondDateTime',
+        'Float'
+            => 'Float',
+        'from'
+            => 'de',
+        'Full Name'
+            => 'Nom',
+        'Go to Details'
+            => 'Au détails',
+        'Inline Edit View'
+            => 'Formulaire de saisie Inline',
+        'Integer'
+            => 'Nombre entier',
+        'Invalid'
+            => 'Invalide',
+        'Last Modified'
+            => 'Dernière modification',
+        'Last Modified Date and User'
+            => 'Dernière modification',
+        'List'
+            => 'Liste',
+        'Model Class Name'
+            => 'Nom de la classe du modèle',
+        'Model Id'
+            => 'ID du modèle',
+        'Modified By User'
+            => 'Dernière modification par',
+        'Modified Date Time'
+            => 'Dernière modification',
+        'Module Name'
+            => 'Nom du Module',
+        'More Details'
+            => 'Plus de détails',
+        'More Options'
+            => 'Plus d\'options',
+        'Name'
+            => 'Nom',
+        'No'
+            => 'Non',
+        'None'
+            => 'Aucun',
+        'Now'
+            => 'Maintenant',
+        'Opted Out'
+            => 'Opt out',
+        'Panel'
+            => 'Panel',
+        'Panel Settings'
+            => 'Paramètres du panel',
+        'Panel Title'
+            => 'Titre du panel',
+        'Perform Action'
+            => 'Sélectionner',
+        'record'
+            => 'donnée',
+        'records'
+            => 'données',
+        'Remove'
+            => 'Supprimer',
+        'Return to List'
+            => 'Retour à la liste',
+        'Row'
+            => '[Ajouter une ligne]',
+        'Save'
+            => 'Sauvegarder',
+        'Search'
+            => 'Rechercher',
+        'Select'
+            => 'Sélectionner',
+        'selected for updating.'
+            => 'sélectionnée(s)',
+        'Serialized Data'
+            => 'Serialized Data',
+        'Some values cannot be removed because they are currently in use. Try changing the records that use them first.'
+            => 'Certain éléments ne peuvent être supprimés car ils sont actuellement utilisés. Veuillez modifier la valeur des champs contenant ces éléments avant de supprimer.',
+        'Tab'
+            => 'Onglet',
+        'This module doesn\'t have anything to configure.'
+            => 'Il n\'y a rien a configurer',
+        'This pick-list is used by more than one module.'
+            => 'Cette liste déroulante est utilisée dans les modules suivants:',
+        'Time'
+            => 'Heure',
+        'Title/First/LastName'
+            => 'Titre/Prénom/Nom',
+        'to'
+            => 'à',
+        'Today'
+            => 'Aujourd\'hui',
+        'Tomorrow'
+            => 'Demain',
+        'Unknown'
+            => 'Inconnu',
+        'Update Selected'
+            => 'Actualiser les données',
+        'User'
+            => 'Utilisateur',
+        'Users'
+            => 'Utilisateurs',
+        'Yes'
+            => 'Oui',
+        'Yesterday'
+            => 'Hier',
+        'You must enter a value'
+            => 'Ce champ est obligatoire',
+        'You must specify a value.'
+            => 'Vous devez spécifier une valeur',
+        '{attribute} is too precise (maximum decimal places is {precision}).'
+            => '{attribute} est trop précis(e) (la précision maximale est {precision})',
+    );
+?>
