c24d37ff7fbca4f1b1a39b8060f581121ed01de1
zurmo <zurmo@localhost>
2011-09-30 07:41:24 -0700

    Spanish version

---
+++ app/protected/extensions/zurmoinc/framework/messages/es/Default.php
@@ -0,0 +1,193 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(None)'
+            => '??',
+        'Advanced Search'
+            => 'Búsqueda avanzada',
+        'All'
+            => 'Todo',
+        'by'
+            => 'por',
+        'Cancel'
+            => 'Cancelar',
+        'Cell Settings'
+            => 'Configuración de las células',
+        'Celsius'
+            => 'Centígrado',
+        'Change Log'
+            => 'Registro de cambios',
+        'Changed'
+            => 'Cambiado',
+        'Clear Search'
+            => 'Restablecer búsqueda',
+        'Created By User'
+            => 'Creado por',
+        'Created Date and User'
+            => 'Fecha de creación y de usuario',
+        'Created Date Time'
+            => 'Fecha y hora de creación',
+        'Created On'
+            => 'Creado el',
+        'Date'
+            => 'Fecha',
+        'Date Time'
+            => 'Fecha',
+        'Delete'
+            => 'Eliminar',
+        'Detail View Only'
+            => 'Solo vista de detalles',
+        'Details'
+            => 'Detalles',
+        'Edit'
+            => 'Editar',
+        'Edit Portlet'
+            => 'Modificar portlet',
+        'Error'
+            => 'Error',
+        'Fahrenheit'
+            => 'Fahrenheit',
+        'Filtered List'
+            => 'Lista filtrada',
+        'firstDateTime must occur after secondDateTime'
+            => 'firstDateTime debe ocurrir después secondDateTime',
+        'firstDateTime must occur before secondDateTime'
+            => 'firstDateTime debe ocurrir antes secondDateTime',
+        'Float'
+            => 'Float',
+        'from'
+            => 'de',
+        'Full Name'
+            => 'Nombre completo',
+        'Go to Details'
+            => 'Ir a detalles',
+        'Inline Edit View'
+            => '??',
+        'Integer'
+            => 'Número entero',
+        'Invalid'
+            => 'Inválido',
+        'Last Modified'
+            => 'Última modificación',
+        'Last Modified Date and User'
+            => 'Última modificación fecha y usuario',
+        'List'
+            => 'Lista',
+        'Model Class Name'
+            => 'Nombre de la clase de modelo',
+        'Model Id'
+            => 'ID de modelo',
+        'Modified By User'
+            => 'Modificado por',
+        'Modified Date Time'
+            => 'Modificado fecha y hora',
+        'Module Name'
+            => 'Nombre de Module',
+        'More Details'
+            => 'Más detalles',
+        'More Options'
+            => 'Más opciones',
+        'Name'
+            => 'Nombre',
+        'No'
+            => 'No',
+        'None'
+            => 'Ninguno',
+        'Now'
+            => 'Ahora',
+        'Opted Out'
+            => 'Excluido',
+        'Panel'
+            => 'Panel',
+        'Panel Settings'
+            => 'Configuración de panel',
+        'Panel Title'
+            => 'Título de panel',
+        'Perform Action'
+            => 'realizar accíon',
+        'record'
+            => 'registro',
+        'records'
+            => 'registros',
+        'Remove'
+            => 'Eliminar',
+        'Return to List'
+            => 'Volver a la lista',
+        'Row'
+            => 'Fila',
+        'Save'
+            => 'Guardar',
+        'Search'
+            => 'Buscar',
+        'Select'
+            => 'Seleccionar',
+        'selected for updating.'
+            => 'Seleccionado por actualización',
+        'Serialized Data'
+            => 'Datos serializados',
+        'Some values cannot be removed because they are currently in use. Try changing the records that use them first.'
+            => 'Algunos valores no se pueden eliminar, ya que están actualmente en uso. Trate de cambiar los registros que usó primero',
+        'Tab'
+            => 'Tab',
+        'This module doesn\'t have anything to configure.'
+            => 'Este module no tiene nada para configurar.',
+        'This pick-list is used by more than one module.'
+            => 'Esta lista de selección es utilizada por más de un module',
+        'Time'
+            => 'Tiempo',
+        'Title/First/LastName'
+            => 'Título/Nombre/Appellido',
+        'to'
+            => 'a',
+        'Today'
+            => 'Hoy',
+        'Tomorrow'
+            => 'Mañana',
+        'Unknown'
+            => 'Desconocido',
+        'Update Selected'
+            => 'Actualización Seleccionada',
+        'User'
+            => 'Usuario',
+        'Users'
+            => 'Usuarios',
+        'Yes'
+            => 'Si',
+        'Yesterday'
+            => 'Ayer',
+        'You must enter a value'
+            => 'Debe introducir un valor',
+        'You must specify a value.'
+            => 'Debe especificar un valor',
+        '{attribute} is too precise (maximum decimal places is {precision}).'
+            => '{attribute} es demasiado preciso (máximo de decimales es {precision}).',
+    );
+?>
