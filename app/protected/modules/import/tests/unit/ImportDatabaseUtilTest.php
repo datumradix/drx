e6ce7108ab80c354b1507a5d15b4970b73a15069
ivica <ivica.nedeljkovic@gmail.com>
2011-09-30 09:01:22 +0200

    Set environment for functional tests, fixing some functional test cases

---
+++ app/protected/modules/import/tests/unit/ImportDatabaseUtilTest.php
@@ -0,0 +1,256 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    class ImportDatabaseUtilTest extends ImportBaseTest
+    {
+        public static function setUpBeforeClass()
+        {
+            parent::setUpBeforeClass();
+            SecurityTestHelper::createSuperAdmin();
+        }
+
+        public function testMakeDatabaseTableByFileHandleAndTableName()
+        {
+            $testTableName = 'testimporttable';
+            $this->assertTrue(ImportTestHelper::createTempTableByFileNameAndTableName('importTest.csv', $testTableName));
+            $sql = 'select * from ' . $testTableName;
+            $tempTableData = R::getAll($sql);
+            $compareData   = array(
+                array
+                (
+                    'id' => 1,
+                    'column_0'           => 'name',
+                    'column_1'           => 'phone',
+                    'column_2'           => 'industry',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 2,
+                    'column_0'           => 'abc',
+                    'column_1'           => '123',
+                    'column_2'           => 'a',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 3,
+                    'column_0'           => 'def',
+                    'column_1'           => '563',
+                    'column_2'           => 'b',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 4,
+                    'column_0'           => 'efg',
+                    'column_1'           => '456',
+                    'column_2'           => 'a',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 5,
+                    'column_0'           => 'we1s',
+                    'column_1'           => null,
+                    'column_2'           => 'b',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+            );
+            $this->assertEquals($compareData, $tempTableData);
+
+            //Now test that using a different file on an existing temporary table will delete the temporary table first
+            $this->assertTrue(ImportTestHelper::createTempTableByFileNameAndTableName('importTest2.csv', $testTableName));
+            $sql = 'select * from ' . $testTableName;
+            $tempTableData = R::getAll($sql);
+            $compareData   = array(
+                array
+                (
+                    'id'                 => 1,
+                    'column_0'           => 'def',
+                    'column_1'           => '563',
+                    'column_2'           => 'b',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 2,
+                    'column_0'           => 'efg',
+                    'column_1'           => '456',
+                    'column_2'           => 'a',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+            );
+            $this->assertEquals($compareData, $tempTableData);
+
+            //Now test using a file with a different enclosure/delimiter schema.
+
+            $testTableName = 'testimporttable';
+            $this->assertTrue(ImportTestHelper::
+                              createTempTableByFileNameAndTableName('importWithDifferentEnclosureAndDelimiterTest.csv',
+                                                                    $testTableName, null, "#", '"'));
+            $sql = 'select * from ' . $testTableName;
+            $tempTableData = R::getAll($sql);
+            $compareData   = array(
+                array
+                (
+                    'id' => 1,
+                    'column_0'           => 'name',
+                    'column_1'           => 'phone',
+                    'column_2'           => 'industry',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 2,
+                    'column_0'           => 'some',
+                    'column_1'           => 'thing',
+                    'column_2'           => 'else',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 3,
+                    'column_0'           => 'some2',
+                    'column_1'           => 'thing2',
+                    'column_2'           => 'else2',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+                array
+                (
+                    'id' => 4,
+                    'column_0'           => 'some3',
+                    'column_1'           => 'thing3',
+                    'column_2'           => 'else3',
+                    'status'             => null,
+                    'serializedmessages' => null,
+                ),
+            );
+            $this->assertEquals($compareData, $tempTableData);
+        }
+
+        /**
+         * @depends testMakeDatabaseTableByFileHandleAndTableName
+         */
+        public function testGetColumnCountByTableName()
+        {
+            $this->assertEquals(5, ImportDatabaseUtil::getColumnCountByTableName('testimporttable'));
+        }
+
+        /**
+         * @depends testGetColumnCountByTableName
+         */
+        public function testGetFirstRowByTableName()
+        {
+            $firstRowData = ImportDatabaseUtil::getFirstRowByTableName('testimporttable');
+            $compareData   = array(
+                    'id' => 1,
+                    'column_0'           => 'name',
+                    'column_1'           => 'phone',
+                    'column_2'           => 'industry',
+                    'status'             => null,
+                    'serializedmessages' => null,
+            );
+            $this->assertEquals($compareData, $firstRowData);
+        }
+
+        /**
+         * @depends testGetFirstRowByTableName
+         */
+        public function testGetSubset()
+        {
+            $firstBean = ImportDatabaseUtil::getSubset('testimporttable', null, 1, 1);
+            $firstBean = current($firstBean);
+            $this->assertTrue($firstBean instanceof RedBean_OODBBean);
+            $this->assertEquals(2, $firstBean->id);
+            $this->assertEquals('some', $firstBean->column_0);
+            $this->assertEquals('thing', $firstBean->column_1);
+            $this->assertEquals('else', $firstBean->column_2);
+        }
+
+        /**
+         * @expectedException RedBean_Exception_SQL
+         */
+        public function testDropTableByTableName()
+        {
+            $testTableName = 'testimporttable';
+            $sql           = 'select * from ' . $testTableName;
+            $tempTableData = R::getAll($sql);
+            $this->assertEquals(4, count($tempTableData));
+            if (RedBeanDatabase::isFrozen())
+            {
+                ImportDatabaseUtil::dropTableByTableName($testTableName);
+                $sql = 'select * from ' . $testTableName;
+                R::getAll($sql);
+            }
+            else
+            {
+                //Unfrozen will not throw an exception in this type of situation.
+                throw new RedBean_Exception_SQL();
+            }
+        }
+
+        /**
+         * @depends testDropTableByTableName
+         */
+        public function testGetCount()
+        {
+            $testTableName = 'testimporttable';
+            $this->assertTrue(ImportTestHelper::createTempTableByFileNameAndTableName('importTest.csv', $testTableName));
+            $count = ImportDatabaseUtil::getCount($testTableName);
+            $this->assertEquals(5, $count);
+            $count = ImportDatabaseUtil::getCount($testTableName, 'column_1 = "456"');
+            $this->assertEquals(1, $count);
+        }
+
+        /**
+         * @depends testGetCount
+         */
+        public function testUpdateRowAfterProcessing()
+        {
+            ImportDatabaseUtil::updateRowAfterProcessing('testimporttable', 2, 4, serialize(array('a' => 'b')));
+            $bean = R::findOne('testimporttable', "id = :id", array('id' => 2));
+            $this->assertEquals(4, $bean->status);
+            $this->assertEquals(serialize(array('a' => 'b')), $bean->serializedmessages);
+            $bean = R::findOne('testimporttable', "id = :id", array('id' => 1));
+            $this->assertEquals(null, $bean->status);
+            $this->assertEquals(null, $bean->serializedmessages);
+            $bean = R::findOne('testimporttable', "id = :id", array('id' => 3));
+            $this->assertEquals(null, $bean->status);
+            $this->assertEquals(null, $bean->serializedmessages);
+        }
+    }
+?>
---
+++ app/protected/modules/zurmo/utils/FileModelDisplayUtil.php
@@ -0,0 +1,75 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * Helper class to help with user interface manipulation for FileModel related actions.
+     */
+    class FileModelDisplayUtil
+    {
+        /**
+         * Given a file size in bytes, convert to a human readable form.
+         * @param integer $size
+         * @return string $content
+         */
+        public static function convertSizeToHumanReadableAndGet($size)
+        {
+            assert('is_numeric($size)');
+            if ($size == 0)
+            {
+                return '0';
+            }
+            if ($size < 1048576)
+            {
+                return round($size / 1024, 2) . 'Kb';
+            }
+            elseif ($size < 1073741824)
+            {
+                return round($size / 1048576, 2) . 'Mb';
+            }
+            else
+            {
+                return round($size / 1073741824, 2) . 'Gb';
+            }
+        }
+
+        public static function renderDownloadLinkContentByRelationModelAndFileModel($model, $fileModel)
+        {
+            assert('$model instanceof RedBeanModel');
+            assert('$fileModel instanceof FileModel');
+            $content = null;
+            $content .= '<span class="ui-icon ui-icon-document" style="display:inline-block;">';
+            $content .= Yii::t('Default', 'Attachment') . '</span>';
+            $content .= CHtml::link(
+                    Yii::app()->format->text($fileModel->name),
+                    Yii::app()->createUrl('zurmo/fileModel/download/',
+                        array('modelId' => $model->id,
+                              'modelClassName' => get_class($model),
+                              'id' => $fileModel->id))
+            );
+            return $content;
+        }
+    }
+?>
