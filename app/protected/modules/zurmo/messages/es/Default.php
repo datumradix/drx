d0a37ed2712a6b175d60c3d5e5a4c5c697042b63
Jason <Jason@BERN>
2011-10-03 22:49:42 -0500

    French language pack work.

---
+++ app/protected/modules/zurmo/messages/es/Default.php
@@ -0,0 +1,345 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(Unknown)'
+            => '(Desconocido)',
+        '(Unnamed)'
+            => '(Sin Nombre)',
+        '2D Donut Chart'
+            => '??',
+        '2D Horizontal Bar Graph'
+            => '??',
+        '2D Pie Chart'
+            => '??',
+        '2D Vertical Bar Graph'
+            => '??',
+        '3D Pie Chart'
+            => '??',
+        '3D Vertical Bar Graph'
+            => '??',
+        'About'
+            => 'Sobre',
+        'Access Failure'
+            => '??',
+        'Admin'
+            => 'Admin',
+        'Alert'
+            => '??',
+        'Allow'
+            => 'Permitir',
+        'Amount'
+            => '??',
+        'An error has occurred. Please click'
+            => 'Ha ocurrido un error. Por favor, haga clic',
+        'Any City'
+            => '??',
+        'Any Country'
+            => '??',
+        'Any Email Address'
+            => '??',
+        'Any Invalid Email'
+            => '??',
+        'Any Opted Out Email'
+            => '??',
+        'Any Postal Code'
+            => '??',
+        'Any State'
+            => '??',
+        'Any Street'
+            => '??',
+        'are required.'
+            => 'son obligatorios.',
+        'Are you sure you want to remove this group?'
+            => '??',
+        'Are you sure you want to remove this role?'
+            => '??',
+        'Attachment'
+            => '??',
+        'Attachments'
+            => '??',
+        'Audit Event'
+            => '??',
+        'Audit Trail'
+            => '??',
+        'Cancel'
+            => '??',
+        'Chart Type'
+            => '??',
+        'City'
+            => '??',
+        'Code'
+            => '??',
+        'Configuration'
+            => '??',
+        'Convert'
+            => '??',
+        'Copyright &#169; Zurmo Inc., 2011. All Rights reserved.'
+            => 'Copyright &#169; Zurmo Inc. de 2011. Todos los derechos reservados.',
+        'Country'
+            => '??',
+        'Create Group'
+            => '??',
+        'Create New'
+            => '??',
+        'Currencies: Create'
+            => '??',
+        'Currencies: List'
+            => '??',
+        'Currency'
+            => '??',
+        'Currency Configuration'
+            => '??',
+        'Data'
+            => '??',
+        'days'
+            => '??',
+        'Delete Group'
+            => '??',
+        'Delete Role'
+            => '??',
+        'Deny'
+            => 'Denegar',
+        'Department'
+            => '??',
+        'Description'
+            => '??',
+        'Email Address'
+            => '??',
+        'every'
+            => '??',
+        'Everyone'
+            => 'Todo el Mundo',
+        'Fields with'
+            => 'Los campos con',
+        'File Content'
+            => '??',
+        'Files'
+            => '??',
+        'Filter By Created User'
+            => '??',
+        'Filtered Lists'
+            => '??',
+        'First Name'
+            => '??',
+        'Global Configuration'
+            => '??',
+        'Go to Search'
+            => '??',
+        'Group'
+            => '??',
+        'Group Module Permissions'
+            => '??',
+        'Group Policies'
+            => '??',
+        'Group Rights'
+            => '??',
+        'Groups'
+            => '??',
+        'here'
+            => 'aquí',
+        'Home'
+            => '??',
+        'Incorrect username or password.'
+            => '??',
+        'Industry'
+            => '??',
+        'Inherited - All'
+            => '??',
+        'Inherited - Allow'
+            => '??',
+        'Invalid currency code.'
+            => '??',
+        'Is Invalid'
+            => '??',
+        'Job Title'
+            => '??',
+        'Label must be all lowercase.'
+            => '??',
+        'Label must not be empty.'
+            => '??',
+        'Label must not contain any special characters.'
+            => '??',
+        'Language'
+            => '??',
+        'Last Name'
+            => '??',
+        'List page size'
+            => '??',
+        'Load'
+            => '??',
+        'Login'
+            => 'Acceder',
+        'Logout'
+            => 'Salir',
+        'Manage Currency Configuration'
+            => '??',
+        'Manage Global Configuration'
+            => '??',
+        'Manage Groups'
+            => '??',
+        'Manage Roles'
+            => '??',
+        'Member'
+            => '??',
+        'Member Of'
+            => '??',
+        'Members'
+            => '??',
+        'Mobile Phone'
+            => '??',
+        'Module Name - Plural'
+            => '??',
+        'Module Name - Plural (lowercase)'
+            => '??',
+        'Module Name - Singular'
+            => '??',
+        'Module Name - Singular (lowercase)'
+            => '??',
+        'Module Permissions'
+            => '??',
+        'Munge rebuild complete.'
+            => '??',
+        'Not Set'
+            => '??',
+        'Not Set - Ad-hoc / Owner'
+            => '??',
+        'Office Fax'
+            => 'Fax de la Oficina',
+        'Office Phone'
+            => 'Teléfono de la Oficina',
+        'Oh no!'
+            => 'Oh no!',
+        'Opt Out'
+            => '??',
+        'Owner'
+            => '??',
+        'Owner Only'
+            => '??',
+        'Parent Group'
+            => '??',
+        'Password'
+            => 'Contraseña',
+        'Permissions'
+            => '??',
+        'Permitable'
+            => '??',
+        'Please use FireFox, Chrome, or Internet Explorer.'
+            => 'Por favor, use Firefox, Chrome, o Internet Explorer.',
+        'Policies'
+            => '??',
+        'Popup list page size'
+            => '??',
+        'Postal Code'
+            => '??',
+        'Primary Address'
+            => '??',
+        'Primary Email'
+            => '??',
+        'Rate to'
+            => '??',
+        'Rate To Base'
+            => '??',
+        'Remember me next time'
+            => '??',
+        'Return to Admin Menu'
+            => '??',
+        'Rights'
+            => '??',
+        'Role'
+            => '??',
+        'Roles'
+            => '??',
+        'Search'
+            => 'Buscar',
+        'Secondary Address'
+            => '??',
+        'Secondary Email'
+            => '??',
+        'Securable Item'
+            => '??',
+        'Security'
+            => '??',
+        'Select a Parent Group'
+            => '??',
+        'Select a Parent Role'
+            => '??',
+        'Select a Role'
+            => '??',
+        'Shipping Address'
+            => '??',
+        'Size'
+            => '??',
+        'Sorry! Your browser is not supported.'
+            => 'Lo sentimos! Su navegador no es compatible.',
+        'Source'
+            => '??',
+        'Stage'
+            => '??',
+        'State'
+            => '??',
+        'Street 1'
+            => '??',
+        'Street 2'
+            => '??',
+        'Sublist page size'
+            => '??',
+        'Successfully updated'
+            => '??',
+        'Super Administrators'
+            => 'Superadministradores',
+        'The currency code is invalid'
+            => '??',
+        'This name is reserved. Please pick a different name.'
+            => '??',
+        'Time zone'
+            => '??',
+        'Title'
+            => '??',
+        'to continue to the home page. If the error persists please contact your administrator.'
+            => 'para continuar a la página de inicio. Si el error persiste, por favor haga una cosa u otra.',
+        'User Membership'
+            => '??',
+        'Username'
+            => 'Usario',
+        'Website'
+            => '??',
+        'Welcome'
+            => 'Bienvenidos',
+        'Your Profile'
+            => 'Su Perfil',
+        'Your RedBean version is currentVersion and it should be acceptableVersion.'
+            => '??',
+        'Your Yii version is currentVersion and it should be acceptableVersion.'
+            => '??',
+        'ZurmoCRM'
+            => 'ZurmoCRM',
+    );
+?>
