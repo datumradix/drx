bc04b0cc5b7867b8c63ca8b6a9af7cc17ee9fe9a
digimagic <digimagic@localhost>
2011-10-12 21:23:52 -0500

    Fixed misspelling.

---
+++ app/protected/modules/zurmo/messages/fr/Default.php
@@ -0,0 +1,409 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(Unknown)'
+            => '(Inconnu)',
+        '(Unnamed)'
+            => '(Anonyme)',
+        '2D Donut Chart'
+            => 'Anneau',
+        '2D Horizontal Bar Graph'
+            => 'Barres',
+        '2D Pie Chart'
+            => 'Secteur 2D',
+        '2D Vertical Bar Graph'
+            => 'Histogramme',
+        '3D Pie Chart'
+            => 'Secteur 3D',
+        '3D Vertical Bar Graph'
+            => 'Histogramme 3D',
+        'About'
+            => 'À propos de zurmo',
+        'Access Failure'
+            => 'Échec de l\'accès',
+        'Active'
+            => 'Active',
+        'Active currencies can be used when creating new records and as a default currency for a user.'
+            => 'Les devises actives sont disponibles pour créer de nouveaux enregistrements et comme devise de défault pour un utilisateur.',
+        'Active languages can be used by users. The system language and any language in use by a user cannot be inactivated'
+            => 'Les langues actives sont disponibles aux utilisateurs. Impossible de désactiver la langue système ou unelangue actuellement utilisé par un utilisateur',
+        'Add File'
+            => 'Ajouter un Fichier',
+        'Add Files'
+            => 'Ajouter des Fichiers',
+        'Admin'
+            => 'Configuration',
+        'Alert'
+            => 'Alerte',
+        'Allow'
+            => 'Autorisé',
+        'Amount'
+            => 'Montant',
+        'An error has occurred. Please click'
+            => 'Il y a une erreur. Veuillez cliquer.',
+        'Any City'
+            => 'Ville',
+        'Any Country'
+            => 'Pays',
+        'Any Email Address'
+            => 'Email',
+        'Any Invalid Email'
+            => 'Email invalide',
+        'Any Opted Out Email'
+            => 'Opt-out',
+        'Any Postal Code'
+            => 'Code postale',
+        'Any State'
+            => 'État/Dépt',
+        'Any Street'
+            => 'Rue',
+        'are required.'
+            => 'sont obligatoires',
+        'Are you sure you want to remove this group?'
+            => 'Êtes-vous sûr(e) de vouloir supprimer ce groupe?',
+        'Are you sure you want to remove this role?'
+            => 'Êtes-vous sûr(e) de vouloir supprimer ce rôle?',
+        'Attachment'
+            => 'Fichier',
+        'Attachments'
+            => 'Fichiers',
+        'Audit Event'
+            => 'Liste de modifications',
+        'Audit Trail'
+            => 'Log de modifications',
+        'Cancel'
+            => 'Annuler',
+        'Changes to active currencies changed successfully.'
+            => 'Les changements de devises actives sont effectués',
+        'Changes to active languages changed successfully.'
+            => 'Les changements de langue actives sont effectués',
+        'Chart Type'
+            => 'Type de graphique',
+        'City'
+            => 'Ville',
+        'Code'
+            => 'Code',
+        'Configuration'
+            => 'Configuration',
+        'Convert'
+            => 'Convertir',
+        'Copyright &#169; Zurmo Inc., 2011. All Rights reserved.'
+            => 'Copyright &#169; Zurmo Inc., 2011. Tous droits réservés.',
+        'Country'
+            => 'Pays',
+        'Create Group'
+            => 'Nouveau Groupe',
+        'Create New'
+            => 'Crée',
+        'Currencies: Create'
+            => 'Devises: Créer',
+        'Currencies: List'
+            => 'Devises: Liste',
+        'Currency'
+            => 'Devise',
+        'Currency Configuration'
+            => 'Devises',
+        'Currency in use.'
+            => 'Devise utiliséé',
+        'Currency Value'
+            => 'Valeur',
+        'Data'
+            => 'Donées',
+        'days'
+            => 'jours',
+        'Delete Group'
+            => 'Supprimer le Groupe',
+        'Delete Role'
+            => 'Supprimer le Rôle',
+        'Deny'
+            => 'Interdit',
+        'Department'
+            => 'Département',
+        'Description'
+            => 'Description',
+        'Email Address'
+            => 'Addresse e-mail',
+        'every'
+            => 'chaque',
+        'Everyone'
+            => 'Tous',
+        'Fields with'
+            => 'Champs avec',
+        'File Content'
+            => 'Contenu du fichier',
+        'File failed to upload. The file is empty.'
+            => 'Impossible de télécharger le fichier.  Ne fichier est vide.',
+        'Files'
+            => 'Fichiers',
+        'Filter By Created User'
+            => 'Filtrer la liste par Créé par',
+        'Filtered Lists'
+            => 'Listes Filtrées',
+        'First Name'
+            => 'Prénom',
+        'Global Configuration'
+            => 'Configuration globale',
+        'Go to Search'
+            => 'à la Recherche',
+        'Group'
+            => 'Groupe',
+        'Group Module Permissions'
+            => 'Permissions',
+        'Group Policies'
+            => 'Règles de sécurité',
+        'Group Rights'
+            => 'Droits de sécurité',
+        'Groups'
+            => 'Groupes',
+        'here'
+            => 'ici',
+        'Home'
+            => 'Accueil',
+        'Incorrect username or password.'
+            => 'Le nom d\'utilisateur ou le mot de passe est incorrect.',
+        'Industry'
+            => 'Secteur',
+        'Info'
+            => 'Info',
+        'Inherited - All'
+            => 'Hériter-Tous',
+        'Inherited - Allow'
+            => 'Hériter-Permettre',
+        'Invalid currency code'
+            => 'Le code de devise est invalide.',
+        'Is Invalid'
+            => 'Invalide',
+        'Job Title'
+            => 'Titre',
+        'Label must be all lowercase.'
+            => 'Veuillez utiliser des lettres miniscules',
+        'Label must not be empty.'
+            => 'L\'étiquette est obligatoire',
+        'Label must not contain any special characters.'
+            => 'L\'étiquette ne peut pas contenir de caractères spéciaux',
+        'Language'
+            => 'Langue',
+        'Languages: List'
+            => 'Langues: Liste',
+        'Last Name'
+            => 'Nom',
+        'Last Updated'
+            => 'Dernière Modification',
+        'List page size'
+            => 'Nb. d\'éléments de la vue liste',
+        'Load'
+            => 'Télécharger',
+        'Login'
+            => 'Connexion',
+        'Logout'
+            => 'Déconnexion',
+        'Manage Currency Configuration'
+            => 'Gestion des devises',
+        'Manage Global Configuration'
+            => 'Gestion de la configuration globale',
+        'Manage Groups'
+            => 'Gestion des groupes de sécurité',
+        'Manage Roles'
+            => 'Gestion des rôles et hiérarchies',
+        'Max upload size: {maxSize}'
+            => 'Taille max de téléchargement: {maxSize}',
+        'Member'
+            => 'Filiale',
+        'Member Of'
+            => 'Maison mère',
+        'Members'
+            => 'Filiales',
+        'Mobile Phone'
+            => 'Téléphone portable',
+        'Module Name - Plural'
+            => 'Nom du module - au pluriel',
+        'Module Name - Plural (lowercase)'
+            => 'Nom du module - au pluriel (miniscule)',
+        'Module Name - Singular'
+            => 'Nom du module - singluier',
+        'Module Name - Singular (lowercase)'
+            => 'Nom du module - au singulier (miniscule)',
+        'Module Permissions'
+            => 'Permissions',
+        'Munge rebuild complete.'
+            => 'La permission de sécurité de vue liste a été reconstruite.',
+        'My List Configuration View'
+            => 'Ma Vue Liste Configuration',
+        'Never'
+            => 'Jamais',
+        'No Results Found'
+            => 'Aucun résultat trouvé',
+        'Not Set'
+            => 'Défaut',
+        'Not Set - Ad-hoc / Owner'
+            => 'Défaut - de circonstance / propriétaire',
+        'Office Fax'
+            => 'Fax de Bureau',
+        'Office Phone'
+            => 'Téléphone de Bureau',
+        'Oh no!'
+            => 'Zut!',
+        'Only Items I Own'
+            => 'Seulement mes éléments',
+        'Opt Out'
+            => 'Opt-Out',
+        'Owner'
+            => 'Resp. du compte',
+        'Owner Only'
+            => 'Propriétaire',
+        'Parent Group'
+            => 'Groupe mère',
+        'Parent Role'
+            => 'Rôle parent',
+        'Password'
+            => 'Mot de passe',
+        'Permissions'
+            => 'Permissions',
+        'Permitable'
+            => 'Permis',
+        'Please use FireFox, Chrome, or Internet Explorer.'
+            => 'Veuillez utiliser FireFox, Chrome, ou Internet Explorer.',
+        'Policies'
+            => 'Règles',
+        'Popup list page size'
+            => 'Nb. d\'éléments de la liste pop-up',
+        'Postal Code'
+            => 'Code Postal',
+        'Primary Address'
+            => 'Adresse principale',
+        'Primary Email'
+            => 'Adresse e-mail',
+        'Rate to'
+            => 'Taux de change à l\'',
+        'Rate To Base'
+            => 'Taux de base',
+        'Read permissions rebuild complete.'
+            => 'Reconstruction des autorisations lecture effectuée',
+        'Recently Viewed'
+            => 'Éléments récents',
+        'Remember me next time'
+            => 'Retenir le nom d\'utilisateur',
+        'Return to Admin Menu'
+            => 'Retour au menu administrateur',
+        'Rights'
+            => 'Droits',
+        'Role'
+            => 'Rôle',
+        'Roles'
+            => 'Rôles',
+        'Search'
+            => 'Rechercher',
+        'Search by name, phone, or e-mail'
+            => 'Recherche par nom, téléphone, ou email',
+        'Secondary Address'
+            => 'Adresse secondaire',
+        'Secondary Email'
+            => 'Adresse e-mail secondaire',
+        'Securable Item'
+            => 'Élément sécurisable',
+        'Security'
+            => 'Sécurité',
+        'Select a Parent Group'
+            => 'Sélectionner le groupe Parent',
+        'Select a Parent Role'
+            => 'Sélectionner le rôle Parent',
+        'Select a Role'
+            => 'Selectionner un rôle',
+        'Shipping Address'
+            => 'Adresse de livraison',
+        'Size'
+            => 'Taille',
+        'Sorry! Your browser is not supported.'
+            => 'Désolé! Votre navigateur n\'est pas supporté',
+        'Source'
+            => 'Origine',
+        'Stage'
+            => 'Étape',
+        'Start'
+            => 'Commencer',
+        'State'
+            => 'Province/Région',
+        'Street 1'
+            => 'Adresse 1',
+        'Street 2'
+            => 'Adresse 2',
+        'Sublist page size'
+            => 'Nb. d\'éléments de la sous-liste',
+        'Successfully updated'
+            => 'Mise à jour réussie',
+        'Super Administrators'
+            => 'Super Administrateur',
+        'The currency code is invalid'
+            => 'Le code de devise est invalide.',
+        'The file did not exist'
+            => 'Le fichier n\'existe pas.',
+        'There are no recently viewed items.'
+            => 'Aucun éléments récents.',
+        'There was an error uploading the file.'
+            => 'Erreur de téléchargement de fichier.',
+        'There was an error with the web service.'
+            => 'Il y avait une erreur avec le service web.',
+        'This action is only available in debug mode.'
+            => 'Cette action est seulement disponible en mode debug.',
+        'This name is reserved. Please pick a different name.'
+            => 'Ce nom est réservé.  Veuillez choisir un nom différent.',
+        'Time zone'
+            => 'Fuseau horaire',
+        'Title'
+            => 'Fonction',
+        'to continue to the home page. If the error persists please contact your administrator.'
+            => 'pour continuer à l\'accueil. Veuillez contacter votre administrateur système si l\'erreur continue',
+        'User Membership'
+            => 'Membres du groupe',
+        'Username'
+            => 'Nom d\'utilisateur',
+        'Value'
+            => 'Valeur',
+        'Website'
+            => 'Site web',
+        'Welcome'
+            => 'Bienvenue',
+        'What is this?'
+            => 'Aide',
+        'Who can read and write'
+            => 'Autorisation lecture/écriture',
+        'You must have at least one active currency.'
+            => 'Vous devez avoir au moins une devise active.',
+        'Your Profile'
+            => 'Votre Profil',
+        'Your RedBean version is currentVersion and it should be acceptableVersion.'
+            => 'Votre version de RedBean est currentVersion et doit être acceptableVersion.',
+        'Your Yii version is currentVersion and it should be acceptableVersion.'
+            => 'Votre version de Yii est currentVersion et doit être acceptableVersion.',
+        'ZurmoCRM'
+            => 'ZurmoCRM',
+    );
+?>
