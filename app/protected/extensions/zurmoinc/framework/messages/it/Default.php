3c1a929c56e1c562ef8a07611be108e74b81df33
ivica <ivica.nedeljkovic@gmail.com>
2011-10-18 16:09:39 +0200

    Upload related tests added

---
+++ app/protected/extensions/zurmoinc/framework/messages/it/Default.php
@@ -0,0 +1,204 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(None)'
+            => '??',
+        'Advanced Search'
+            => '??',
+        'All'
+            => '??',
+        'by'
+            => '??',
+        'Cancel'
+            => '??',
+        'Cell Settings'
+            => '??',
+        'Celsius'
+            => '??',
+        'Change Log'
+            => '??',
+        'Changed'
+            => '??',
+        'Clear Search'
+            => '??',
+        'Created By User'
+            => '??',
+        'Created Date and User'
+            => '??',
+        'Created Date Time'
+            => '??',
+        'Created On'
+            => '??',
+        'Date'
+            => '??',
+        'Date Time'
+            => '??',
+        'Default Value'
+            => '??',
+        'Delete'
+            => '??',
+        'Detail View Only'
+            => '??',
+        'Details'
+            => '??',
+        'Edit'
+            => '??',
+        'Edit Portlet'
+            => '??',
+        'Error'
+            => '??',
+        'Fahrenheit'
+            => '??',
+        'Filtered Lists'
+            => '??',
+        'firstDateTime must occur after secondDateTime'
+            => '??',
+        'firstDateTime must occur before secondDateTime'
+            => '??',
+        'Float'
+            => '??',
+        'from'
+            => '??',
+        'Full Name'
+            => '??',
+        'Go to Details'
+            => '??',
+        'Inline Edit View'
+            => '??',
+        'Integer'
+            => '??',
+        'Invalid'
+            => '??',
+        'Last Modified'
+            => '??',
+        'Last Modified Date and User'
+            => '??',
+        'List'
+            => '??',
+        'Loading'
+            => '??',
+        'Model Class Name'
+            => '??',
+        'Model Id'
+            => '??',
+        'Modified By User'
+            => '??',
+        'Modified Date Time'
+            => '??',
+        'Module Name'
+            => '??',
+        'More Details'
+            => '??',
+        'More Options'
+            => '??',
+        'My List View'
+            => '??',
+        'Name'
+            => '??',
+        'No'
+            => '??',
+        'None'
+            => '??',
+        'Now'
+            => '??',
+        'Opted Out'
+            => '??',
+        'Panel'
+            => '??',
+        'Panel Settings'
+            => '??',
+        'Panel Title'
+            => '??',
+        'Perform Action'
+            => '??',
+        'Radio Drop Down'
+            => '??',
+        'record'
+            => '??',
+        'records'
+            => '??',
+        'Remove'
+            => '??',
+        'Return to List'
+            => '??',
+        'Row'
+            => '??',
+        'Save'
+            => '??',
+        'Search'
+            => '??',
+        'Select'
+            => '??',
+        'selected for updating.'
+            => '??',
+        'Serialized Data'
+            => '??',
+        'Some values cannot be removed because they are currently in use. ' .
+        'Try changing the records that use them first.'
+            => '??',
+        'Tab'
+            => '??',
+        'This module doesn\'t have anything to configure.'
+            => '??',
+        'This pick-list is used by more than one module.'
+            => '??',
+        'This portlet will be removed, ok?'
+            => '??',
+        'Time'
+            => '??',
+        'Title/First/LastName'
+            => '??',
+        'to'
+            => '??',
+        'Today'
+            => '??',
+        'Tomorrow'
+            => '??',
+        'Unknown'
+            => '??',
+        'Update Selected'
+            => '??',
+        'User'
+            => '??',
+        'Users'
+            => '??',
+        'Yes'
+            => '??',
+        'Yesterday'
+            => '??',
+        'You must enter a value'
+            => '??',
+        'You must specify a value.'
+            => '??',
+        '{attribute} is too precise (maximum decimal places is {precision}).'
+            => '??',
+    );
+?>
---
+++ app/protected/modules/designer/messages/fr/Default.php
@@ -0,0 +1,107 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A field with this name is already used.'
+            => 'Un champ avec ce nom est déjà utilisé.',
+        'All panels visible'
+            => 'Tous visibles',
+        'Create Field'
+            => 'Ajouter un champ',
+        'Designer'
+            => 'Designer',
+        'Detail And Edit View'
+            => 'Vue des formulaires',
+        'Details View'
+            => 'Vue détaillée',
+        'Display Name'
+            => 'Étiquette du champ',
+        'Each item must be uniquely named'
+            => 'Merci d\'ajouter une valeur qui n\'est pas dans la liste.',
+        'Edit View'
+            => 'Formulaire de saisie',
+        'Field Name'
+            => 'Nom du champ',
+        'Field Type'
+            => 'Type de champ',
+        'Fields'
+            => 'Champs',
+        'First panel visible, then `show more` link'
+            => 'Premier panneau visible, ensuite un lien "plus de détails"',
+        'General'
+            => 'Général',
+        'Invalid panel configuration type'
+            => 'La configuration des panneaux choisie est invalide',
+        'Layouts'
+            => 'Présentations de page',
+        'List View'
+            => 'Vue liste',
+        'Manage module fields, layouts, and labels.'
+            => 'Personalisez vos champs, étiquettes, mises en pages etc.',
+        'Mass Edit View'
+            => 'Vue modification en masse',
+        'Maximum Length'
+            => 'Longueur maximale',
+        'Maximum Value'
+            => 'Valeur maximale',
+        'Menu'
+            => 'Menu',
+        'Menus'
+            => 'Menus',
+        'Minimum Value'
+            => 'Valeur minimale',
+        'Module'
+            => 'Module',
+        'Panels are tabbed'
+            => 'Onglets',
+        'Panels Configuration'
+            => 'Configuration des panneaux',
+        'Pick List Values'
+            => 'Éléments de la liste déroulante',
+        'Popup List View'
+            => 'Vue liste pop-up',
+        'Popup Search View'
+            => 'Vue recherche pop-up',
+        'Precision'
+            => 'Précision',
+        'Related List View'
+            => 'Vue liste associée',
+        'Required Field'
+            => 'Champ Requis',
+        'Save Layout'
+            => 'Publier',
+        'Search View'
+            => 'Vue recherche',
+        'There was an error processing your request.'
+            => 'Il y a une erreur',
+        'Track Audit Log'
+            => 'Auditer ce champ',
+    );
+?>
