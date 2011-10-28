d6676cb0951ed55d5e6f0126b94cfb3f2cb6b62c
Jason <Jason@BERN>
2011-10-27 22:17:31 -0500

    more french language work

---
+++ app/protected/modules/designer/messages/es/Default.php
@@ -0,0 +1,187 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A check box'
+            => '??',
+        'A currency field'
+            => '??',
+        'A date field'
+            => '??',
+        'A date/time field'
+            => '??',
+        'A decimal field'
+            => '??',
+        'A description box'
+            => '??',
+        'A field that contains a URL'
+            => '??',
+        'A field with this name is already used.'
+            => '??',
+        'A phone field'
+            => '??',
+        'A pick list that can have multiple selections'
+            => '??',
+        'A pick list with specific values to select from'
+            => '??',
+        'A radio button pick list'
+            => '??',
+        'A text field'
+            => '??',
+        'Access Designer Tool'
+            => '??',
+        'Address fields'
+            => '??',
+        'All panels visible'
+            => '??',
+        'All required fields must be placed in this layout.'
+            => '??',
+        'An integer field'
+            => '??',
+        'Check Box'
+            => '??',
+        'Create Field'
+            => '??',
+        'Custom Fields'
+            => '??',
+        'Decimal'
+            => '??',
+        'Designer'
+            => 'Deseñador',
+        'Designer Home'
+            => '??',
+        'Designer Tool'
+            => '??',
+        'Detail and Edit View'
+            => '??',
+        'Details View'
+            => '??',
+        'Display Name'
+            => '??',
+        'Each item must be uniquely named'
+            => '??',
+        'Edit Field'
+            => '??',
+        'Edit Layout'
+            => '??',
+        'Edit View'
+            => '??',
+        'Email address fields'
+            => '??',
+        'Email Address Information'
+            => '??',
+        'Field'
+            => '??',
+        'Field Name'
+            => '??',
+        'Field Type'
+            => '??',
+        'Fields'
+            => '??',
+        'First character must be a lower case letter'
+            => '??',
+        'First panel visible, then `show more` link'
+            => '??',
+        'General'
+            => '??',
+        'General Edit'
+            => '??',
+        'Invalid panel configuration type'
+            => '??',
+        'Layout saved successfully.'
+            => '??',
+        'Layouts'
+            => '??',
+        'List View'
+            => '??',
+        'Manage module fields, layouts, and labels.'
+            => '??',
+        'Mass Edit View'
+            => '??',
+        'Maximum Length'
+            => '??',
+        'Maximum Value'
+            => '??',
+        'Maximum Value must be larger than the minimum value'
+            => '??',
+        'Menus'
+            => '??',
+        'Minimum Value'
+            => '??',
+        'Module'
+            => '??',
+        'Modules'
+            => '??',
+        'Multi-Select Pick List'
+            => '??',
+        'Name must not contain spaces or special characters'
+            => '??',
+        'Panels are tabbed'
+            => '??',
+        'Panels Configuration'
+            => '??',
+        'Phone'
+            => '??',
+        'Pick List'
+            => '??',
+        'Pick List Value Translations'
+            => '??',
+        'Pick List Values'
+            => '??',
+        'Popup List View'
+            => '??',
+        'Popup Search View'
+            => '??',
+        'Precision'
+            => '??',
+        'Radio Pick List'
+            => '??',
+        'Related List View'
+            => '??',
+        'Required Field'
+            => '??',
+        'Save Layout'
+            => '??',
+        'Search View'
+            => '??',
+        'Text'
+            => '??',
+        'Text Area'
+            => '??',
+        'There was an error processing your request.'
+            => '??',
+        'Track Audit Log'
+            => '??',
+        'URL'
+            => '??',
+        'You must have at least one panel in order to save a layout.'
+            => '??',
+    );
+?>
