d651c839bbb3c8bee0ad9d7ee3f703a377acdec9
Jason <Jason@BERN>
2011-10-28 10:55:22 -0500

    french language work

---
+++ app/protected/modules/contacts/messages/fr/Default.php
@@ -0,0 +1,77 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Company Name'
+            => 'Nom du compte',
+        'Contact'
+            => 'Contact',
+        'Contact Stage'
+            => 'Statut du Contact',
+        'Contact Status Translated Labels'
+            => 'Étiquettes traduites du statut Contact',
+        'Contact Statuses'
+            => 'Statut des Contacts',
+        'Contacts'
+            => 'Contacts',
+        'ContactsModulePluralLabel'
+            => 'ContactsModulePluralLabel',
+        'ContactsModulePluralLabel For AccountsModuleSingularLabel'
+            => 'ContactsModulePluralLabel pour AccountsModuleSingularLabel',
+        'ContactsModulePluralLabel For OpportunitiesModuleSingularLabel'
+            => 'ContactsModulePluralLabel pour OpportunitiesModuleSingularLabel',
+        'ContactsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel',
+        'ContactsModuleSingularLabel or LeadsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel ou LeadsModuleSingularLabel',
+        'ContactsModuleSingularLabel Search'
+            => 'ContactsModuleSingularLabel Search',
+        'Create ContactsModuleSingularLabel'
+            => 'Nouveau ContactsModuleSingularLabel',
+        'Each ContactsModuleSingularLowerCaseLabel state must be uniquely named'
+            => 'Chaque état du ContactsModuleSingularLowerCaseLabel doit avoir un nom unique',
+        'My ContactsModulePluralLabel'
+            => 'Mes ContactsModulePluralLabel',
+        'Order'
+            => 'Ordre',
+        'Starting Status'
+            => 'Valeur par défaut',
+        'The contact state field'
+            => 'Le champ',
+        'The default status specified does not exist.'
+            => 'La valeur du statut par défault spécifié n\'existe pas.',
+        'The status is required.  Neither a value nor a default was specified.'
+            => 'Le champ \'statut\' est obligatoire.  Vous devez spécifiez une valeur.',
+        'The status specified does not exist.'
+            => 'La valeur du statut spécifié n\'existe pas.',
+        'The status specified is not unique and is invalid.'
+            => 'La valeur du statut spécifié n\'est pas unique et n\'est pas valide.',
+    );
+?>
