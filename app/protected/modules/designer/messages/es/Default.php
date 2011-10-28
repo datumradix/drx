50c60eff0972d4eb2e9c24e3468d8d9e33e62d43
zurmo <jason@zurmo.com>
2011-10-28 10:47:32 -0500

    spanish language work

---
+++ app/protected/modules/designer/messages/es/Default.php
@@ -0,0 +1,121 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A field with this name is already used.'
+            => 'Un campo con este nombre ya está en uso.',
+        'A phone field'
+            => 'Un campo de teléfono',
+        'A pick list that can have multiple selections'
+            => 'Un pick list que puede tener varias selecciones',
+        'A pick list with specific values to select from'
+            => 'Un pick list con valores específicos para seleccionar ',
+        'A radio button pick list'
+            => 'Un botón de radio pick list',
+        'A text field'
+            => 'Un campo de texto',
+        'Access Designer Tool'
+            => 'Acceso a Designer',
+        'Address fields'
+            => 'Campos de dirección',
+        'All panels visible'
+            => 'Todos los paneles visibles',
+        'Create Field'
+            => 'Crear campo',
+        'Designer'
+            => 'Diseñador',
+        'Detail and Edit View'
+            => 'Vista de formularios',
+        'Details View'
+            => 'Vista de detalles',
+        'Display Name'
+            => 'Etiqueta del campo',
+        'Each item must be uniquely named'
+            => 'Cada elemento debe tener un nombre exclusivo',
+        'Edit View'
+            => 'Formulario de inscripción',
+        'Field Name'
+            => 'Nombre de campo',
+        'Field Type'
+            => 'Tipo de campo',
+        'Fields'
+            => 'Campos',
+        'First panel visible, then `show more` link'
+            => 'Primer panel visible, después `mostrar más` link',
+        'General'
+            => 'General',
+        'Invalid panel configuration type'
+            => 'Tipo de configuración de panel inválido',
+        'Layouts'
+            => 'Presentaciones de página',
+        'List View'
+            => 'Vista de lista',
+        'Manage module fields, layouts, and labels.'
+            => 'Administrar campos de módulo, diseños y etiquetas',
+        'Mass Edit View'
+            => 'Vista de modificación de masa',
+        'Maximum Length'
+            => 'Longitud máxima',
+        'Maximum Value'
+            => 'Valor máximo',
+        'Menu'
+            => 'menú',
+        'Menus'
+            => 'menús',
+        'Minimum Value'
+            => 'valor mínimo',
+        'Module'
+            => 'Módulo',
+        'Panels are tabbed'
+            => 'Los paneles son pestañas',
+        'Panels Configuration'
+            => 'Configuración de paneles',
+        'Pick List Values'
+            => 'Valores de pick list',
+        'Popup List View'
+            => 'Vista de lista popup',
+        'Popup Search View'
+            => 'Búsquada de lista popup',
+        'Precision'
+            => 'precisión',
+        'Related List View'
+            => 'Vista de lista relacionada',
+        'Required Field'
+            => 'Campo requerido',
+        'Save Layout'
+            => 'Publicar',
+        'Search View'
+            => 'Vista de búsqueda',
+        'There was an error processing your request.'
+            => 'Hubo un error al procesar su pedido',
+        'Track Audit Log'
+            => 'Auditar este campo',
+    );
+?>
