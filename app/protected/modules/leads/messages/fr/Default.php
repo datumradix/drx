b78cd95cccc649b2484548b434ea365ebbc652a2
digimagic <digimagic@localhost>
2011-10-31 16:16:17 -0500

    Fixed minor issue with import util.  The incoming argument value is never an integer but always a string.

---
+++ app/protected/modules/leads/messages/fr/Default.php
@@ -0,0 +1,87 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access LeadsModulePluralLabel Tab'
+            => 'Accès aux LeadsModulePluralLabel',
+        'AccountsModuleSingularLabel Optional'
+            => 'AccountsModuleSingularLabel Optionnel',
+        'AccountsModuleSingularLabel Required'
+            => 'AccountsModuleSingularLabel Requis',
+        'Complete Conversion'
+            => 'Convertir',
+        'Complete LeadsModuleSingularLowerCaseLabel conversion without selecting or creating an AccountsModuleSingularLowerCaseLabel.'
+            => 'Convertir la LeadsModuleSingularLowerCaseLabel sans sélectionner ou créer un AccountsModuleSingularLowerCaseLabel.',
+        'Conversion is set to require an AccountsModuleSingularLowerCaseLabel.  Currently you do not have access to the AccountsModulePluralLowerCaseLabel module. Please contact your administrator regarding this.'
+            => 'Vous avez chosi de convertir un AccountsModuleSingularLowerCaseLabel, mais vous n\'avez pas accès au module de AccountsModuleSingularLowerCaseLabel.  Veuillez contacter votre administrateur système.',
+        'Conversion requires access to the ContactsModulePluralLowerCaseLabel module which you do not have. Please contact your administrator regarding this.'
+            => 'Vous devez avoir accès au module de ContactsModulePluralLowerCaseLabel pour continuer.  Veuillez contacter votre administrateur système.',
+        'Convert LeadsModulePluralLabel'
+            => 'Convertir LeadsModulePluralLabel',
+        'Create LeadsModulePluralLabel'
+            => 'Créer LeadsModulePluralLabel',
+        'Create LeadsModuleSingularLabel'
+            => 'Nouvelle LeadsModuleSingularLabel',
+        'Delete LeadsModulePluralLabel'
+            => 'Supprimer LeadsModulePluralLabel',
+        'Do not show AccountsModuleSingularLabel'
+            => 'Ne pas montrer le AccountsModuleSingularLabel',
+        'Lead'
+            => 'Piste',
+        'Leads'
+            => 'Pistes',
+        'LeadsModulePluralLabel'
+            => 'LeadsModulePluralLabel',
+        'LeadsModulePluralLabel and ContactsModulePluralLabel are the same records,' .
+        ' just in a different status. To create a LeadsModuleSingularLowerCaseLabel' .
+        ' field, create a ContactsModuleSingularLowerCaseLabel field, and then it ' .
+        ' will be placeable in the LeadsModulePluralLowerCaseLabel layouts.'
+            => 'LeadsModulePluralLabel et ContactsModulePluralLabel sont les mêmes données,' .
+               ' ils ont juste un statut différent. Pour créer un champ LeadsModuleSingularLowerCaseLabel,' .
+               ' veuillez créer le champ dans le module ContactsModuleSingularLowerCaseLabel, le champ' .
+               ' apparaîtra alors dans les présentations de pages des LeadsModulePluralLowerCaseLabel.',
+        'LeadsModuleSingularLabel'
+            => 'LeadsModuleSingularLabel',
+        'LeadsModuleSingularLabel Conversion'
+            => 'Conversion de la LeadsModuleSingularLabel',
+        'LeadsModuleSingularLabel Search'
+            => 'Recherche LeadsModuleSingularLabel',
+        'LeadsModuleSingularLabel successfully converted.'
+            => 'LeadsModuleSingularLabel convertie.',
+        'LeadsModuleSingularLabel was not converted. An error occurred.'
+            => 'La LeadsModuleSingularLabel n\'a pas été convertie.  Il y a eu une erreur.',
+        'My LeadsModulePluralLabel'
+            => 'Mes LeadsModulePluralLabel',
+        'Select AccountsModuleSingularLabel'
+            => 'Choisir un AccountsModuleSingularLabel',
+        'Skip AccountsModuleSingularLabel'
+            => 'Aucun AccountsModuleSingularLabel',
+    );
+?>
---
+++ app/protected/modules/zurmo/messages/es/Default.php
@@ -0,0 +1,549 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(Unknown)'
+            => '(Desconocido)',
+        '(Unnamed)'
+            => '(Sin Nombre)',
+        '2D Donut Chart'
+            => 'Anillo',
+        '2D Horizontal Bar Graph'
+            => 'Bares',
+        '2D Pie Chart'
+            => 'Gráfico circular 2D',
+        '2D Vertical Bar Graph'
+            => 'Histograma 2D',
+        '3D Pie Chart'
+            => 'Gráfico circular 3D',
+        '3D Vertical Bar Graph'
+            => 'Histograma 3D',
+        '<b>Zurmo</b> is a <b>Customer Relationship Management</b> system by <b>Zurmo Inc.</b>'
+            => '??',
+        '<b>Zurmo</b> is licensed under the GPLv3.  You can read the <a href="http://www.zurmo.org/license">license here</a>'
+            => '??',
+        '<b>Zurmo</b> uses the following great Open Source tools and frameworks:'
+            => '??',
+        'About'
+            => 'Sobre',
+        'Access Administration Tab'
+            => 'Acceso a Administración',
+        'Access Currency Configuration'
+            => 'Acceso a Divisas',
+        'Access Failure'
+            => 'Falta de acceso',
+        'Access Global Configuration'
+            => 'Acceso a la Configuración Global',
+        'Access Groups Tab'
+            => 'Acceso a Grupos',
+        'Access Roles Tab'
+            => 'Acceso a Roles',
+        'Active'
+            => 'Activo',
+        'Active currencies can be used when creating new records and as a default currency for a user.'
+            => 'Divisas activos se pueden utilizar al crear nuevos registros, y como una divisa predeterminada para un usuario.',
+        'Active languages can be used by users. The system language and any language in use by a user cannot be inactivated'
+            => 'Idiomas activos pueden ser utilizados por los usuarios. El idioma del sistema y de cualquier idioma utilizado por un usuario no puede ser desactivado.',
+        'Ad-hoc / Owner'
+            => 'Ad-hoc / Propietario',
+        'Add Currency'
+            => '??',
+        'Address'
+            => 'Dirección',
+        'Admin'
+            => 'Admin',
+        'Alert'
+            => 'Alerta',
+        'Allow'
+            => 'Permitir',
+        'Amount'
+            => 'Suma',
+        'An error has occurred. Please click'
+            => 'Ha ocurrido un error. Por favor, haga clic',
+        'Any City'
+            => 'Ciudad',
+        'Any Country'
+            => 'País',
+        'Any Email Address'
+            => 'Dirección de email',
+        'Any Invalid Email'
+            => 'Email inválida',
+        'Any Opted Out Email'
+            => 'Email opt-out',
+        'Any Postal Code'
+            => 'Código postal',
+        'Any State'
+            => 'Estado',
+        'Any Street'
+            => 'Calle',
+        'are required.'
+            => 'son obligatorios.',
+        'Are you sure you want to remove this group?'
+            => '¿Está seguro que desea eliminar este grupo?',
+        'Are you sure you want to remove this role?'
+            => '¿Está seguro que desea eliminar este role?',
+        'Attachment'
+            => 'Adjunto',
+        'Attachments'
+            => 'Adjuntos',
+        'Audit Event'
+            => 'Lista de modificaciones',
+        'Audit Trail'
+            => 'Log de modificaciones',
+        'Automotive'
+            => '??',
+        'Banking'
+            => '??',
+        'Business Services'
+            => '??',
+        'Cancel'
+            => 'Cancelar',
+        'Changes to active currencies changed successfully.'
+            => 'Divisas activas cambiado con éxito.',
+        'Changes to active languages changed successfully.'
+            => 'Idiomas activos cambiado con éxito.',
+        'Chart Type'
+            => 'Tipo de gráfico',
+        'City'
+            => 'Ciudad',
+        'Close'
+            => '??',
+        'Code'
+            => 'Código',
+        'Configuration'
+            => 'Configuración',
+        'Convert'
+            => 'Convertir',
+        'Copyright &#169; Zurmo Inc., 2011. All Rights reserved.'
+            => 'Copyright &#169; Zurmo Inc. de 2011. Todos los derechos reservados.',
+        'Country'
+            => 'Pais',
+        'Create Group'
+            => 'Crear grupo',
+        'Create Groups'
+            => 'Crear grupos',
+        'Create New'
+            => 'Crear nuevo',
+        'Create Role'
+            => 'Crear role',
+        'Create Roles'
+            => 'Crear roles',
+        'Currencies: Create'
+            => 'Divisas: crear',
+        'Currencies: List'
+            => 'Divisas: listar',
+        'Currency'
+            => 'Divisa',
+        'Currency Configuration'
+            => 'Configuración de divisas',
+        'Currency in use.'
+            => 'Divisa en uso',
+        'Currency Value'
+            => 'Valor de divisa',
+        'Customer'
+            => '??',
+        'Dashboard portlet list page size'
+            => 'Cantidad de elementos que figuran en el portlet',
+        'Data'
+            => 'Datos',
+        'days'
+            => 'Días',
+        'Dead'
+            => '??',
+        'Delete Group'
+            => 'Eliminar grupo',
+        'Delete Groups'
+            => 'Eliminar grupos',
+        'Delete Role'
+            => 'Eliminar role',
+        'Delete Roles'
+            => 'Eliminar roles',
+        'Deny'
+            => 'Denegar',
+        'Department'
+            => 'Departamento',
+        'Description'
+            => 'Descripción',
+        'Dr.'
+            => '??',
+        'Email Address'
+            => 'Dirección de email',
+        'Energy'
+            => '??',
+        'Event Name'
+            => '??',
+        'every'
+            => 'cada',
+        'Everyone'
+            => 'Todo el Mundo',
+        'Fields with'
+            => 'Los campos con',
+        'File Content'
+            => 'Contenido de archivo',
+        'File failed to upload. The file is empty.'
+            => 'Archivo no se pudo cargar. El archivo está vacío.',
+        'Files'
+            => 'Archivos',
+        'Filter By Created User'
+            => 'Filtrar la lista por Creado por',
+        'Filtered Lists'
+            => 'Listas filtradas',
+        'Financial Services'
+            => '??',
+        'First Name'
+            => 'Primer nombre',
+        'Global Configuration'
+            => 'Configuración global',
+        'Global configuration saved successfully.'
+            => 'Configuración global guardado con éxito.',
+        'Go to Search'
+            => 'Ir a la búsqueda',
+        'Group'
+            => 'Grupo',
+        'Group Module Permissions'
+            => 'Permisos',
+        'Group Policies'
+            => 'Reglas de Seguridad',
+        'Group Rights'
+            => 'Derechos de grupo',
+        'Groups'
+            => 'Grupos',
+        'here'
+            => 'aquí',
+        'Home'
+            => 'Inicio',
+        'In Progress'
+            => '??',
+        'Inbound Call'
+            => '??',
+        'Incorrect username or password.'
+            => 'Nombre de usuario o contraseña incorrecta.',
+        'Industry'
+            => 'industria',
+        'Inherited - All'
+            => 'Heredar - Todos',
+        'Inherited - Allow'
+            => 'Heredar - Permitir',
+        'Inherited - Deny'
+            => 'Heredar - Denegar',
+        'Inherited - No'
+            => 'Heredar - No',
+        'Inherited - Owner Only'
+            => 'Heredar - propietario sólo',
+        'Inherited - Yes'
+            => 'Heredar - Sí',
+        'Inherited Value:'
+            => 'Valor heredado:',
+        'Insurance'
+            => '??',
+        'Invalid currency code'
+            => 'Código de divisa inválido',
+        'Is Invalid'
+            => 'Es inválido',
+        'Job Title'
+            => 'Título',
+        'Label must be all lowercase.'
+            => 'Etiqueta debe ser todo minúsculo.',
+        'Label must not be empty.'
+            => 'Etiqueta no debe ser vacío.',
+        'Label must not contain any special characters.'
+            => 'La etiqueta no debe contener caracteres especiales.',
+        'Language'
+            => 'Lengua',
+        'Languages'
+            => '??',
+        'Languages: List'
+            => 'Idiomas: Lista',
+        'Last Name'
+            => 'Apellido',
+        'Last Updated'
+            => 'Última actualización',
+        'List Filters'
+            => '??',
+        'List page size'
+            => 'Número de elementos en la vista de lista',
+        'List View'
+            => 'Vista de lista',
+        'Load'
+            => 'Cargar',
+        'Login'
+            => 'Acceder',
+        'Logout'
+            => 'Salir',
+        'Manage Active Languages'
+            => '??',
+        'Manage Currency Configuration'
+            => 'Administrar la configuración de divisas',
+        'Manage Global Configuration'
+            => 'Administrar la configuración global',
+        'Manage Groups'
+            => 'Administrar grupos',
+        'Manage Roles'
+            => 'Administrar roles',
+        'Manufacturing'
+            => '??',
+        'Member'
+            => 'Miembro',
+        'Member Of'
+            => 'Miembro de',
+        'Members'
+            => 'Miembros',
+        'Menu'
+            => 'Menú',
+        'Missing Required Fields'
+            => 'Los campos obligatorios se colocará en el punto de vista',
+        'Mobile Phone'
+            => 'Tel. móvil',
+        'Module Name - Plural'
+            => 'Nombre del módulo - Plural',
+        'Module Name - Plural (lowercase)'
+            => 'Nombre del módulo - Plural (en minúsculas)',
+        'Module Name - Singular'
+            => 'Nombre del módulo - Singular',
+        'Module Name - Singular (lowercase)'
+            => 'Nombre del módulo - Singular (en minúsculas)',
+        'Module Permissions'
+            => 'Permisos de módulo',
+        'Module Permissions Saved Successfully.'
+            => 'Permisos de módulo guardado correctamente.',
+        'Mr.'
+            => '??',
+        'Mrs.'
+            => '??',
+        'Ms.'
+            => '??',
+        'My List Configuration View'
+            => 'Mi vista de lista configuración',
+        'Never'
+            => 'Nunca',
+        'New'
+            => '??',
+        'No Results Found'
+            => 'No se encontraron resultados',
+        'Non Members'
+            => 'No miembres',
+        'Not Set'
+            => 'Predeterminado',
+        'Not Set - Ad-hoc / Owner'
+            => 'Predeterminado - Ad-hoc / Propietario',
+        'Office Fax'
+            => 'Fax de Oficina',
+        'Office Phone'
+            => 'Teléfono de la Oficina',
+        'Oh no!'
+            => 'Oh no!',
+        'Only Items I Own'
+            => 'Sólo mis elementos',
+        'Opt Out'
+            => 'Opt out',
+        'Owner'
+            => 'Propietario',
+        'Owner and users in'
+            => 'Propietario y usuarios en',
+        'Owner Only'
+            => 'Propietario sólo',
+        'Parent Group'
+            => 'Parent group',
+        'Parent Role'
+            => 'Parent role',
+        'Password'
+            => 'Contraseña',
+        'Permissions'
+            => 'Permisos',
+        'Permitable'
+            => 'Permitable',
+        'Please use FireFox, Chrome, or Internet Explorer.'
+            => 'Por favor, use Firefox, Chrome, o Internet Explorer.',
+        'Policies'
+            => 'Reglas',
+        'Policies Saved Successfully.'
+            => 'Reglas guardado correctamente.',
+        'Popup list page size'
+            => 'Número de elementos en la lista popup',
+        'Postal Code'
+            => 'Código postal',
+        'Primary Address'
+            => 'Dirección principal',
+        'Primary Email'
+            => 'Email principal',
+        'Qualified'
+            => '??',
+        'Rate to'
+            => 'Tipo de cambio a',
+        'Rate To Base'
+            => 'Tipo de cambio base',
+        'Read'
+            => '??',
+        'Read permissions rebuild complete.'
+            => 'Reconstrucción de permisis lectura completo.',
+        'Recently Viewed'
+            => 'Visto recientemente',
+        'Recycled'
+            => '??',
+        'Remember me next time'
+            => 'Recordarme la próxima vez',
+        'Retail'
+            => '??',
+        'Return to Admin Menu'
+            => 'Volver al menú de administración',
+        'Rights'
+            => 'Derechos',
+        'Rights Saved Successfully.'
+            => 'Derechos guardado correctamente.',
+        'Role'
+            => 'Role',
+        'Roles'
+            => 'Roles',
+        'Salutation'
+            => '??',
+        'Save Changes'
+            => '??',
+        'Search'
+            => 'Buscar',
+        'Search by name, phone, or e-mail'
+            => 'Buscar por nombre, teléfono, o e-mail',
+        'Secondary Address'
+            => 'Dirección secundaria',
+        'Secondary Email'
+            => 'Email secundaria',
+        'Securable Item'
+            => 'Artículo asegurable',
+        'Security'
+            => 'seguridad',
+        'Select a Parent Group'
+            => 'Seleccione un grupo de padres',
+        'Select a Parent Role'
+            => 'Seleccione un role de padres',
+        'Select a Role'
+            => 'Seleccione un role',
+        'Self-Generated'
+            => '??',
+        'Shipping Address'
+            => 'Dirección de envío',
+        'Size'
+            => 'Tamaño',
+        'skipped because you do not have sufficient permissions.'
+            => 'Omitido porque no tiene los permisos necesarios.',
+        'Sorry! Your browser is not supported.'
+            => '¡Lo siento! Su navegador no es compatible.',
+        'Source'
+            => 'Fuente',
+        'Stage'
+            => 'Etapa',
+        'Standard Fields'
+            => '??',
+        'State'
+            => 'Estado',
+        'Status'
+            => '??',
+        'Street 1'
+            => 'Calle 1',
+        'Street 2'
+            => 'Calle 2',
+        'Sublist page size'
+            => 'Número de elementos de la sublista',
+        'Successfully updated'
+            => 'Actualizado correctamente',
+        'Super Administrators'
+            => 'Superadministradores',
+        'Technology'
+            => '??',
+        'The currency rate web service says this currency code is invalid even though zurmo says it is valid. The rate could not be automatically updated.'
+            => 'El servicio web para los tipos de cambio indica que este código de divisa no es válida, aunque zurmo indica que es válida. El tipo de cambio no puede ser actualizado automáticamente',
+        'The currency rate web service was unavailable. The rate could not be automatically updated.'
+            => 'El servicio web para los tipos de cambio no estaba disponible. El tipo de cambio no puede ser actualizado automáticamente.',
+        'The currency was not removed because it is in use.'
+            => 'La moneda no se ha eliminado porque está en uso.',
+        'The status specified is invalid.'
+            => '??',
+        'There are no recently viewed items.'
+            => 'No hay elementons recientemente vistos',
+        'There are required fields missing from the following layout: {view}.  Please contact your administrator.'
+            => 'Campos obligatorios faltan en la siguiente diseño: {vista}. Por favor, contacte con su administrador.',
+        'This action is only available in debug mode.'
+            => 'Esta acción sólo está disponible en modo debug.',
+        'This is <b>version {zurmoVersion}</b> of <b>Zurmo</b>.'
+            => '??',
+        'This name is reserved. Please pick a different name.'
+            => 'Este nombre está reservado. Por favor, elija un nombre distinto.',
+        'Time zone'
+            => 'Zona horaria',
+        'Title'
+            => 'Título',
+        'to continue to the home page. If the error persists please contact your administrator.'
+            => 'para continuar a la página de inicio. Si el error persiste, por favor comuníquese con el administrador.',
+        'Tradeshow'
+            => '??',
+        'Type a currency code or name to search.'
+            => 'Escriba un código de divisa o nombre para buscar',
+        'Update'
+            => '??',
+        'User Membership'
+            => 'Miembros del grupo',
+        'User Membership Saved Successfully.'
+            => 'Miembros del grupo guardado con éxito.',
+        'Username'
+            => 'Usuario',
+        'Value'
+            => 'Valor',
+        'Visit <b>Zurmo Inc.</b> at {url}.'
+            => '??',
+        'Visit the <b>Zurmo Open Source Project</b> at {url}.'
+            => '??',
+        'Website'
+            => 'Sitio web',
+        'Welcome'
+            => 'Bienvenidos',
+        'What is this?'
+            => '¿Qué es esto?',
+        'Who can read and write'
+            => 'Permiso de lectura / escritura',
+        'Word of Mouth'
+            => '??',
+        'Write'
+            => '??',
+        'You have tried to access a page you do not have access to.'
+            => 'Ha intentado acceder a una página que no tienen acceso.',
+        'You must have at least one active currency.'
+            => 'Debe tener al menos una divisa activa.',
+        'Your Profile'
+            => 'Su Perfil',
+        'Your RedBean version is currentVersion and it should be acceptableVersion.'
+            => 'Su versión de RedBean es currentVersion y debe ser acceptableVersion.',
+        'Your Yii version is currentVersion and it should be acceptableVersion.'
+            => 'Su versión de Yii es currentVersion y debe ser acceptableVersion',
+        'ZurmoCRM'
+            => 'ZurmoCRM',
+        '{url} (installed with Yii)'
+            => '??',
+        '{url} (version {version} is installed)'
+            => '??',
+    );
+?>
