9aee1c5f94fbfdcae1f643353a804399656f780f
Nilesh Patkar <bitbucket@antechnologies.co.in>
2011-11-29 16:05:21 +0530

    This commit is for fixing the isAjaxMode functionality.

---
+++ app/protected/modules/leads/messages/de/Default.php
@@ -0,0 +1,84 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access LeadsModulePluralLabel Tab'
+            => 'Zugang LeadsModulePluralLabel Tab',
+        'AccountsModuleSingularLabel Optional'
+            => 'AccountsModuleSingularLabel Optional',
+        'AccountsModuleSingularLabel Required'
+            => 'AccountsModuleSingularLabel Erforderlich',
+        'Complete LeadsModuleSingularLowerCaseLabel conversion without selecting or creating an AccountsModuleSingularLowerCaseLabel.'
+            => 'Konvertieren LeadsModuleSingularLowerCaseLabel ohne eine AccountsModuleSingularLowerCaseLabel',
+        'Conversion is set to require an AccountsModuleSingularLowerCaseLabel.  Currently you do not have access to the AccountsModulePluralLowerCaseLabel module. Please contact your administrator regarding this.'
+            => 'Konvertierung erfordert eine AccountsModuleSingularLowerCaseLabel. Derzeit Sie haben keinen Zugriff auf die AccountsModulePluralLowerCaseLabel Modul. Bitte kontaktieren Sie an Ihren Administrator..',
+        'Conversion requires access to the ContactsModulePluralLowerCaseLabel module which you do not have. Please contact your administrator regarding this.'
+            => 'Konvertierung erfordert Zugang zu den ContactsModulePluralLowerCaseLabel Modul, das Sie nicht haben. Bitte kontaktieren Sie an Ihren Administrator.',
+        'Convert LeadsModulePluralLabel'
+            => 'Konvertieren LeadsModulePluralLabel',
+        'Create LeadsModulePluralLabel'
+            => 'LeadsModulePluralLabel erstellen',
+        'Create LeadsModuleSingularLabel'
+            => 'LeadsModuleSingularLabel erstellen',
+        'Delete LeadsModulePluralLabel'
+            => 'LeadsModulePluralLabel löschen',
+        'Do not show AccountsModuleSingularLabel'
+            => 'AccountsModuleSingularLabel Nicht anzeigen',
+        'Lead'
+            => 'Interessent',
+        'Leads'
+            => 'Interessenten',
+        'LeadsModulePluralLabel'
+            => 'LeadsModulePluralLabel',
+        'LeadsModulePluralLabel and ContactsModulePluralLabel are the same records, ' .
+        'just in a different status. To create a LeadsModuleSingularLowerCaseLabel ' .
+        'field, create a ContactsModuleSingularLowerCaseLabel field, and then it ' .
+        'will be placeable in the LeadsModulePluralLowerCaseLabel layouts.'
+            => 'LeadsModulePluralLabel und ContactsModulePluralLabel sind die gleichen Datensätze' .
+            'aber in einen anderen Status. Zum Erstellen einer LeadsModuleSingularLowerCaseLabel Feld, erstellen Sie eine ContactsModuleSingularLowerCaseLabel Feld,' .
+            'und dann wird es in der LeadsModulePluralLowerCaseLabel Layouts platziert werden.',
+        'LeadsModuleSingularLabel'
+            => 'LeadsModuleSingularLabel',
+        'LeadsModuleSingularLabel Conversion'
+            => 'LeadsModuleSingularLabel ??',
+        'LeadsModuleSingularLabel Search'
+            => 'LeadsModuleSingularLabel ??',
+        'LeadsModuleSingularLabel successfully converted.'
+            => 'LeadsModuleSingularLabel erfolgreich konvertiert',
+        'LeadsModuleSingularLabel was not converted. An error occurred.'
+            => 'LeadsModuleSingularLabel wurde nicht konvertiert. Ein Fehler ist aufgetreten',
+        'My LeadsModulePluralLabel'
+            => 'Meine LeadsModulePluralLabel',
+        'Select AccountsModuleSingularLabel'
+            => 'AccountsModuleSingularLabel Wählen',
+        'Skip AccountsModuleSingularLabel'
+            => 'AccountsModuleSingularLabel überspringen',
+    );
+?>
