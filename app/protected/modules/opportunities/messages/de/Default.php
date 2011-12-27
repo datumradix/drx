96863e8d9d21c1da220d0bf846377d1a260fc7ba
digimagic <digimagic@localhost>
2011-12-27 12:59:12 -0500

    Refactored ControllerSecurityUtil to support override of the failure processing.

---
+++ app/protected/modules/opportunities/messages/de/Default.php
@@ -0,0 +1,77 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access OpportunitiesModulePluralLabel Tab'
+            => 'Zugang OpportunitiesModulePluralLabel Tab',
+        'Close Date'
+            => 'Abschlussdatum',
+        'Closed Lost'
+            => 'Closed Lost',
+        'Closed Won'
+            => 'Closed Won',
+        'Create OpportunitiesModulePluralLabel'
+            => 'OpportunitiesModulePluralLabel erstellen',
+        'Create OpportunitiesModuleSingularLabel'
+            => 'OpportunitiesModuleSingularLabel erstellen',
+        'Delete OpportunitiesModulePluralLabel'
+            => 'OpportunitiesModulePluralLabel löschen',
+        'My OpportunitiesModulePluralLabel'
+            => 'Meine OpportunitiesModulePluralLabel',
+        'Negotiating'
+            => 'Negotiating',
+        'Opportunities'
+            => 'Verkaufschancen',
+        'Opportunities By Lead Source'
+            => 'Verkaufschancen von Interessente Quelle',
+        'Opportunities By Sales Stage'
+            => 'Verkaufschancen von Verkaufsphase',
+        'OpportunitiesModulePluralLabel'
+            => 'OpportunitiesModulePluralLabel',
+        'OpportunitiesModulePluralLabel For AccountsModuleSingularLabel'
+            => 'OpportunitiesModulePluralLabel für AccountsModuleSingularLabel',
+        'OpportunitiesModulePluralLabel For ContactsModuleSingularLabel'
+            => 'OpportunitiesModulePluralLabel für ContactsModuleSingularLabel',
+        'OpportunitiesModuleSingularLabel'
+            => 'OpportunitiesModuleSingularLabel',
+        'OpportunitiesModuleSingularLabel Search'
+            => 'OpportunitiesModuleSingularLabel Suche',
+        'Opportunity'
+            => 'Verkaufschance',
+        'Probability'
+            => 'Wahrscheinlichkeit ',
+        'Prospecting'
+            => 'Prospecting',
+        'Qualification'
+            => 'Qualifikation',
+        'Verbal'
+            => 'Mündlich',
+    );
+?>
