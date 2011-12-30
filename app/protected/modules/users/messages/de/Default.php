c458a57b5bd12e47c005d643af4e7e2f2d05456f
Laura <Laura@lolo>
2011-12-30 11:00:02 -0500

    Italian language translation work

---
+++ app/protected/modules/users/messages/de/Default.php
@@ -0,0 +1,99 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A user field'
+            => 'Ein Benutzer Feld',
+        'Access Users Tab'
+            => 'Zugang Benutzer Tab',
+        'Change Password'
+            => 'Kennwort ändern',
+        'Change User Passwords'
+            => 'Ändern Kennwörter von Benutzern',
+        'Confirm Password'
+            => 'Kennwort bestätigen',
+        'Create User'
+            => 'Benutzer erstellen',
+        'Create Users'
+            => 'Benutzern erstellen',
+        'day(s)'
+            => 'Tag(e)',
+        'Enforce Strong Passwords'
+            => 'Erzwingen sicherer Kennwörter',
+        'Hash'
+            => 'Hash',
+        'Inactive users cannot log in using the web, mobile or web API. Login for active users is controlled by group rights.'
+            => 'Inaktive Benutzer können sich nicht anmelden über die Web-, Mobil-oder Web-API. Anmeldung für aktive Benutzer wird durch Gruppenrechte kontrolliert.',
+        'Inherited Value:'
+            => 'Vererbte Wert:',
+        'Login Via Mobile'
+            => 'Anmelden via Mobile',
+        'Login Via Web'
+            => 'Anmelden via Web',
+        'Login Via Web API'
+            => 'Anmelden via Web API',
+        'Manage Users'
+            => 'Benutzer verwalten',
+        'Manager'
+            => 'Manager',
+        'Minimum Password Length'
+            => 'Minimale Kennwortlänge',
+        'Minimum Username Length'
+            => 'Minimale Länge des Benutzernamens',
+        'Password Expires'
+            => 'Kennwort abläuft',
+        'Password Expiry Days'
+            => 'Kennwort Ablauf Tage',
+        'Profile'
+            => 'Profil',
+        'The password is too short. Minimum length is {minimumLength}.'
+            => 'Das Kennwort ist zu kurz. Minimale Länge ist {minimumLength}.',
+        'The password must have at least one lowercase letter'
+            => 'Das Kennwort muss mindestens einen Kleinbuchstaben enthalten',
+        'The password must have at least one number and one letter'
+            => 'Das Kennwort muss mindestens eine Zahl und einen Buchstaben enthalten',
+        'The password must have at least one uppercase letter'
+            => 'Das Kennwort muss mindestens einen Großbuchstaben enthalten',
+        'The time zone is invalid.'
+            => 'Die Zeitzone ist ungültig.',
+        'The username is too short. Minimum length is {minimumLength}.'
+            => 'Der Benutzername ist zu kurz. Minimale Länge ist {minimumLength}.',
+        'Time Zone'
+            => 'Zeitzone',
+        'User'
+            => 'Benutzer',
+        'User configuration saved successfully.'
+            => 'Benutzerkonfiguration erfolgreich gespeichert.',
+        'User Search'
+            => 'Benutzer Suche',
+        'Users'
+            => 'Benutzern',
+    );
+?>
