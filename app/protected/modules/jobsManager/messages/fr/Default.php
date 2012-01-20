dcaef07f6d977e8a8538b1f932f11a64285ee343
ivica <ivica.nedeljkovic@gmail.com>
2012-01-20 15:42:54 +0100

    Track runtime, data and assests folders

---
+++ app/protected/modules/jobsManager/messages/fr/Default.php
@@ -0,0 +1,119 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2011 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '[with errors]'
+            => '[avec erreurs]',
+        'A job was completed with errors.'
+            => 'Erreurs lors de l\'exécution d\'un job.',
+        'Access Jobs Manager Tab'
+            => 'Accès aux tâches système',
+        'Available Jobs'
+            => 'Jobs disponibles',
+        'Click Here'
+            => 'Cliquez ici',
+        'Completed'
+            => 'Réalisé',
+        'Completed with Errors'
+            => 'Réalisé avec des erreurs',
+        'End Date Time'
+            => 'Date/heure réalisé?',
+        'Error Log'
+            => 'Log d\'Erreurs',
+        'Every 5 minutes'
+            => 'Chaque 5 minutes',
+        'hostInfo or scriptUrl not set up'
+            => 'hostInfo ou scriptUrl n\'est pas configuré',
+        'How often should I run each Job?'
+            => 'Fréquence d\'exécution de tâches: recommendations',
+        'How to Setup the Jobs to Run Automatically'
+            => 'Comment démarrer les jobs automatiquement',
+        'In Process (Stuck)'
+            => 'En cours (échoué)',
+        'In Process [Started: {startedDateTime}]'
+            => 'En cours [commencé: {startedDateTime}]',
+        'Is Processed'
+            => 'IsProcessed',
+        'Is Processed must be set as true or false, not null.'
+            => 'L\'attribut IsProcessed ne peut être nul.',
+        'Job completed with errors.'
+            => 'Erreurs lors de l\'exécution du job.',
+        'Job Log'
+            => 'Fichier log',
+        'Job Log for {jobDisplayName}'
+            => 'Fichier log pour {jobDisplayName}',
+        'Job Name'
+            => 'Nom du Job',
+        'Jobs Manager'
+            => 'Tâches système',
+        'Jobs Manager: Home'
+            => 'Tâches système: Home',
+        'Last Completed Run'
+            => 'Dernière exécution',
+        'Manage Scheduled Jobs'
+            => 'Gestion des tâches système',
+        'Message'
+            => 'Message',
+        'Monitor Job'
+            => 'Suivi des tâches système',
+        'Not Running'
+            => 'Inactif',
+        'Recommended Frequency'
+            => 'Fréquence recommandée',
+        'Reset'
+            => 'Relance',
+        'Return to Job Manager'
+            => 'Retour aux Tâches système',
+        'Scheduled jobs are stuck'
+            => 'Les tâches planifiées ont échouées.',
+        'Script will run at most for {seconds} seconds.'
+            => 'Éxécution maximum du script: {seconds} secondes.',
+        'Start Date Time'
+            => 'Date/heure commencé',
+        'The job {jobName} has been reset.'
+            => 'Le job {jobName} a été relancé.',
+        'The job {jobName} was not found to be stuck and therefore was not reset.'
+            => 'Le job {jobName} n\'a pas échoué et n\'a pas été relancé.',
+        'The monitor job is stuck.'
+            => 'Le suivi des tâches système a échoué.',
+        'The Monitor Job runs constantly making sure all jobs are running properly.'
+            => 'Le suivi des tâches système vérifie que les processus fonctionnent correctement.',
+        'The system has detected there are jobs that are stuck.'
+            => 'Le système a détecté que des jobs ont échoués.',
+        'What is the Monitor Job?'
+            => 'Qu\'est ce le suivi des tâches système?',
+        '{ClickHereLink} for help on setting up a cron in Linux or a scheduled task in Windows'
+            => '{ClickHereLink} pour des informations comment installer le cron en Linux ou une tâche planifiée en Windows',
+        '{dateTimeString} Ending job type: {type}'
+            => '{dateTimeString} Annulation du job: {type}',
+        '{dateTimeString} Starting job type: {type}'
+            => '{dateTimeString} Lancement du job: {type}',
+    );
+?>
