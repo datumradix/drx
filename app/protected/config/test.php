20cd57d735942b9ff9bd50832760760debf8a734
Jason <Jason@BERN>
2012-02-23 20:33:51 -0600

    language labels

---
+++ app/protected/config/test.php
@@ -0,0 +1,166 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+    if (!defined('IS_TEST'))
+    {
+        define('IS_TEST', true);
+    }
+    $common_config = CMap::mergeArray(
+        require('main.php'),
+        array(
+            'import' => array(
+                'application.extensions.zurmoinc.framework.adapters.*',
+                'application.extensions.zurmoinc.framework.adapters.columns.*',
+                'application.extensions.zurmoinc.framework.adapters.dataproviders.*',
+                'application.extensions.zurmoinc.framework.configuration.*',
+                'application.extensions.zurmoinc.framework.components.*',
+                'application.extensions.zurmoinc.framework.controllers.*',
+                'application.extensions.zurmoinc.framework.dataproviders.*',
+                'application.extensions.zurmoinc.framework.elements.*',
+                'application.extensions.zurmoinc.framework.elements.actions.*',
+                'application.extensions.zurmoinc.framework.elements.derived.*',
+                'application.extensions.zurmoinc.framework.exceptions.*',
+                'application.extensions.zurmoinc.framework.forms.*',
+                'application.extensions.zurmoinc.framework.interfaces.*',
+                'application.extensions.zurmoinc.framework.models.*',
+                'application.extensions.zurmoinc.framework.models.validators.*',
+                'application.extensions.zurmoinc.framework.modules.*',
+                'application.extensions.zurmoinc.framework.portlets.*',
+                'application.extensions.zurmoinc.framework.portlets.rules.*',
+                'application.extensions.zurmoinc.framework.rules.*',
+                'application.extensions.zurmoinc.framework.utils.*',
+                'application.extensions.zurmoinc.framework.validators.*',
+                'application.extensions.zurmoinc.framework.views.*',
+                'application.extensions.zurmoinc.framework.widgets.*',
+                'application.extensions.zurmoinc.framework.tests.common.*',
+                'application.extensions.zurmoinc.framework.tests.unit.*',
+                'application.extensions.zurmoinc.framework.tests.unit.components.*',
+                'application.extensions.zurmoinc.framework.tests.unit.forms.*',
+                'application.extensions.zurmoinc.framework.tests.unit.models.*',
+                'application.extensions.zurmoinc.framework.tests.unit.modules.*',
+                'application.extensions.zurmoinc.framework.tests.unit.views.*',
+                'application.modules.zurmo.tests.components.*',
+                'application.tests.unit.*',
+////////////////////////////////////////////////////////////////////////////////
+// Temporary - See Readme.txt in the app/protected/tests/unit/notSupposedToBeHere directory.
+                'application.tests.unit.notSupposedToBeHere.*',
+////////////////////////////////////////////////////////////////////////////////
+            ),
+            'components' => array(
+                'fixture' => array(
+                    'class' => 'system.test.CDbFixtureManager',
+                ),
+            ),
+            'components' => array(
+                'user' => array(
+                    'class' => 'TestWebUser',
+                ),
+            ),
+        )
+    );
+
+    // THIS IS LIKELY TO BE A PERFORMANCE ISSUE, SEARCHING SO MANY DIRECTORIES. TO BE INVESTIGATED.
+    // Add aliases here that are likely to be useful in any module.
+    foreach ($common_config['modules'] as $index => $moduleName)
+    {
+        //This is to handle nested modules in the config above.
+        if (is_array($moduleName))
+        {
+            $moduleName = $index;
+        }
+        $common_config['import'][] = "application.modules.$moduleName.*";                           // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.adapters.*";                  // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.adapters.columns.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.dataproviders.*";             // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.elements.*";                  // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.elements.actions.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.elements.actions.security.*"; // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.elements.derived.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.components.*";                // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.controllers.*";               // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.controllers.filters.*";       // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.exceptions.*";                // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.forms.*";                     // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.forms.attributes.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.interfaces.*";                // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.jobs.*";                      // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.models.*";                    // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.modules.*";                   // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.rules.*";                     // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.rules.attributes.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.rules.policies.*";            // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.tests.unit.*";                // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.tests.unit.controllers.*";    // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.tests.unit.files.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.tests.unit.models.*";         // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.tests.unit.walkthrough.*";    // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.utils.*";                     // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.utils.charts.*";              // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.utils.sanitizers.*";          // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.utils.security.*";            // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.utils.analyzers.*";           // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.validators.*";                // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.views.*";                     // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.views.attributetypes.*";      // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.views.charts.*";              // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.views.related.*";             // Not Coding Standard
+        $common_config['import'][] = "application.modules.$moduleName.widgets.*";                   // Not Coding Standard
+    }
+
+    // Add aliases here that are likely to only be specific to a particular module.
+    $common_config['import'][] = "application.modules.designer.rules.*";                            // Not Coding Standard
+    $common_config['import'][] = "application.modules.designer.rules.elements.*";                   // Not Coding Standard
+    $common_config['import'][] = "application.modules.designer.elements.layoutsettings.*";          // Not Coding Standard
+    $common_config['import'][] = "application.modules.designer.forms.attributes.*";                 // Not Coding Standard
+    $common_config['import'][] = "application.modules.install.serviceHelpers.*";                    // Not Coding Standard
+    $common_config['import'][] = "application.modules.zurmo.elements.security.*";                   // Not Coding Standard
+    $common_config['import'][] = "application.modules.zurmo.utils.security.*";                      // Not Coding Standard
+    $common_config['import'][] = "application.modules.zurmo.views.currency.*";                      // Not Coding Standard
+    $common_config['import'][] = "application.modules.zurmo.views.language.*";                      // Not Coding Standard
+    $common_config['import'][] = "application.modules.zurmo.views.security.*";                      // Not Coding Standard
+    $common_config['import'][] = "application.modules.api.adapters.api.*";                          // Not Coding Standard
+    $common_config['import'][] = "application.modules.api.tests.unit.forms.*";                      // Not Coding Standard
+
+    //override and use test specific begin behavior
+    $common_config['behaviors']['onBeginRequest'] = array(
+        'class' => 'application.tests.BeginRequestTestBehavior'
+    );
+    //override and use INSTANCE_ROOT for handling paths during testing.
+    $common_config['components']['assetManager']['baseUrl'] = INSTANCE_ROOT . DIRECTORY_SEPARATOR . 'assets/';
+    $common_config['behaviors']['onEndRequest']['class'] = 'application.tests.EndRequestTestBehavior';
+    //breaks WalkthroughDocumentationTests so disabling Csrf and Cookie validation and use different class
+    $common_config['components']['clientScript']['class']             = 'application.tests.ClientScriptForTesting';
+    $common_config['components']['request']['class']                  = 'application.tests.HttpRequestForTesting';
+    $common_config['components']['request']['enableCsrfValidation']   = false; //todo: get this working, since for production this is true.
+    $common_config['components']['request']['enableCookieValidation'] = false;
+    $common_config['components']['emailHelper']['class']              = 'application.tests.EmailHelperForTesting';
+    //Set the GeoCodeApiKey to null which will work for localhost requests. If this is not running on
+    //localhost, then modify perInstanceConfig.php with an updated key.
+    if (!isset($common_config['params']['testGoogleGeoCodeApiKey']))
+    {
+        $common_config['params']['testGoogleGeoCodeApiKey'] = null;
+    }
+    return $common_config;
+?>
---
+++ app/protected/extensions/zurmoinc/framework/messages/de/Default.php
@@ -0,0 +1,287 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        '(None)'
+            => '(keine)',
+        'Add File'
+            => 'Datei hinzufügen',
+        'Add Files'
+            => 'Dateien hinzufügen',
+        'Add Item'
+            => 'Element hinzufügen',
+        'Advanced Search'
+            => 'Erweiterte Suche',
+        'After'
+            => 'Nach',
+        'All'
+            => 'Alle',
+        'and'
+            => 'und',
+        'Basic Search'
+            => 'Einfache Suche',
+        'Before'
+            => 'Vor',
+        'Between'
+            => 'zwischen',
+        'Bulk insert failed. There was a row with an incorrect column quantity'
+            => 'Fehler bei der BULK INSERT-Operation. Eine Zeile enthält eine Reihe von schlechten Säulen',
+        'by'
+            => 'Von',
+        'Cancel'
+            => 'Abbrechen',
+        'Cell Settings'
+            => 'Zelle Einstellungen',
+        'Celsius'
+            => 'Celsius',
+        'Change Log'
+            => 'Änderungsprotokoll',
+        'Changed'
+            => 'Geändert',
+        'Clear Search'
+            => 'Suche löschen',
+        'Create'
+            => 'Erstellen',
+        'Created By User'
+            => 'Erstellt von Benutzer',
+        'Created Date and User'
+            => 'Erstellt Datum und Benutzer',
+        'Created Date Time'
+            => 'Erstellt Datum Uhrzeit',
+        'Created On'
+            => 'Erstellt am',
+        'CustomManagement class needs to be extended.'
+            => 'CustomManagement Klasse muss erweitert werden',
+        'Date'
+            => 'Datum',
+        'Date Time'
+            => 'Datum Uhrzeit',
+        'Default Value'
+            => 'Ausgangswert',
+        'Delete'
+            => 'Löschen',
+        'Detail View Only'
+            => 'Nur Detailansicht',
+        'Details'
+            => 'Details',
+        'Done'
+            => 'Fertig',
+        'Edit'
+            => 'Editieren',
+        'Edit Portlet'
+            => 'Portlet bearbeiten',
+        'Error'
+            => 'Fehler',
+        'Fahrenheit'
+            => 'Fahrenheit',
+        'Filtered Lists'
+            => 'Gefilterte Listen',
+        'firstDateTime must occur after secondDateTime'
+            => 'firstDateTime muss nach secondDateTime kommen',
+        'firstDateTime must occur before secondDateTime'
+            => 'firstDateTime muss vor secondDateTime kommen',
+        'First character must be a lower case letter'
+            => 'Das erste Zeichen muss Kleinbuchstabe sein.',
+        'First select the {attributeLabel}'
+            => 'Wählen Sie zuerst das {attributeLabel}',
+        'Float'
+            => 'Float',
+        'from'
+            => 'von',
+        'Full Name'
+            => 'voller Name',
+        'Go to Details'
+            => 'Zur Detailansicht',
+        'Hour'
+            => 'Stunde',
+        'Info'
+            => 'Info',
+        'Inline Edit View'
+            => 'Inline Edit Ansicht',
+        'Integer'
+            => 'Ganzzahl',
+        'Invalid'
+            => 'Ungültig',
+        'Invalid currency code'
+            => 'Ungültige Währungscode',
+        'Last 7 Days'
+            => 'Letzte 7 Tage',
+        'Last Modified'
+            => 'Letzte Änderung',
+        'Last Modified Date and User'
+            => 'Letzte Änderung Datum und benutzer ',
+        'List'
+            => 'Liste',
+        'Loading'
+            => 'Loading',
+        'Mass Update'
+            => 'Massenaktualisierung',
+        'Max upload size: {maxSize}'
+            => 'Max Upload-Größe: {maxSize}',
+        'Minute'
+            => 'Minute',
+        'Missing the attribute labels.'
+            => 'Attribut-Tags fehlt',
+        'Model Class Name'
+            => 'Modell Klassenname',
+        'Model Id'
+            => 'Modell Id',
+        'Modified By User'
+            => 'Geändert von Benutzer',
+        'Modified Date Time'
+            => 'Geändert Datum Uhrzeit',
+        'Module Name'
+            => 'Modulname',
+        'More Details'
+            => 'Weitere Details',
+        'More Options'
+            => 'Weitere Optionen',
+        'My List View'
+            => 'Meine Liste Ansicht',
+        'Name'
+            => 'Name',
+        'Name must not contain spaces or special characters'
+            => 'Der Name darf keine Sonderzeichen oder Leerzeichen haben',
+        'Next 7 Days'
+            => 'Nächste 7 Tage',
+        'No'
+            => 'Nein',
+        'No import processes found.'
+            => 'Kein Import-Prozesse gefunden',
+        'None'
+            => 'Keiner',
+        'Now'
+            => 'Jetzt',
+        'of'
+            => 'von',
+        'On'
+            => 'Am',
+        'Opted Out'
+            => 'Opted Out',
+        'or'
+            => 'oder',
+        'Panel'
+            => 'Panel',
+        'Panel Settings'
+            => 'Panel Einstellungen',
+        'Panel Title'
+            => 'Panel-Titel',
+        'Perform Action'
+            => 'Aktion ausführen',
+        'Radio Drop Down'
+            => 'Radio Drop Down',
+        'record'
+            => 'Datensatz',
+        'records'
+            => 'Datensätze',
+        'Remove'
+            => 'Entfernen',
+        'Return to List'
+            => 'Zurück zur Liste',
+        'Row'
+            => 'Reihe',
+        'Save'
+            => 'Speichern',
+        'Search'
+            => 'Suchen',
+        'Second'
+            => 'Zweite',
+        'Select'
+            => 'Wählen',
+        'selected for updating.'
+            => 'Ausgewählt zur Aktualisierung',
+        'Serialized Data'
+            => 'Serialisierte Daten',
+        'Some values cannot be removed because they are currently in use. ' .
+        'Try changing the records that use them first.'
+            => 'Einige Werte können nicht entfernt werden, da sie gerade in Gebrauch sind'.
+               'Ändern Sie die Datensätze, die sie zum ersten Mal verwenden',
+        'Start'
+            => 'Anfang',
+        'Tab'
+            => 'Tab',
+        'The file did not exist'
+            => 'Diese Datei existierte nicht',
+        'There was an error uploading the file.'
+            => 'Es gab einen Fehler beim Upload der Datei.',
+        'There was an error with the web service.'
+            => 'Es gab einen Fehler mit dem Web-Service.',
+        'This module doesn\'t have anything to configure.'
+            => 'Dieses Modul hat nichts zu konfigurieren.',
+        'This pick-list is used by more than one module.'
+            => 'Diese Pick-Liste ist nach mehr als einem Modul verwendet.',
+        'This portlet will be removed, ok?'
+            => 'Dieses Portlet werden entfernt, ok?',
+        'Time'
+            => 'Uhrzeit',
+        'Title/First/LastName'
+            => 'Titel/Vorname/Nachname',
+        'to'
+            => 'zu',
+        'Today'
+            => 'Heute',
+        'Tomorrow'
+            => 'Morgen',
+        'total'
+            => 'gesamt',
+        'Type'
+            => 'Typ',
+        'Type to find a tag'
+            => 'Schreiben, um ein tab zu finden',
+        'Unknown'
+            => 'unbekannt',
+        'Update Selected'
+            => 'Aktualisierung ausgewählt',
+        'updated successfully.'
+            => 'erfolgreich aktualisiert.',
+        'Updating'
+            => 'Aktualisierung',
+        'User'
+            => 'Benutzer',
+        'Users'
+            => 'Benutzern',
+        'Yes'
+            => 'Ja',
+        'Yesterday'
+            => 'Gestern',
+        'You must enter a value'
+            => 'Sie müssen einen Wert eingeben',
+        'You must select at least one field to modify.'
+            => 'Wählen Sie mindestens ein Feld zu ändern.',
+        'You must select at least one record'
+            => 'Wählen Sie mindestens einen Datensatz',
+        'You must specify a value.'
+            => 'Geben Sie einen Wert',
+        '{attribute} "{value}" is already in use.'
+            => '{attribute} "{value}" ist bereits im Einsatz.',
+        '{attribute} is too precise (maximum decimal places is {precision}).'
+            => '{attribute} ist zu genau (maximum decimal places is {precision})',
+    );
+?>
