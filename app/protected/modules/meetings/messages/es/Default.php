39589ffbe4cf11cff009d17a2b72aedcab0d6328
Amit_Ashckenazi <obviocity@gmail.com>
2012-03-29 19:44:39 -0300

    attendees/token input styling

---
+++ app/protected/modules/meetings/messages/es/Default.php
@@ -0,0 +1,73 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access MeetingsModulePluralLabel'
+            => 'Acceso a MeetingsModulePluralLabel',
+        'Attendees'
+            => 'Asistentes',
+        'Call'
+            => 'Llamada',
+        'Category'
+            => 'Categoría',
+        'Create MeetingsModulePluralLabel'
+            => 'Crear MeetingsModulePluralLabel',
+        'Delete MeetingsModulePluralLabel'
+            => 'Eliminar MeetingsModulePluralLabel',
+        'End Time'
+            => 'Hora de finalización',
+        'Location'
+            => 'Lugar',
+        'Meeting'
+            => 'Reunión',
+        'Meetings'
+            => 'Reuniones',
+        'MeetingsModulePluralLabel'
+            => 'MeetingsModulePluralLabel',
+        'MeetingsModulePluralLabel For AccountsModuleSingularLabel'
+            => 'MeetingsModulePluralLabel para AccountsModuleSingularLabel',
+        'MeetingsModulePluralLabel For ContactsModuleSingularLabel'
+            => 'MeetingsModulePluralLabel para ContactsModuleSingularLabel',
+        'MeetingsModulePluralLabel For OpportunitiesModuleSingularLabel'
+            => 'MeetingsModulePluralLabel para OpportunitiesModuleSingularLabel',
+        'MeetingsModuleSingularLabel'
+            => 'MeetingsModuleSingularLabel',
+        'My Upcoming MeetingsModulePluralLabel'
+            => 'Mis próximas MeetingsModulePluralLabel',
+        'Related ContactsModulePluralLabel and LeadsModulePluralLabel'
+            => 'Related ContactsModulePluralLabel y LeadsModulePluralLabel',
+        'Start Time'
+            => 'Hora de inicio',
+        'Type a ContactsModuleSingularLowerCaseLabel or LeadsModuleSingularLowerCaseLabel: name or email address'
+            => 'Escribir un ContactsModuleSingularLowerCaseLabel o LeadsModuleSingularLowerCaseLabel: nombre o dirección de email',
+        'Upcoming MeetingsModulePluralLabel'
+            => 'Próximos MeetingsModulePluralLabel',
+    );
+?>
