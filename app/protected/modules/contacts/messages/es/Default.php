1a72bdf8bc3ca59d80fe097ee939357a0a006c44
digimagic <digimagic@localhost>
2012-05-07 13:11:53 -0500

    Merge with default

---
+++ app/protected/modules/contacts/messages/es/Default.php
@@ -0,0 +1,85 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access ContactsModulePluralLabel Tab'
+            => 'Acceso a ContactsModulePluralLabel',
+        'Company Name'
+            => 'Nombre de la empresa',
+        'Contact'
+            => 'Contacto',
+        'Contact Stage'
+            => 'Estatus de contacto',
+        'Contact Status Translated Labels'
+            => 'Etiquetas traducidas de estatus de contacto',
+        'Contact Statuses'
+            => 'Estatus de contacto',
+        'Contacts'
+            => 'Contactos',
+        'ContactsModulePluralLabel'
+            => 'ContactsModulePluralLabel',
+        'ContactsModulePluralLabel For AccountsModuleSingularLabel'
+            => 'ContactsModulePluralLabel para AccountsModuleSingularLabel',
+        'ContactsModulePluralLabel For OpportunitiesModuleSingularLabel'
+            => 'ContactsModulePluralLabel para OpportunitiesModuleSingularLabel',
+        'ContactsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel',
+        'ContactsModuleSingularLabel or LeadsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel o LeadsModuleSingularLabel',
+        'ContactsModuleSingularLabel Search'
+            => 'ContactsModuleSingularLabel búsqueada',
+        'Create ContactsModulePluralLabel'
+            => 'Crear ContactsModulePluralLabel',
+        'Create ContactsModuleSingularLabel'
+            => 'Crear una ContactsModuleSingularLabel',
+        'Delete ContactsModulePluralLabel'
+            => 'Eliminar ContactsModulePluralLabel',
+        'Each ContactsModuleSingularLowerCaseLabel state must be uniquely named'
+            => 'Cada ContactsModuleSingularLowerCaseLabel debe ser un nombre exclusivo',
+        'My ContactsModulePluralLabel'
+            => 'Mis ContactsModulePluralLabel',
+        'Order'
+            => 'Orden',
+        'Serialized Labels'
+            => 'etiquetas serializadas',
+        'Starting Status'
+            => 'Estado de partida',
+        'The contact stage field'
+            => 'El campo de estatus del contacto',
+        'The default status specified does not exist.'
+            => 'El estado especificado predeterminado no existe.',
+        'The status is required.  Neither a value nor a default was specified.'
+            => 'El estado es necesario. Ni un valor ni un predeterminado se ha especificado.',
+        'The status specified does not exist.'
+            => 'El estado especificado no existe.',
+        'The status specified is not unique and is invalid.'
+            => 'El estado especificado no es único y es invalido.',
+    );
+?>
