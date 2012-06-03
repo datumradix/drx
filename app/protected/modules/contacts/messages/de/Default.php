e0195d414756eb957f831dd5522c3e74fb4b93c9
Nilesh Patkar <bitbucket@antechnologies.co.in>
2012-06-03 18:18:25 +0530

    This branch is for the installation selenium test case.

---
+++ app/protected/modules/contacts/messages/de/Default.php
@@ -0,0 +1,85 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Access ContactsModulePluralLabel Tab'
+            => 'Zu ContactsModulePluralLabel Tab',
+        'Company Name'
+            => 'Firmenname',
+        'Contact'
+            => 'Kontakt',
+        'Contact Stage'
+            => 'Kontaktphase',
+        'Contact Status Translated Labels'
+            => 'Kontakt Status Übersetzte Labels',
+        'Contact Statuses'
+            => 'Kontakt Status',
+        'Contacts'
+            => 'Kontakte',
+        'ContactsModulePluralLabel'
+            => 'ContactsModulePluralLabel', // Same Word Translated
+        'ContactsModulePluralLabel For AccountsModuleSingularLabel'
+            => 'ContactsModulePluralLabel für AccountsModuleSingularLabel',
+        'ContactsModulePluralLabel For OpportunitiesModuleSingularLabel'
+            => 'ContactsModulePluralLabel für OpportunitiesModuleSingularLabel',
+        'ContactsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel', // Same Word Translated
+        'ContactsModuleSingularLabel or LeadsModuleSingularLabel'
+            => 'ContactsModuleSingularLabel oder LeadsModuleSingularLabel',
+        'ContactsModuleSingularLabel Search'
+            => 'ContactsModuleSingularLabel Search', // Same Word Translated
+        'Create ContactsModulePluralLabel'
+            => 'Erstellen ContactsModulePluralLabel',
+        'Create ContactsModuleSingularLabel'
+            => 'Erstellen ContactsModuleSingularLabel',
+        'Delete ContactsModulePluralLabel'
+            => 'Löschen ContactsModulePluralLabel',
+        'Each ContactsModuleSingularLowerCaseLabel state must be uniquely named'
+            => 'Jeder ContactsModuleSingularLowerCaseLabel Zustand muss eindeutig benannt werden',
+        'My ContactsModulePluralLabel'
+            => 'Meine ContactsModulePluralLabel',
+        'Order'
+            => 'Reihenfolge',
+        'Serialized Labels'
+            => 'Etikettenserien',
+        'Starting Status'
+            => 'Status am Anfang',
+        'The contact stage field'
+            => 'Kontakt Phase Feld',
+        'The default status specified does not exist.'
+            => 'Die Default-Status existiert nicht.',
+        'The status is required.  Neither a value nor a default was specified.'
+            => 'Status ist ein Mussfeld. Weder ein Wert noch ein Standard festgelegt wurde.',
+        'The status specified does not exist.'
+            => 'Die angegebenen Status existiert nicht',
+        'The status specified is not unique and is invalid.'
+            => 'Die angegebenen Status ist nicht eindeutig und ist ungültig',
+    );
+?>
