27c6bf6989179ad3783036754350fd2dde4b0810
Nilesh Patkar <bitbucket@antechnologies.co.in>
2012-06-26 19:06:26 +0530

    This commit impliments fixes to AccountsTestSuite.

---
+++ app/protected/modules/zurmo/data/PersonRandomData.php
@@ -0,0 +1,108 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * Address related array of random seed data parts.
+     */
+    return array(
+        'jobTitlesAndDepartments' =>  array(
+            array('Vice President', 'Sales'),
+            array('Sales Director', 'Sales'),
+            array('Sales Manager',  'Sales'),
+            array('Vice President', 'Information Technology'),
+            array('IT Director',    'Information Technology'),
+            array('IT Manager',     'Information Technology'),
+            ),
+        'maleFirstNames'     => array(
+            'Jason',
+            'Ray',
+            'Jeffrey',
+            'Kirby',
+            'Michael',
+            'Raymond',
+            'Nev',
+            'Alex',
+            'Jake',
+            'Jose',
+            'Ignacio',
+            'David',
+            'Stafford',
+            'Walter'
+        ),
+        'femaleFirstNames'    => array(
+            'Katie',
+            'Gail',
+            'Laura',
+            'Rosina',
+            'Alice',
+            'Maya',
+            'Sophie',
+            'Lisa',
+            'Sarah',
+            'Ruth',
+            'Ester',
+            'Betty'
+        ),
+        'maleTitles'    => array(
+            'Mr.',
+            'Dr.'
+        ),
+        'femaleTitles'    => array(
+            'Mrs.',
+            'Ms.',
+            'Dr.'
+        ),
+        'lastNames' => array(
+            'Johnson',
+            'Williams',
+            'Jones',
+            'Green',
+            'Brown',
+            'Davis',
+            'Miller',
+            'Wilson',
+            'Moore',
+            'Taylor',
+            'Anderson',
+            'Thomas',
+            'Jackson',
+            'White',
+            'Harris',
+            'Martin',
+            'Thompson',
+            'Garcia',
+            'Martinez',
+            'Robinson',
+            'Clark',
+            'Rodriguez',
+            'Lewis',
+            'Lee',
+            'Walker',
+            'Hall',
+            'Allen',
+        )
+    );
+?>
