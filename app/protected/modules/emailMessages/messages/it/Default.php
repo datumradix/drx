387e450f6ba900f8e578eb68d51d3373d0b72ff2
digimagic <digimagic@localhost>
2012-07-25 20:01:07 -0500

    Fixing span highlight for sticky search

---
+++ app/protected/modules/emailMessages/messages/it/Default.php
@@ -0,0 +1,161 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A test email address must be entered before you can send a test email.'
+            => 'Si prega di inserire un indirizzo e-mail per inviare una email di prova.',
+        'A test email from Zurmo'
+            => 'Un messaggio di prova da Zurmo',
+        'A test text message from Zurmo.'
+            => 'Un messaggio SMS di prova da Zurmo.',
+        'Access Email Configuration'
+            => 'Accesso alla configurazione di mail',
+        'Access Emails Tab'
+            => 'Accesso ai Emails Tab',
+        'Archived'
+            => 'Archiviate',
+        'Archived Unmatched'
+            => 'Archiviate non trovate',
+        'Body'
+            => 'Corpo',
+        'Cc'
+            => 'Cc', // Same Word Translated
+        'Cc Recipients'
+            => 'Destinatari Cc',
+        'Clear Old Sent Notifications Email Job'
+            => 'Cancellare le vecchie notificazioni inviati',
+        'Could not connect to IMAP server.'
+            => 'Impossibile connettersi al server IMAP.',
+        'Create Emails'
+            => 'Creare Emails',
+        'Delete Emails'
+            => 'Cancellare Emails',
+        'Draft'
+            => 'Bozza',
+        'Email address does not exist in system.'
+            => 'L\'indirizzo e-mail non esiste.',
+        'Email Archiving Configuration (IMAP)'
+            => 'Connessione IMAP per archiviare mail',
+        'Email Configuration'
+            => 'Configurazione di mail',
+        'Email configuration saved successfully.'
+            => 'Configurazione salvata con successo.',
+        'Emails'
+            => 'Emails', // Same Word Translated
+        'Error Code:'
+            => 'Codici d\'errore',
+        'Error Message:'
+            => 'Messaggio d\'errore',
+        'Every 1 minute.'
+            => 'Ogni minuto',
+        'Extra Mail Settings'
+            => 'Impostazioni avanzate',
+        'Folder'
+            => 'Cartella',
+        'From'
+            => 'Da',
+        'From Address'
+            => 'Indirizzo email mittente',
+        'From Name'
+            => 'Nome mittente',
+        'from: {senderContent}'
+            => 'da: {senderContent}',
+        'Host'
+            => 'Host', // Same Word Translated
+        'Html Content'
+            => 'Contenuto HTML',
+        'Inbox'
+            => 'Posta in arrivo',
+        'Invalid email address.'
+            => 'Indirizzo email non valido.',
+        'Manage Email Configuration'
+            => 'Gestione email',
+        'Message failed to send'
+            => 'Messaggio non inviato',
+        'Message successfully sent'
+            => 'Messaggio inviato con successo',
+        'Missing Rights'
+            => 'Diritti mancanti',
+        'Outbound Email Configuration (SMTP)'
+            => 'Server di posta in uscita (SMTP)',
+        'Outbound email configuration saved successfully.'
+            => 'Server di posta in uscita (SMTP) salvato.',
+        'Outbox'
+            => 'Posta in uscita',
+        'Outbox Error'
+            => 'Errore con la posta in uscita',
+        'Person'
+            => 'Persona',
+        'Port'
+            => 'Porta',
+        'Process Inbound Email Job'
+            => 'Elabora mail in entrata',
+        'Process Outbound Email Job'
+            => 'Inizia il processo de posta in uscita',
+        'Response from Server'
+            => 'Risposta del server',
+        'Send a test email to'
+            => 'Inviare una prova di email a ',
+        'Send system notifications from'
+            => 'Invia notificazioni dall\'utente',
+        'Send Test Email'
+            => 'Invia email di test',
+        'Sent'
+            => 'Inviata',
+        'SSL connection'
+            => 'Connessione protetta (SSL)',
+        'Successfully connected to IMAP server.'
+            => 'Connesso al server IMAP con successo.',
+        'Test Connection'
+            => 'Prova di connessione',
+        'Test IMAP connection'
+            => 'Prova di connessione IMAP',
+        'Test Message Results'
+            => 'Risultati del messaggio di prova',
+        'Testing Outbound Email Connection Job'
+            => 'Prova Server di posta in uscita (SMTP)',
+        'Text Content'
+            => 'Messaggio',
+        'To'
+            => 'A',
+        'To Address'
+            => 'Indirizzo Destinatario',
+        'To Name'
+            => 'Nome Destinatario',
+        'To Recipients'
+            => 'Ai Destinatari',
+        'to: {recipientContent}'
+            => 'a: {recipientContent}',
+        'You do not have rights to access, create, or connect emails in the system.'
+            => 'Non si dispone di diritti di accesso, creare o collegare e-mails nel sistema.',
+        'Zurmo sends out system notifications.  The notifications must appear as coming from a super administrative user.'
+            => 'Notificazioni del sistema.  Le notificazioni devono mandare di un utente super admin.',
+    );
+?>
