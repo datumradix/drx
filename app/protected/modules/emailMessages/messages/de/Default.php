497d45d84ce1629b79d1171a94e7e37978714491
ivica <ivica.nedeljkovic@gmail.com>
2012-08-01 16:25:10 +0200

    Fixing bug in conversations demo data

---
+++ app/protected/modules/emailMessages/messages/de/Default.php
@@ -0,0 +1,163 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A test email address must be entered before you can send a test email.'
+            => 'Bitte geben Sie eine E-Mail-Adresse.',
+        'A test email from Zurmo'
+            => 'Eine Testnachricht von Zurmo',
+        'A test text message from Zurmo.'
+            => 'Eine Textnachricht von Zurmo.',
+        'Access Email Configuration'
+            => 'Zugriff auf die E-Mail Konfiguration',
+        'Access Emails Tab'
+            => 'Zugang zu Emails',
+        'Archived'
+            => 'Archiviert',
+        'Archived Unmatched'
+            => 'Archivierte Artikel Unvergleichliche',
+        'Body'
+            => 'Körper',
+        'Cc'
+            => 'Cc', // Same Word Translated
+        'Cc Recipients'
+            => 'CC-Empfänger',
+        'Clear Old Sent Notifications Email Job'
+            => 'Löschen die alten Benachrichtigungen',
+        'Could not connect to IMAP server.'
+            => 'IMAP-Konto fehlgeschlagen.',
+        'Create Emails'
+            => 'Emails erstellen',
+        'Delete Emails'
+            => 'Emails entfernen',
+        'Draft'
+            => 'Entwurf',
+        'Email address does not exist in system.'
+            => 'Die E-Mail-Adresse existiert nicht.',
+        'Email Archiving Configuration (IMAP)'
+            => 'Konto-Einstellungen für archivierte Mails (IMAP)',
+        'Email Configuration'
+            => 'E-Mail Parametern',
+        'Email configuration saved successfully.'
+            => 'E-Mail-Konfiguration wurde erfolgreich gespeichert.',
+        'Emails'
+            => 'Emails', // Same Word Translated
+        'Error Code:'
+            => 'Fehlercode:',
+        'Error Message:'
+            => 'Fehlermeldung:',
+        'Every 1 minute.'
+            => 'Jede Minute.',
+        'Extra Mail Settings'
+            => 'Erweiterte Einstellungen',
+        'Folder'
+            => 'Ordner',
+        'From'
+            => 'Von',
+        'From Address'
+            => 'Adresse des Absenders',
+        'From Name'
+            => 'Absender Name',
+        'from: {senderContent}'
+            => 'von: {senderContent}',
+        'Host'
+            => 'Hostname',
+        'Html Content'
+            => 'HTML-Inhalt',
+        'Inbox'
+            => 'Posteingang',
+        'Invalid email address'
+            => 'Ungültige E-Mail-Adresse',
+        'Manage Email Configuration'
+            => 'E-Mail Konfiguration',
+        'Message failed to send'
+            => 'Nachricht nicht gesendet',
+        'Message successfully sent'
+            => 'Nachricht erfolgreich gesendet',
+        'Missing Rights'
+            => 'Fehlende Rechte',
+        'Outbound Email Configuration (SMTP)'
+            => 'Postausgangsserver (SMTP)',
+        'Outbound email configuration saved successfully.'
+            => 'Postausgangsserver (SMTP) wurde erfolgreich gespeichert.',
+        'Outbox'
+            => 'Postausgang',
+        'Outbox Error'
+            => 'Postausgang Fehler ',
+        'Person'
+            => 'Name',
+        'Person Or Account'
+            => 'Person Or Account', // Same Word Translated
+        'Port'
+            => 'Port', // Same Word Translated
+        'Process Inbound Email Job'
+            => 'Inbound Email Job verfahren',
+        'Process Outbound Email Job'
+            => 'Ausgehende E-Mail Prozess starten',
+        'Response from Server'
+            => 'Server Response',
+        'Send a test email to'
+            => 'Senden eine Testnachricht  zu',
+        'Send system notifications from'
+            => 'Senden System-Meldungen von',
+        'Send Test Email'
+            => 'Eine Testnachricht senden',
+        'Sent'
+            => 'Nachricht wurde gesendet.',
+        'SSL connection'
+            => 'SSL-Verbindung',
+        'Successfully connected to IMAP server.'
+            => 'Erfolgreich auf IMAP-Server verbunden.',
+        'Test Connection'
+            => 'Verbindung prüfen',
+        'Test IMAP connection'
+            => 'IMAP-Verbindung prüfen',
+        'Test Message Results'
+            => 'Ergebnisse der Testnachricht',
+        'Testing Outbound Email Connection Job'
+            => 'Prüfparameter für die E-Mail Konto',
+        'Text Content'
+            => 'Nachricht',
+        'To'
+            => 'An',
+        'To Address'
+            => 'Zieladresse',
+        'To Name'
+            => 'Name des Empfängers',
+        'To Recipients'
+            => 'An Empfänger',
+        'to: {recipientContent}'
+            => 'an: {recipientContent}',
+        'You do not have rights to access, create, or connect emails in the system.'
+            => 'Du hast keine Rechte auf Zugang, erstellen oder zu verknüpfen E-Mails in das System.',
+        'Zurmo sends out system notifications.  The notifications must appear as coming from a super administrative user.'
+            => 'Zurmo sendet System Benachrichtigungen. Die Benachrichtigungen müssen von einem Super-administrativen Benutzerrechten angezeigt werden.',
+    );
+?>
