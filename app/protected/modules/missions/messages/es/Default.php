f25d1376d71abb5b49466480ad2dbd93b8e1bfc3
Laura <Laura@lolo>
2012-10-23 09:36:10 -0500

    language work

---
+++ app/protected/modules/missions/messages/es/Default.php
@@ -0,0 +1,85 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+		'A mission status has changed'
+            => '??',
+		'A mission you completed has been accepted'
+            => '??',
+		'A mission you completed has been rejected'
+            => '??',
+		'A mission you created has been completed'
+            => '??',
+		'A mission you created has been taken on by {takenByUserName}'
+            => '??',
+		'Accept'
+            => '??',
+		'Accepted'
+            => '??',
+		'Access Missions Tab'
+            => '??',
+		'At least one mission has unread content'
+            => '??',
+		'Available'
+            => '??',
+		'Awaiting Acceptance'
+            => '??',
+		'Complete'
+            => '??',
+		'Create Mission'
+            => '??',
+		'Create Missions'
+            => '??',
+		'Delete Missions'
+            => '??',
+		'Due'
+            => '??',
+		'Mission'
+            => '??',
+		'Mission Latest'
+            => '??', // Same Word Translated
+		'Missions'
+            => '??',
+		'My Missions'
+            => '??',
+		'Reject'
+            => '??',
+		'Rejected'
+            => '??',
+		'Reward'
+            => '??',
+		'Taken By'
+            => '??',
+		'This mission is already taken'
+            => '??',
+		'{n} Mission created|{n} Missions created'
+            => '??',
+    );
+?>
