8d59b0d5e91defb706e9db62bcf433f694db96b0
ivica <ivica.nedeljkovic@gmail.com>
2012-10-24 16:21:34 +0200

    Fixing issue with quatations in javascript modal views

---
+++ app/protected/modules/gamification/messages/es/Default.php
@@ -0,0 +1,87 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+		'Account Management'
+             => '??',
+		'Achievements'
+             => '??',
+		'Communication'
+             => '??',
+		'Congratulations!'
+             => '??',
+		'Continue'
+             => '??',
+		'Game on!'
+             => 'Game on!', // Same Word Translated
+		'I reached level {nextLevel}'
+             => '??',
+		'I received a new badge: {badgeContent}'
+             => '??',
+		'Leaderboard'
+             => '??',
+		'Leaderboard Ranking'
+             => '??',
+		'Level'
+             => '??',
+		'Monthly'
+             => '??',
+		'New Badge'
+             => '??',
+		'New Business'
+             => '??',
+		'No achievements earned.'
+             => '??',
+		'Overall'
+             => '??',
+		'Overall Statistics'
+             => '??',
+		'Points'
+             => '??',
+		'Rank'
+             => '??',
+		'Sales'
+             => '??',
+		'Skip'
+             => '??',
+		'Time Management'
+             => '??',
+		'Weekly'
+             => '??',
+		'You have reached level {nextLevel}'
+             => '??',
+		'{n} Zurmo early morning login|{n} Zurmo early morning logins'
+             => '??',
+		'{n} Zurmo login|{n} Zurmo logins'
+             => '??',
+		'{n} Zurmo nighttime login|{n} Zurmo nighttime logins'
+             => '??',
+    );
+?>
