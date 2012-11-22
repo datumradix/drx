a5a85cca886699d56d6e86d8505ae0c807608a19
user <user@user-PC>
2012-11-22 21:52:16 +0530

    Commit for Imports Functioanl Fixes

---
+++ app/protected/modules/gamification/messages/de/Default.php
@@ -0,0 +1,87 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'Account Management'
+             => 'Kontoverwaltung',
+        'Achievements'
+             => 'Erfolge',
+        'Communication'
+             => 'Kommunikation',
+        'Congratulations!'
+             => 'Herzlichen Glückwunsch!',
+        'Continue'
+             => 'Fortfahren',
+        'Game on!'
+             => 'Game on!', // Same Word Translated
+        'I reached level {nextLevel}'
+             => 'Erreichte ich Ebene {NextLevel}',
+        'I received a new badge: {badgeContent}'
+             => 'Ich verdiente ein neues Abzeiche {badgeContent}',
+        'Leaderboard'
+             => 'Rangliste',
+        'Leaderboard Ranking'
+             => 'Rangliste Platzierung',
+        'Level'
+             => 'Ebene',
+        'Monthly'
+             => 'Monatlich',
+        'New Badge'
+             => 'Neue Abzeichen',
+        'New Business'
+             => 'Neue Unternehmen',
+        'No achievements earned.'
+             => 'Keine Leistungen verdient.',
+        'Overall'
+             => 'Insgesamt',
+        'Overall Statistics'
+             => 'Gesamt Statistiken',
+        'Points'
+             => 'Punkte',
+        'Rank'
+             => 'Rang',
+        'Sales'
+             => 'Vertrieb',
+        'Skip'
+             => 'Überspringen',
+        'Time Management'
+             => 'Zeitmanagement',
+        'Weekly'
+             => 'Wöchentlich',
+        'You have reached level {nextLevel}'
+             => 'Du hast Ebene {NextLevel} erreicht',
+        '{n} Zurmo early morning login|{n} Zurmo early morning logins'
+             => '{n} Zurmo Morgenstund hat Gold im Mund login |{n} Zurmo Morgenstund hat Gold im Mund Logins',
+        '{n} Zurmo login|{n} Zurmo logins'
+             => '{n} Zurmo login |{n} Zurmo Logins', // Same Word Translated
+        '{n} Zurmo nighttime login|{n} Zurmo nighttime logins'
+             => '{n} Zurmo nachtzeit login |{n} Zurmo nachtzeit Logins',
+    );
+?>
