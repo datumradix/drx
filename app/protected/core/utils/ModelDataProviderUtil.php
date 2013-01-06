22231614deafdc15845bda82292c9bccb638e344
speixoto <speixoto@localhost>
2013-01-06 18:47:43 +0000

    Add legend to AmChartMaker

---
+++ app/protected/core/utils/ModelDataProviderUtil.php
@@ -0,0 +1,317 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * A helper class for assisting the data providers in building query parts for fetching data.
+     *
+     */
+    class ModelDataProviderUtil
+    {
+        /**
+         * If the $onTableAliasName is used (not null):
+         * Special use of sort attribute resolution. If you are resolving a sort attribute against a relation then
+         * the joins must utilize a left join in the case of casting up.  Does not support when the attribute is a
+         * relation itself as this expects any relation processing to be done before this is called.
+         *
+         * @param RedBeanModelAttributeToDataProviderAdapter $modelAttributeToDataProviderAdapter
+         * @param RedBeanModelJoinTablesQueryAdapter $joinTablesAdapter
+         * @param null | string $onTableAliasName
+         * @return string
+         * @throws NotSupportedException
+         */
+        public static function resolveSortAttributeColumnName(RedBeanModelAttributeToDataProviderAdapter
+                                                              $modelAttributeToDataProviderAdapter,
+                                                              RedBeanModelJoinTablesQueryAdapter
+                                                              $joinTablesAdapter,
+                                                              $onTableAliasName = null)
+        {
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            $builder        = new ModelJoinBuilder($modelAttributeToDataProviderAdapter, $joinTablesAdapter);
+            $tableAliasName = $builder->resolveJoins($onTableAliasName, self::resolveCanUseFromJoins($onTableAliasName));
+            if($modelAttributeToDataProviderAdapter->hasRelatedAttribute())
+            {
+                $resolvedSortColumnName     = $modelAttributeToDataProviderAdapter->getRelatedAttributeColumnName();
+            }
+            else
+            {
+                $resolvedSortColumnName     = $modelAttributeToDataProviderAdapter->getColumnName();
+            }
+            return self::resolveSortColumnNameString($tableAliasName, $resolvedSortColumnName);
+        }
+
+        public static function resolveCanUseFromJoins($onTableAliasName)
+        {
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            if($onTableAliasName != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        protected static function resolveSortColumnNameString($tableAliasName, $resolvedSortColumnName)
+        {
+            assert('is_string($tableAliasName)');
+            assert('is_string($resolvedSortColumnName)');
+            $sort  = DatabaseCompatibilityUtil::quoteString($tableAliasName);
+            $sort .= '.';
+            $sort .= DatabaseCompatibilityUtil::quoteString($resolvedSortColumnName);
+            return $sort;
+        }
+
+        /**
+         * If the $onTableAliasName is used (not null):
+         * Special use of group by attribute resolution. If you are resolving a group by attribute against a relation then
+         * the joins must utilize a left join in the case of casting up.  Does not support when the attribute is a
+         * relation itself as this expects any relation processing to be done before this is called.
+         *
+         * @param RedBeanModelAttributeToDataProviderAdapter $modelAttributeToDataProviderAdapter
+         * @param RedBeanModelJoinTablesQueryAdapter $joinTablesAdapter
+         * @param null | string $onTableAliasName
+         * @return string
+         * @throws NotSupportedException
+         */
+        public static function resolveGroupByAttributeColumnName(RedBeanModelAttributeToDataProviderAdapter
+                                                                 $modelAttributeToDataProviderAdapter,
+                                                                 RedBeanModelJoinTablesQueryAdapter
+                                                                 $joinTablesAdapter,
+                                                                 $onTableAliasName = null)
+        {
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            $builder        = new ModelJoinBuilder($modelAttributeToDataProviderAdapter, $joinTablesAdapter);
+            $tableAliasName = $builder->resolveJoins($onTableAliasName, self::resolveCanUseFromJoins($onTableAliasName));
+            if($modelAttributeToDataProviderAdapter->hasRelatedAttribute())
+            {
+                $resolvedGroupByColumnName = $modelAttributeToDataProviderAdapter->getRelatedAttributeColumnName();
+            }
+            else
+            {
+                $resolvedGroupByColumnName = $modelAttributeToDataProviderAdapter->getColumnName();
+            }
+            return self::resolveGroupByColumnNameString($tableAliasName, $resolvedGroupByColumnName);
+        }
+
+        protected static function resolveGroupByColumnNameString($tableAliasName, $resolvedSortColumnName)
+        {
+            assert('is_string($tableAliasName)');
+            assert('is_string($resolvedSortColumnName)');
+            $groupBy  = DatabaseCompatibilityUtil::quoteString($tableAliasName);
+            $groupBy .= '.';
+            $groupBy .= DatabaseCompatibilityUtil::quoteString($resolvedSortColumnName);
+            return $groupBy;
+        }
+
+        /**
+         * Override from RedBeanModelDataProvider to support multiple
+         * where clauses for the same attribute and operatorTypes
+         * @param $modelClassName
+         * @param array $metadata - array expected to have clauses and structure elements
+         * @param $joinTablesAdapter
+         * @return string
+         */
+        public static function makeWhere($modelClassName, array $metadata, $joinTablesAdapter, $onTableAliasName = null)
+        {
+            assert('is_string($modelClassName) && $modelClassName != ""');
+            assert('$joinTablesAdapter instanceof RedBeanModelJoinTablesQueryAdapter');
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            if (empty($metadata))
+            {
+                return;
+            }
+            $where = array();
+            foreach ($metadata['clauses'] as $key => $clauseInformation)
+            {
+                static::processMetadataClause($modelClassName, $key, $clauseInformation, $where, $joinTablesAdapter, $onTableAliasName);
+            }
+            if (count($where)> 0)
+            {
+                return strtr(strtolower($metadata["structure"]), $where);
+            }
+            return;
+        }
+
+        /**
+         * @param string $modelClassName
+         * @param integer $clausePosition
+         * @param array $clauseInformation
+         * @param array $where
+         * @param RedBeanModelJoinTablesQueryAdapter $joinTablesAdapter
+         * @throws NotSupportedException
+         */
+        protected static function processMetadataClause($modelClassName, $clausePosition, $clauseInformation, & $where,
+                                                        & $joinTablesAdapter, $onTableAliasName = null)
+        {
+            assert('is_string($modelClassName) && $modelClassName != ""');
+            assert('$joinTablesAdapter instanceof RedBeanModelJoinTablesQueryAdapter');
+            assert('is_int($clausePosition)');
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            if (isset($clauseInformation['relatedModelData']))
+            {
+                static::processMetadataContainingRelatedModelDataClause($modelClassName,
+                    $clausePosition,
+                    $clauseInformation,
+                    $where,
+                    $joinTablesAdapter,
+                    $onTableAliasName);
+            }
+            elseif (isset($clauseInformation['concatedAttributeNames']))
+            {
+                if (isset($clauseInformation['relatedAttributeName']) &&
+                   $clauseInformation['relatedAttributeName'] != null)
+                {
+                    throw new NotSupportedException();
+                }
+                $tableAliasAndColumnNames = self::makeTableAliasAndColumnNamesForNonRelatedConcatedAttributes(
+                                            $modelClassName, $clauseInformation['concatedAttributeNames'],
+                                            $joinTablesAdapter);
+                self::addWherePartByClauseInformationForConcatedAttributes($clauseInformation['operatorType'],
+                                            $clauseInformation['value'], $where, $clausePosition,
+                                            $tableAliasAndColumnNames);
+            }
+            else
+            {
+                $modelAttributeToDataProviderAdapter =  new RedBeanModelAttributeToDataProviderAdapter(
+                                                        $modelClassName,
+                                                        $clauseInformation['attributeName'],
+                                                        ArrayUtil::getArrayValue($clauseInformation, 'relatedAttributeName'));
+                $builder = new ModelWhereAndJoinBuilder($modelAttributeToDataProviderAdapter, $joinTablesAdapter, true,
+                                                        ArrayUtil::getArrayValue($clauseInformation, 'modifierType'));
+                $builder->resolveJoinsAndBuildWhere(    $clauseInformation['operatorType'],
+                                                        $clauseInformation['value'], $clausePosition,
+                                                        $where, $onTableAliasName);
+            }
+        }
+
+        /**
+         * @param string $modelClassName
+         * @param integer $clausePosition
+         * @param array $clauseInformation
+         * @param array $where
+         * @param RedBeanModelJoinTablesQueryAdapter $joinTablesAdapter
+         */
+        protected static function processMetadataContainingRelatedModelDataClause($modelClassName,
+                                                                                  $clausePosition,
+                                                                                  $clauseInformation,
+                                                                                  & $where,
+                                                                                  $joinTablesAdapter,
+                                                                                  $onTableAliasName = null)
+        {
+            assert('is_string($modelClassName) && $modelClassName != ""');
+            assert('$joinTablesAdapter instanceof RedBeanModelJoinTablesQueryAdapter');
+            assert('is_int($clausePosition)');
+            assert('is_array($clauseInformation["relatedModelData"]) && count($clauseInformation["relatedModelData"]) > 0');
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            $modelAttributeToDataProviderAdapter = new RedBeanModelAttributeToDataProviderAdapter(
+                                                   $modelClassName,
+                                                   $clauseInformation['attributeName'],
+                                                   $clauseInformation['relatedModelData']['attributeName']);
+            $builder                             = new ModelWhereAndJoinBuilder($modelAttributeToDataProviderAdapter,
+                                                   $joinTablesAdapter, true);
+                                                   $builder->resolveJoins($onTableAliasName,
+                                                   self::resolveCanUseFromJoins($onTableAliasName)); //todo: when we used the onTableAliasName returned value things broke in tests which i think makes sense...
+            $relationModelClassName              = $modelAttributeToDataProviderAdapter->getRelationModelClassName();
+            //if there is no more relatedModelData then we know this is the end of the nested information.
+            if (isset($clauseInformation['relatedModelData']['relatedModelData']))
+            {
+                return static::processMetadataClause($relationModelClassName, $clausePosition,
+                                                     $clauseInformation['relatedModelData'],
+                                                     $where, $joinTablesAdapter, $onTableAliasName);
+            }
+            //Supporting the use of relatedAttributeName. Alternatively you can use relatedModelData to produce the same results.
+            if (isset($clauseInformation['relatedModelData']['relatedAttributeName']))
+            {
+                $modelAttributeToDataProviderAdapter = new RedBeanModelAttributeToDataProviderAdapter(
+                                                           $relationModelClassName,
+                                                           $clauseInformation['relatedModelData']['attributeName'],
+                                                           $clauseInformation['relatedModelData']['relatedAttributeName']);
+                $builder = new ModelWhereAndJoinBuilder($modelAttributeToDataProviderAdapter, $joinTablesAdapter, true,
+                               ArrayUtil::getArrayValue($clauseInformation['relatedModelData'], 'modifierType'));
+            }
+            $builder->resolveJoinsAndBuildWhere(
+                        $clauseInformation['relatedModelData']['operatorType'],
+                        $clauseInformation['relatedModelData']['value'], $clausePosition, $where, $onTableAliasName);
+        }
+
+        /**
+         * @param string $modelClassName
+         * @param array $concatedAttributeNames
+         * @param RedBeanModelJoinTablesQueryAdapter $joinTablesAdapter
+         * @return array
+         */
+        protected static function makeTableAliasAndColumnNamesForNonRelatedConcatedAttributes( $modelClassName,
+                                                                                               $concatedAttributeNames,
+                                                                                               $joinTablesAdapter,
+                                                                                               $onTableAliasName = null)
+        {
+            assert('is_string($modelClassName)');
+            assert('is_array($concatedAttributeNames) && count($concatedAttributeNames) == 2');
+            assert('$joinTablesAdapter instanceof RedBeanModelJoinTablesQueryAdapter');
+            assert('is_string($onTableAliasName) || $onTableAliasName == null');
+            $tableAliasAndColumnNames = array();
+            foreach ($concatedAttributeNames as $attributeName)
+            {
+                $modelAttributeToDataProviderAdapter = new RedBeanModelAttributeToDataProviderAdapter(
+                                                       $modelClassName, $attributeName);
+                $builder                             = new ModelWhereAndJoinBuilder(
+                                                       $modelAttributeToDataProviderAdapter, $joinTablesAdapter, true);
+                $tableAliasName                      = $builder->resolveJoins($onTableAliasName,
+                                                       self::resolveCanUseFromJoins($onTableAliasName));
+                $tableAliasAndColumnNames[]          = array($tableAliasName,
+                                                       $modelAttributeToDataProviderAdapter->getColumnName());
+            }
+            return $tableAliasAndColumnNames;
+        }
+
+        /**
+         * Add a sql string to the where array base on the $operatorType, $value and $tableAliasAndColumnNames concated
+         * together.  How the sql string is built depends on if the value is a string or not.
+         * @param string $operatorType
+         * @param mixed $value
+         * @param array $where
+         * @param integer $whereKey
+         * @param array $tableAliasAndColumnNames
+         */
+        protected static function addWherePartByClauseInformationForConcatedAttributes($operatorType, $value, &$where,
+                                                                    $whereKey, $tableAliasAndColumnNames)
+        {
+            assert('is_string($operatorType)');
+            assert('is_array($where)');
+            assert('is_int($whereKey)');
+            assert('is_array($tableAliasAndColumnNames) && count($tableAliasAndColumnNames) == 2');
+            $quote = DatabaseCompatibilityUtil::getQuote();
+            if (is_string($value) || (is_array($value) && count($value) > 0) || $value !== null)
+            {
+                $first            = ModelJoinBuilder::makeColumnNameWithTableAlias(
+                                    $tableAliasAndColumnNames[0][0], $tableAliasAndColumnNames[0][1]);
+                $second           = ModelJoinBuilder::makeColumnNameWithTableAlias(
+                                    $tableAliasAndColumnNames[1][0], $tableAliasAndColumnNames[1][1]);
+                $concatedSqlPart  = DatabaseCompatibilityUtil::concat(array($first, '\' \'', $second));
+                $where[$whereKey] = "($concatedSqlPart " . // Not Coding Standard
+                                    DatabaseCompatibilityUtil::getOperatorAndValueWherePart($operatorType, $value) . ")";
+            }
+        }
+    }
+?>
---
+++ app/protected/modules/reports/dataproviders/SummationReportDataProvider.php
@@ -0,0 +1,166 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    class SummationReportDataProvider extends ReportDataProvider
+    {
+        /**
+         * Resolved to include the groupBys as query only display attributes, and mark all display attributes that are
+         * also groupBys as used by the drillDown.
+         * @var null | array of DisplayAttributesForReportForms
+         */
+        private $resolvedDisplayAttributes;
+
+        public function __construct(Report $report, array $config = array())
+        {
+            parent::__construct($report, $config);
+        }
+
+        public function calculateTotalItemCount()
+        {
+        $selectQueryAdapter     = new RedBeanModelSelectQueryAdapter();
+        $sql                    = $this->makeSqlQueryForFetchingTotalItemCount($selectQueryAdapter);
+        echo $sql . "<BR>";
+        $rows                   = R::getAll($sql);
+        $count                  = count($rows);
+        echo 'the count ' . $count . "<BR>";
+        return $count;
+        }
+
+        public function getChartData()
+        {
+
+            $resultsData              = $this->fetchChartData();
+            $firstSeriesAttributeName = $this->resolveChartFirstSeriesAttributeNameForReportResultsRowData();
+            $firstRangeAttributeName  = $this->resolveChartFirstRangeAttributeNameForReportResultsRowData();
+            $chartData = array();
+            foreach ($resultsData as $data)
+            {
+                $chartData[] = array('value'        => $data->$firstRangeAttributeName,
+                                     'displayLabel' => strval($data->$firstSeriesAttributeName));
+            }
+            return $chartData;
+        }
+
+        public function resolveDisplayAttributesToUse()
+        {
+            if($this->resolvedDisplayAttributes != null)
+            {
+                return $this->resolvedDisplayAttributes;
+            }
+            $this->resolvedDisplayAttributes = array();
+            foreach($this->report->getDisplayAttributes() as $displayAttribute)
+            {
+                $this->resolvedDisplayAttributes[] = $displayAttribute;
+            }
+            if(($this->report->getDrillDownDisplayAttributes()) > 0)
+            {
+                $this->resolveGroupBysThatAreNotYetDisplayAttributesAsDisplayAttributes();
+            }
+            return $this->resolvedDisplayAttributes;
+        }
+
+        protected function isReportValidType()
+        {
+            if($this->report->getType() != Report::TYPE_SUMMATION)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        protected function fetchChartData()
+        {
+            //todO: $totalItemCount = $this->getTotalItemCount(); if too many rows over 100? then we should block or limit or something not sure...
+            return $this->runQueryAndGetResolveResultsData(null, null);
+        }
+
+        public function resolveFirstSeriesLabel()
+        {
+            foreach($this->report->getDisplayAttributes() as $key => $displayAttribute)
+            {
+                if($displayAttribute->attributeIndexOrDerivedType == $this->report->getChart()->firstSeries)
+                {
+                    return $displayAttribute->label;
+                }
+            }
+        }
+
+        public function resolveFirstRangeLabel()
+        {
+            foreach($this->report->getDisplayAttributes() as $key => $displayAttribute)
+            {
+                if($displayAttribute->attributeIndexOrDerivedType == $this->report->getChart()->firstRange)
+                {
+                    return $displayAttribute->label;
+                }
+            }
+        }
+
+        protected function resolveChartFirstSeriesAttributeNameForReportResultsRowData()
+        {
+            foreach($this->report->getDisplayAttributes() as $key => $displayAttribute)
+            {
+                if($displayAttribute->attributeIndexOrDerivedType == $this->report->getChart()->firstSeries)
+                {
+                    return $displayAttribute->resolveAttributeNameForGridViewColumn($key);
+                }
+            }
+        }
+
+        protected function resolveChartFirstRangeAttributeNameForReportResultsRowData()
+        {
+            foreach($this->report->getDisplayAttributes() as $key => $displayAttribute)
+            {
+                if($displayAttribute->attributeIndexOrDerivedType == $this->report->getChart()->firstRange)
+                {
+                    return $displayAttribute->resolveAttributeNameForGridViewColumn($key);
+                }
+            }
+        }
+
+        private function resolveGroupBysThatAreNotYetDisplayAttributesAsDisplayAttributes()
+        {
+            foreach($this->resolveGroupBys() as $groupBy)
+            {
+                if(null === $index = $this->report->getDisplayAttributeIndex($groupBy->attributeIndexOrDerivedType))
+                {
+                    $displayAttribute                              = new DisplayAttributeForReportForm(
+                                                                     $groupBy->getModuleClassName(),
+                                                                     $groupBy->getModelClassName(),
+                                                                     $this->report->getType());
+                    $displayAttribute->attributeIndexOrDerivedType = $groupBy->attributeIndexOrDerivedType;
+                    $displayAttribute->queryOnly                   = true;
+                    $displayAttribute->valueUsedAsDrillDownFilter  = true;
+                    $this->resolvedDisplayAttributes[]             = $displayAttribute;
+                }
+                else
+                {
+                    $this->resolvedDisplayAttributes[$index]->valueUsedAsDrillDownFilter = true;
+                }
+            }
+
+        }
+    }
+?>
