73ee2a0909601f373c26874f872d443fb6cf0c3a
Amit Ashckenazi <obviocity@gmail.com>
2013-01-18 18:18:00 -0300

    cleanup. adding margin under drop area

---
+++ app/protected/modules/designer/views/AttributesCollectionView.php
@@ -0,0 +1,134 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    class AttributesCollectionView extends MetadataView
+    {
+        protected $cssClasses =  array( 'TableOfContentsView');
+
+        protected $controllerId;
+
+        protected $moduleId;
+
+        protected $attributesCollection;
+
+        protected $moduleClassName;
+
+        protected $modelClassName;
+
+        public function __construct($controllerId, $moduleId, $attributesCollection, $moduleClassName, $modelClassName, $title)
+        {
+            $this->controllerId           = $controllerId;
+            $this->moduleId               = $moduleId;
+            $this->attributesCollection   = $attributesCollection;
+            $this->moduleClassName        = $moduleClassName;
+            $this->modelClassName         = $modelClassName;
+            $this->modelId                = null;
+            $this->title                  = $title;
+        }
+
+        protected function renderContent()
+        {
+            $content  = null;
+            $content .= $this->renderBeforeTableContent();
+            if (count($this->attributesCollection) > 0)
+            {
+                $content .= '<div>';
+                $content .= $this->renderTitleContent();
+                $content .= '<ul class="configuration-list">';
+                foreach ($this->attributesCollection as $attributeName => $information)
+                {
+                    $route = $this->moduleId . '/' . $this->controllerId . '/AttributeEdit/';
+                    $attributeFormClassName = AttributesFormFactory::getFormClassNameByAttributeType($information['elementType']);
+                    if ($information['elementType'] == 'EmailAddressInformation' ||
+                        $information['elementType'] == 'Address' ||
+                        $information['elementType'] == 'User' ||
+                        $information['isReadOnly'] ||
+                        $attributeName == 'id' ||
+                        $this->isAttributeOnModelOrCastedUp($attributeName))
+                    {
+                        //temporary until we figure out how to handle these types.
+                        $linkContent = null;
+                    }
+                    else
+                    {
+                        $url         = Yii::app()->createUrl($route,
+                                            array(
+                                                'moduleClassName' => $this->moduleClassName,
+                                                'attributeTypeName' => $information['elementType'],
+                                                'attributeName' => $attributeName)
+                                            );
+                        $linkContent = static::renderConfigureLinkContent($url, 'edit-link-' . $attributeName);
+                    }
+                    $content .= '<li>';
+                    $content .= '<h4>' . $information['attributeLabel'] . '</h4>';
+                    $content .= ' - ' . $attributeFormClassName::getAttributeTypeDisplayName();
+                    $content .= $linkContent;
+                    $content .= '</li>';
+                }
+                $content .= '</ul>';
+                $content .= '</div>';
+            }
+            return $content;
+        }
+
+        /**
+         * If the attribute is not on the same model class but nested up, it should be blocked from being configured
+         * in the designer tool since it can have side effects. You can still manually override this in the code if
+         * necessary.
+         */
+        protected function isAttributeOnModelOrCastedUp($attributeName)
+        {
+            assert('is_string($attributeName)');
+            $attributeAdapter = new RedBeanModelAttributeToDataProviderAdapter($this->modelClassName, $attributeName);
+            if (!$attributeAdapter->getModel()->isAttribute($attributeName))
+            {
+                return false;
+            }
+            if ($attributeAdapter->getAttributeModelClassName() != $this->modelClassName)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public function isUniqueToAPage()
+        {
+            return false;
+        }
+
+        protected function renderBeforeTableContent()
+        {
+        }
+
+        protected static function renderConfigureLinkContent($url, $id)
+        {
+            assert('is_string($url) || $url == null');
+            assert('is_string($id)');
+            return ZurmoHtml::link(ZurmoHtml::tag('span', array('class' => 'z-label'), Yii::t('Default', 'Configure')),
+                                $url, array('id' => $id, 'class' => 'z-button'));
+        }
+    }
+?>
---
+++ app/protected/modules/users/messages/it/Default.php
@@ -0,0 +1,141 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A user field'
+            => 'Un campo utente',
+        'Access Users Tab'
+            => 'Accesso ai Utente',
+        'Change Password'
+            => 'Cambia Password',
+        'Change Profile Picture'
+            => 'Cambia la foto del profilo',
+        'Change User Passwords'
+            => 'Cambia Password di Utente',
+        'Confirm Password'
+            => 'Conferma password',
+        'Confirm your time zone'
+            => 'Conferma il tuo fuso orario',
+        'Create User'
+            => 'Nuovo Utente',
+        'Create Users'
+            => 'Crea Utente',
+        'Custom Outbound Email Settings'
+            => 'Parametri personali della posta in uscita',
+        'day(s)'
+            => 'giorno/i',
+        'Email Configuration'
+            => 'Configurazione email',
+        'Email Signature'
+            => 'Firma email',
+        'Enforce Strong Passwords'
+            => 'Attuare l\'uso di password forti',
+        'General'
+            => 'Generale',
+        'Hash'
+            => 'Hash', // Same Word Translated
+        'Hide welcome page'
+            => 'Nascondere la pagina di benvenuto',
+        'If unchecked, will use system SMTP settings.'
+            => 'Se non è spunta, le impostazioni SMTP del sistema saranno utilizzati.',
+        'Inactive users cannot log in using the web, mobile or web API. Login for active users is controlled by group rights.'
+            => 'Gli utenti inatyivi no possono login nel web, mobile, o web API.  Il login per gli utente attivi è controllato per i diritti del grupo.',
+        'Inherited Value:'
+            => 'Valore ereditato:',
+        'Manage Users'
+            => 'Gestione degli Utenti',
+        'Manager'
+            => 'Manager', // Same Word Translated
+        'Minimum Password Length'
+            => 'Lunghezza minima della password',
+        'Minimum Username Length'
+            => 'Lunghezza minima del nome utente',
+        'My Profile'
+            => 'Il mio profile',
+        'No Profile Picture'
+            => 'Non visualizzare una foto',
+        'Password Expires'
+            => 'Password scadenza',
+        'Password Expiry Days'
+            => 'Giorni de scandenza delle password',
+        'Profile'
+            => 'Profile', // Same Word Translated
+        'Quick Tip'
+            => 'Punta Rapida',
+        'Sign in Via Mobile'
+            => 'Login  mobile',
+        'Sign in Via Web'
+            => 'Login  web',
+        'Sign in Via Web API'
+            => 'Login Web API',
+        'Texture'
+            => 'Apparenza',
+        'The password is too short. Minimum length is {minimumLength}.'
+            => 'La password è troppo corta. La lunghezza minima è {minimumLength} carattere.',
+        'The password must have at least one lowercase letter'
+            => 'La password deve avere almeno uno lettera minuscola.',
+        'The password must have at least one number and one letter'
+            => 'La password deve comprendere almeno un numero e una lettera.',
+        'The password must have at least one uppercase letter'
+            => 'La password deve avere almeno uno lettera maiuscola. ',
+        'The time zone is invalid.'
+            => 'Il fuso oraria non è valido.',
+        'The username is too short. Minimum length is {minimumLength}.'
+            => 'Il nome di utente è troppo corto.  La lunghezza minima è {minimumLength} carattere.',
+        'Theme'
+            => 'Tema',
+        'Time Zone'
+            => 'Fuso orario',
+        'Turn off email notifications'
+            => 'Disattivare le notifiche e-mail',
+        'Use Gravatar with custom email'
+            => 'Utilizza Gravatar con un email personalizzato',
+        'Use Gravatar with primary email ({primaryEmail})'
+            => 'Utilizza Gravatar con l\'indirizzo e-mail primario ({primaryEmail})',
+        'User'
+            => 'Utente',
+        'User configuration saved successfully.'
+            => 'Configurazione salva con successo',
+        'User email configuration saved successfully.'
+            => 'Configurazione email salvata con successo.',
+        'User Search'
+            => 'Ricerca Utente',
+        'Users'
+            => 'Utenti',
+        'Users cannot be deleted.  You can however change their status to inactive.'
+            => 'Gli utenti non possono essere cancellati. È tuttavia possibile cambiare il proprio status di inattivo.',
+        'Users Home'
+            => 'Utenti Home',
+        'You need to choose an email address.'
+            => 'È necessario scegliere un indirizzo email.',
+        'Your Gravatar is an image that follows you from site to site appearing beside your name when you do things like comment or post on a blog.'
+            => 'Tuo Gravatar è un immagine associata al tuo indirizzo email che appare accanto al tuo nome quando scrivi un articolo, commenti un post o una foto.',
+    );
+?>
