cad0907471906ada4cfbd02f7200f9b1d654ad9f
Dhananjay Donthula <Dhananjay Donthula@localhost>
2013-02-01 13:09:42 +0530

    commit for LDAP service optional

---
+++ app/protected/modules/missions/messages/es/Default.php
@@ -0,0 +1,85 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A mission status has changed'
+            => 'Un estado de la misión ha cambiado',
+        'A mission you completed has been accepted'
+            => 'Una misión que completó ha sido aceptada',
+        'A mission you completed has been rejected'
+            => 'Una misión que completó ha sido rechazada',
+        'A mission you created has been completed'
+            => 'Una misión que ha creado se ha completado',
+        'A mission you created has been taken on by {takenByUserName}'
+            => 'Una misión que ha creado ha sido asumida por {takenByUserName}',
+        'Accept'
+            => 'Aceptar',
+        'Accepted'
+            => 'Aceptado',
+        'Access Missions Tab'
+            => 'Acceso Misiones',
+        'At least one mission has unread content'
+            => 'Por lo menos una misión tiene mensajes no leídos',
+        'Available'
+            => 'Disponible',
+        'Awaiting Acceptance'
+            => 'En espera de aceptación',
+        'Complete'
+            => 'Completar',
+        'Create Mission'
+            => 'Crear Misión',
+        'Create Missions'
+            => 'Crear Misiones',
+        'Delete Missions'
+            => 'Eliminar Misiones',
+        'Due'
+            => 'Fecha límite',
+        'Mission'
+            => 'Misión',
+        'Mission Latest'
+            => 'Mission Latest', // Same Word Translated
+        'Missions'
+            => 'Misiones',
+        'My Missions'
+            => 'Mi Misiónes',
+        'Reject'
+            => 'Rechazar',
+        'Rejected'
+            => 'Rechazado',
+        'Reward'
+            => 'Recompensa',
+        'Taken By'
+            => 'Tomado Por',
+        'This mission is already taken'
+            => 'Esta misión ya está en uso',
+        '{n} Mission created|{n} Missions created'
+            => '{n} Misión creada|{n} Misiones creadas',
+    );
+?>
