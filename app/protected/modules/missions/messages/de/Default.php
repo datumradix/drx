c025f6ad9c33f1a2a9925fa1bf9b76b2e1b70b81
Dhananjay Donthula <Dhananjay Donthula@localhost>
2013-02-08 22:35:23 +0530

    commit for 'sentDateTime' field in email messages

---
+++ app/protected/modules/missions/messages/de/Default.php
@@ -0,0 +1,85 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    // KEEP these in alphabetical order.
+    // KEEP them indented correctly.
+    // KEEP all the language files up-to-date with each other.
+    // DON'T MAKE A MESS!
+    return array(
+        'A mission status has changed'
+            => 'Eine Mission Status war geändert',
+        'A mission you completed has been accepted'
+            => 'Eine Mission, die du abgeschlosst wurde akzeptiert',
+        'A mission you completed has been rejected'
+            => 'Eine Mission, die du abgeschlosst wurde abgelehnt',
+        'A mission you created has been completed'
+            => 'Eine Mission, die du erstelltst wurde abgeschlossen',
+        'A mission you created has been taken on by {takenByUserName}'
+            => 'Eine Mission, die du erstellst wurde von {takenByUserName} entnommen',
+        'Accept'
+            => 'Akzeptieren',
+        'Accepted'
+            => 'Akzeptiert',
+        'Access Missions Tab'
+            => 'Zugang Missions Tab',
+        'At least one mission has unread content'
+            => 'Mindestens ein Mission hat ungelesenen Nachrichten',
+        'Available'
+            => 'Verfügbar',
+        'Awaiting Acceptance'
+            => 'Wartet Akzeptanz',
+        'Complete'
+            => 'Komplett',
+        'Create Mission'
+            => 'Mission erstellen',
+        'Create Missions'
+            => 'Missionen erstellen',
+        'Delete Missions'
+            => 'Missionen löschen',
+        'Due'
+            => 'Fällig',
+        'Mission'
+            => 'Mission', // Same Word Translated
+        'Mission Latest'
+            => 'Mission Latest', // Same Word Translated
+        'Missions'
+            => 'Missionen',
+        'My Missions'
+            => 'Meine Missionen',
+        'Reject'
+            => 'Ablehnen',
+        'Rejected'
+            => 'Abgelehnt',
+        'Reward'
+            => 'Belohne',
+        'Taken By'
+            => 'Aufgenommen von',
+        'This mission is already taken'
+            => 'Diese Mission ist bereits vergeben',
+        '{n} Mission created|{n} Missions created'
+            => '{n} Mission erstellt|{n} Missionen erstellt',
+    );
+?>
