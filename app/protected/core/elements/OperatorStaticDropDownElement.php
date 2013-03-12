448b4ff8a1fe7e143428fb176ff86fc2106bbbc7
speixoto <speixoto@localhost>
2013-03-12 18:02:49 +0000

    Fix the valueField in pie charts

---
+++ app/protected/core/elements/OperatorStaticDropDownElement.php
@@ -0,0 +1,77 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * Class used by reporting or workflow to show available operator types in a dropdown.
+     */
+    class OperatorStaticDropDownElement extends DataFromFormStaticDropDownFormElement
+    {
+        protected function getEditableHtmlOptions()
+        {
+            $htmlOptions = parent::getEditableHtmlOptions();
+            if(isset($htmlOptions['class']))
+            {
+                $htmlOptions['class'] .= ' operatorType';
+            }
+            else
+            {
+                $htmlOptions['class']  = 'operatorType';
+            }
+            return $htmlOptions;
+        }
+
+        protected function renderControlEditable()
+        {
+            $content = parent::renderControlEditable();
+            return $content;
+        }
+
+        protected function getDataAndLabelsModelPropertyName()
+        {
+            return 'getOperatorValuesAndLabels';
+        }
+
+        public static function getValueTypesRequiringFirstInput()
+        {
+            return array(OperatorRules::TYPE_EQUALS,
+                         OperatorRules::TYPE_DOES_NOT_EQUAL,
+                         OperatorRules::TYPE_GREATER_THAN_OR_EQUAL_TO,
+                         OperatorRules::TYPE_LESS_THAN_OR_EQUAL_TO,
+                         OperatorRules::TYPE_GREATER_THAN,
+                         OperatorRules::TYPE_LESS_THAN,
+                         OperatorRules::TYPE_ONE_OF,
+                         OperatorRules::TYPE_BETWEEN,
+                         OperatorRules::TYPE_STARTS_WITH,
+                         OperatorRules::TYPE_ENDS_WITH,
+                         OperatorRules::TYPE_CONTAINS);
+        }
+
+        public static function getValueTypesRequiringSecondInput()
+        {
+            return array(OperatorRules::TYPE_BETWEEN);
+        }
+    }
+?>
---
+++ app/protected/modules/api/tests/unit/models/ApiTestModelItem.php
@@ -0,0 +1,135 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+    * Test model for API: ApiTestModelItem
+    */
+    class ApiTestModelItem extends OwnedSecurableItem
+    {
+        public static function getByName($name)
+        {
+            return self::getByNameOrEquivalent('firstName', $name);
+        }
+
+        protected function untranslatedAttributeLabels()
+        {
+            return array_merge(parent::untranslatedAttributeLabels(),
+                array(
+                    'fullName' => 'Name',
+                )
+            );
+        }
+
+        public static function getDefaultMetadata()
+        {
+            $metadata = parent::getDefaultMetadata();
+            $metadata[__CLASS__] = array(
+                'members' => array(
+                    'firstName',
+                    'lastName',
+                    'boolean',
+                    'date',
+                    'dateTime',
+                    'float',
+                    'integer',
+                    'phone',
+                    'string',
+                    'textArea',
+                    'url',
+                ),
+                'relations' => array(
+                    'apiTestModelItem'  => array(RedBeanModel::HAS_MANY_BELONGS_TO,  'ApiTestModelItem'),
+                    'modelItems'        => array(RedBeanModel::HAS_MANY,             'ApiTestModelItem'),
+                    'currencyValue'     => array(RedBeanModel::HAS_ONE,   'CurrencyValue',    RedBeanModel::OWNED),
+                    'dropDown'          => array(RedBeanModel::HAS_ONE,   'OwnedCustomField', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'dropDown'),
+                    'radioDropDown'     => array(RedBeanModel::HAS_ONE,   'OwnedCustomField', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'radioDropDown'),
+                    'multiDropDown'     => array(RedBeanModel::HAS_ONE,   'OwnedMultipleValuesCustomField', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'multiDropDown'),
+                    'tagCloud'          => array(RedBeanModel::HAS_ONE,   'OwnedMultipleValuesCustomField', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'tagCloud'),
+                    'modelItem2'        => array(RedBeanModel::HAS_ONE,   'ApiTestModelItem2', RedBeanModel::NOT_OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'modelItem2'),
+                    'modelItems3'       => array(RedBeanModel::MANY_MANY, 'ApiTestModelItem3'),
+                    'modelItems4'       => array(RedBeanModel::HAS_MANY,  'ApiTestModelItem4'),
+                    'primaryEmail'      => array(RedBeanModel::HAS_ONE,   'Email', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'primaryEmail'),
+                    'primaryAddress'    => array(RedBeanModel::HAS_ONE,   'Address', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'primaryAddress'),
+                    'secondaryEmail'    => array(RedBeanModel::HAS_ONE,   'Email', RedBeanModel::OWNED,
+                                                RedBeanModel::LINK_TYPE_SPECIFIC, 'secondaryEmail'),
+                ),
+                'rules' => array(
+                    array('firstName', 'type',   'type' => 'string'),
+                    array('firstName', 'length', 'min'  => 1, 'max' => 32),
+                    array('lastName',  'required'),
+                    array('lastName',  'type',   'type' => 'string'),
+                    array('lastName',  'length', 'min'  => 2, 'max' => 32),
+                    array('boolean',   'boolean'),
+                    array('date',      'type', 'type' => 'date'),
+                    array('dateTime',  'type', 'type' => 'datetime'),
+                    array('float',     'type',    'type' => 'float'),
+                    array('integer',   'type',    'type' => 'integer'),
+                    array('phone',     'type',    'type' => 'string'),
+                    array('phone',     'length',  'min'  => 1, 'max' => 14),
+                    array('string',    'required'),
+                    array('string',    'type',  'type' => 'string'),
+                    array('string',    'length',  'min'  => 3, 'max' => 64),
+                    array('textArea',  'type',    'type' => 'string'),
+                    array('url',       'url'),
+                ),
+                'elements' => array(
+                    'apiTestModelItem' => 'ApiModelTestItem',
+                    'currencyValue'    => 'CurrencyValue',
+                    'date'             => 'Date',
+                    'dateTime'         => 'DateTime',
+                    'modelItem2'       => 'ApiModelTestItem2',
+                    'phone'            => 'Phone',
+                    'primaryEmail'     => 'EmailAddressInformation',
+                    'secondaryEmail'   => 'EmailAddressInformation',
+                    'primaryAddress'   => 'Address',
+                    'textArea'         => 'TextArea',
+                    'radioDropDown'    => 'RadioDropDown',
+                    'multiDropDown'    => 'MultiSelectDropDown',
+                    'tagCloud'         => 'TagCloud',
+                ),
+                'customFields' => array(
+                    'dropDown'        => 'ApiTestDropDown',
+                    'radioDropDown'   => 'ApiTestRadioDropDown',
+                    'multiDropDown'   => 'ApiTestMultiDropDown',
+                    'tagCloud'        => 'ApiTestTagCloud',
+                ),
+            );
+            return $metadata;
+        }
+
+        public static function isTypeDeletable()
+        {
+            return true;
+        }
+    }
+?>
