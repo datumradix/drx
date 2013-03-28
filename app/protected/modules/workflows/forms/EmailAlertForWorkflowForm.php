cbc91df7ba52a231128d2ef7367fab36df09b71f
digimagic <digimagic@localhost>
2013-03-28 11:52:50 -0400

    Workflow fixes.  Adding DetailsView

---
+++ app/protected/modules/workflows/forms/EmailAlertForWorkflowForm.php
@@ -0,0 +1,284 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * Class that defines the email alerts used for a workflow
+     */
+    class EmailAlertForWorkflowForm extends ConfigurableMetadataModel implements RowKeyInterface
+    {
+        const SEND_FROM_TYPE_DEFAULT      = 'Default';
+
+        const SEND_FROM_TYPE_CUSTOM       = 'Custom';
+
+        /**
+         * Similar to the types defined in ComponentForWorkflowForm like TYPE_EMAIL_ALERTS.
+         */
+        const TYPE_EMAIL_ALERT_RECIPIENTS = 'EmailAlertRecipients';
+
+        /**
+         * Utilized by arrays to define the element that is for the actionAttributes
+         */
+        const EMAIL_ALERT_RECIPIENTS     = 'EmailAlertRecipients';
+
+        public $emailTemplateId;
+
+        public $sendAfterDurationSeconds;
+
+        public $sendFromType;
+
+        public $sendFromName;
+
+        public $sendFromAddress;
+
+        /**
+         * @var string
+         */
+        private $_workflowType;
+
+        /**
+         * @var array of WorkflowActionAttributeForms indexed by attributeNames
+         */
+        private $_emailAlertRecipients = array();
+
+        /**
+         * Posted data can be using different keys. if you add 3 then remove 2 and add a fourth, the key would be 4.
+         * but if this is the only alert recipient, then this will save as key 0. this helps to resolve that.
+         * @var array
+         */
+        private $_emailAlertRecipientsRealToTemporaryKeyData = array();
+
+        /**
+         * @var string string references the modelClassName of the workflow itself
+         */
+        private $_modelClassName;
+
+        /**
+         * @var int
+         */
+        private $_rowKey;
+
+        /**
+         * @param $attributeName string
+         * @return string
+         */
+        protected static function resolveErrorAttributePrefix($attributeName)
+        {
+            assert('is_int($attributeName)');
+            return self::EMAIL_ALERT_RECIPIENTS . '_' .  $attributeName . '_';
+        }
+
+        public function getRowKey()
+        {
+            return $this->_rowKey;
+        }
+        /**
+         * @param string $modelClassName
+         * @param string $workflowType
+         * @param int $rowNumber
+         */
+        public function __construct($modelClassName, $workflowType, $rowKey = 0)
+        {
+            assert('is_string($modelClassName)');
+            assert('is_string($workflowType)');
+            assert('is_int($rowKey)');
+            $this->_modelClassName = $modelClassName;
+            $this->_workflowType   = $workflowType;
+            $this->_rowKey         = $rowKey;
+        }
+
+        /**
+         * @return int
+         */
+        public function getEmailAlertRecipientFormsCount()
+        {
+            return count($this->_emailAlertRecipients);
+        }
+
+        /**
+         * @return array
+         */
+        public function getEmailAlertRecipients()
+        {
+            return $this->_emailAlertRecipients;
+        }
+
+        public function getWorkflowType()
+        {
+            return $this->_workflowType;
+        }
+
+        /**
+         * @return array
+         */
+        public function rules()
+        {
+            return array_merge(parent::rules(), array(
+                array('emailTemplateId',          'required'),
+                array('sendAfterDurationSeconds', 'type', 'type' => 'integer'),
+                array('sendFromType',             'type',  'type' => 'string'),
+                array('sendFromType',             'validateSendFromType'),
+                array('sendFromName',             'type',  'type' => 'string'),
+                array('sendFromAddress',          'type',  'type' => 'string'),
+            ));
+        }
+
+        /**
+         * @return array
+         */
+        public function attributeLabels()
+        {
+            return array('emailTemplateId'          => Zurmo::t('WorkflowsModule', 'Email Template'),
+                         'sendAfterDurationSeconds' => Zurmo::t('WorkflowsModule', 'Send'),
+                         'sendFromType'             => Zurmo::t('WorkflowsModule', 'Send From'),
+                         'sendFromName'             => Zurmo::t('WorkflowsModule', 'From Name'),
+                         'sendFromAddress'          => Zurmo::t('WorkflowsModule', 'From Address'),
+            );
+        }
+
+        /**
+         * Process all attributes except 'emailAlertRecipients' first
+         * @param $values
+         * @param bool $safeOnly
+         * @throws NotSupportedException if the post values data is malformed
+         */
+        public function setAttributes($values, $safeOnly = true)
+        {
+            $recipients = null;
+            if(isset($values[self::EMAIL_ALERT_RECIPIENTS]))
+            {
+                $recipients = $values[self::EMAIL_ALERT_RECIPIENTS];
+                unset($values[self::EMAIL_ALERT_RECIPIENTS]);
+                $this->_emailAlertRecipients = array();
+            }
+            parent::setAttributes($values, $safeOnly);
+            if($recipients != null)
+            {
+                $count = 0;
+                foreach($recipients as $temporaryKey => $recipientData)
+                {
+                    if(!isset($recipientData['type']))
+                    {
+                        throw new NotSupportedException();
+                    }
+                    $form = WorkflowEmailAlertRecipientFormFactory::make($recipientData['type'], $this->_modelClassName,
+                            $this->_workflowType);
+                    $form->setAttributes($recipientData);
+                    $this->_emailAlertRecipients[] = $form;
+                    $this->_emailAlertRecipientsRealToTemporaryKeyData[] = $temporaryKey;
+                    $count ++;
+                }
+            }
+        }
+
+        /**
+         * @return bool
+         */
+        public function validateSendFromType()
+        {
+            if($this->sendFromType == self::SEND_FROM_TYPE_CUSTOM)
+            {
+                $validated = true;
+                if($this->sendFromName == null)
+                {
+                    $this->addError('sendFromName', Zurmo::t('WorkflowsModule', 'From Name cannot be blank.'));
+                    $validated = false;
+                }
+                if($this->sendFromAddress == null)
+                {
+                    $this->addError('sendFromName', Zurmo::t('WorkflowsModule', 'From Email Address cannot be blank.'));
+                    $validated = false;
+                }
+                return $validated;
+            }
+            elseif($this->sendFromType != self::SEND_FROM_TYPE_DEFAULT)
+            {
+                $this->addError('type', Zurmo::t('WorkflowsModule', 'Invalid Send From Type'));
+            }
+            return false;
+        }
+
+        /**
+         * @return bool
+         */
+        public function beforeValidate()
+        {
+            if(!$this->validateRecipients())
+            {
+                return false;
+            }
+            return parent::beforeValidate();
+        }
+
+        /**
+         * @return bool
+         */
+        public function validateRecipients()
+        {
+            $passedValidation = true;
+            if(count($this->_emailAlertRecipients) == 0)
+            {
+                $this->addError('recipientsValidation',
+                                Zurmo::t('WorkflowsModule', 'At least one recipient must be added'));
+                return false;
+            }
+            foreach($this->_emailAlertRecipients as $key => $workflowEmailAlertRecipientForm)
+            {
+                if(!$workflowEmailAlertRecipientForm->validate())
+                {
+                    foreach($workflowEmailAlertRecipientForm->getErrors() as $attribute => $errorArray)
+                    {
+                        assert('is_array($errorArray)');
+                        $attributePrefix = static::resolveErrorAttributePrefix($this->resolveTemporaryKeyByRealKey($key));
+                        $this->addError( $attributePrefix . $attribute, $errorArray[0]);
+                    }
+                    $passedValidation = false;
+                }
+            }
+            return $passedValidation;
+        }
+
+        public function getSendFromTypeValuesAndLabels()
+        {
+            $data                               = array();
+            $data[self::SEND_FROM_TYPE_DEFAULT] = Zurmo::t('WorkflowsModule', 'Default System From Name/Address'); //todo: relabel since we don't define this persay anywhere
+            $data[self::SEND_FROM_TYPE_CUSTOM]  = Zurmo::t('WorkflowsModule', 'Custom From Name/Address');
+            return $data;
+        }
+
+        public function getSendAfterDurationValuesAndLabels()
+        {
+            $data = array();
+            WorkflowUtil::resolveSendAfterDurationData($data);
+            return $data;
+        }
+
+        protected function resolveTemporaryKeyByRealKey($key)
+        {
+            assert(is_int($key));
+            return $this->_emailAlertRecipientsRealToTemporaryKeyData[$key];
+        }
+    }
+?>
---
+++ app/protected/modules/workflows/tests/unit/SavedWorkflowsUtilTest.php
@@ -0,0 +1,231 @@
+<?php
+/*********************************************************************************
+ * Zurmo is a customer relationship management program developed by
+ * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+ *
+ * Zurmo is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+ * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+ ********************************************************************************/
+
+    class SavedWorkflowsUtilTest extends ZurmoBaseTest
+    {
+        public static function setUpBeforeClass()
+        {
+            parent::setUpBeforeClass();
+            SecurityTestHelper::createSuperAdmin();
+        }
+
+        public function setup()
+        {
+            parent::setUp();
+            Yii::app()->user->userModel = User::getByUsername('super');
+        }
+
+        public function testResolveProcessDateTimeByWorkflowAndModel()
+        {
+            //Test Date
+            $model    = new WorkflowModelTestItem();
+            $model->date = '2007-02-02';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('date', 'Is Time For', null, 86400);
+            $processDateTime = SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+            $this->assertEquals('2007-02-03 00:00:00', $processDateTime);
+
+            //Test Date with negative duration
+            $model    = new WorkflowModelTestItem();
+            $model->date = '2007-02-02';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('date', 'Is Time For', null, -86400);
+            $processDateTime = SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+            $this->assertEquals('2007-02-01 00:00:00', $processDateTime);
+
+            //Test DateTime
+            $model           = new WorkflowModelTestItem();
+            $model->dateTime = '2007-05-02 04:00:02';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('dateTime', 'Is Time For', null, 86400);
+            $processDateTime = SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+            $this->assertEquals('2007-05-03 04:00:02', $processDateTime);
+
+            //Test DateTime with negative duration
+            $model           = new WorkflowModelTestItem();
+            $model->dateTime = '2007-05-02 04:00:02';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('dateTime', 'Is Time For', null, -86400);
+            $processDateTime = SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+            $this->assertEquals('2007-05-01 04:00:02', $processDateTime);
+        }
+
+        /**
+         * @depends testResolveProcessDateTimeByWorkflowAndModel
+         * @expectedException ValueForProcessDateTimeIsNullException
+         */
+        public function testResolveProcessDateTimeByWorkflowAndModelWithNullDate()
+        {
+            $model    = new WorkflowModelTestItem();
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('date', 'Is Time For', null, 86400);
+            SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+        }
+
+        /**
+         * @depends testResolveProcessDateTimeByWorkflowAndModelWithNullDate
+         * @expectedException ValueForProcessDateTimeIsNullException
+         */
+        public function testResolveProcessDateTimeByWorkflowAndModelWithPseudoNullDate()
+        {
+            $model           = new WorkflowModelTestItem();
+            $model->dateTime = '0000-00-00';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('date', 'Is Time For', null, 86400);
+            SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+        }
+
+        /**
+         * @depends testResolveProcessDateTimeByWorkflowAndModelWithPseudoNullDate
+         * @expectedException ValueForProcessDateTimeIsNullException
+         */
+        public function testResolveProcessDateTimeByWorkflowAndModelWithNullDateTime()
+        {
+            $model    = new WorkflowModelTestItem();
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('dateTime', 'Is Time For', null, 86400);
+            SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+        }
+
+        /**
+         * @depends testResolveProcessDateTimeByWorkflowAndModelWithNullDateTime
+         * @expectedException ValueForProcessDateTimeIsNullException
+         */
+        public function testResolveProcessDateTimeByWorkflowAndModelWithPseudoNullDateTime()
+        {
+            $model    = new WorkflowModelTestItem();
+            $model->dateTime = '0000-00-00 00:00:00';
+            $workflow = WorkflowTriggersUtilBaseTest::
+                        makeOnSaveWorkflowAndTimeTriggerForDateOrDateTime('dateTime', 'Is Time For', null, 86400);
+            SavedWorkflowsUtil::resolveProcessDateTimeByWorkflowAndModel($workflow, $model);
+        }
+
+        /**
+         * @depends testResolveProcessDateTimeByWorkflowAndModelWithPseudoNullDateTime
+         */
+        public function testResolveOrder()
+        {
+            $this->assertCount(0, SavedWorkflow::getAll());
+            $savedWorkflow = new SavedWorkflow();
+            $savedWorkflow->name            = 'the name';
+            $savedWorkflow->moduleClassName = 'AccountsModule';
+            $savedWorkflow->serializedData  = serialize(array('some data'));
+            $savedWorkflow->triggerOn       = Workflow::TRIGGER_ON_NEW;
+            $savedWorkflow->type            = Workflow::TYPE_ON_SAVE;
+            $this->assertNull($savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(1, $savedWorkflow->order);
+            $saved = $savedWorkflow->save();
+            $this->assertTrue($saved);
+            $savedWorkflowId1 = $savedWorkflow->id;
+
+            $savedWorkflow = new SavedWorkflow();
+            $savedWorkflow->name            = 'the name 2';
+            $savedWorkflow->moduleClassName = 'AccountsModule';
+            $savedWorkflow->serializedData  = serialize(array('some data 2'));
+            $savedWorkflow->triggerOn       = Workflow::TRIGGER_ON_NEW;
+            $savedWorkflow->type            = Workflow::TYPE_ON_SAVE;
+            $this->assertNull($savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(2, $savedWorkflow->order);
+            $saved = $savedWorkflow->save();
+            $this->assertTrue($saved);
+            $savedWorkflowId2 = $savedWorkflow->id;
+
+            $savedWorkflow = new SavedWorkflow();
+            $savedWorkflow->name            = 'the name 3';
+            $savedWorkflow->moduleClassName = 'AccountsModule';
+            $savedWorkflow->serializedData  = serialize(array('some data 2'));
+            $savedWorkflow->triggerOn       = Workflow::TRIGGER_ON_NEW;
+            $savedWorkflow->type            = Workflow::TYPE_ON_SAVE;
+            $this->assertNull($savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(3, $savedWorkflow->order);
+            $saved = $savedWorkflow->save();
+            $this->assertTrue($saved);
+            $savedWorkflowId3 = $savedWorkflow->id;
+
+            $savedWorkflow = new SavedWorkflow();
+            $savedWorkflow->name            = 'the name 4';
+            $savedWorkflow->moduleClassName = 'ContactsModule';
+            $savedWorkflow->serializedData  = serialize(array('some data'));
+            $savedWorkflow->triggerOn       = Workflow::TRIGGER_ON_NEW;
+            $savedWorkflow->type            = Workflow::TYPE_ON_SAVE;
+            $this->assertNull($savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(1, $savedWorkflow->order);
+            $saved = $savedWorkflow->save();
+            $this->assertTrue($saved);
+            $savedWorkflowId4 = $savedWorkflow->id;
+
+            $savedWorkflow = SavedWorkflow::getById($savedWorkflowId2);
+            $this->assertEquals(2, $savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(2, $savedWorkflow->order);
+
+            //Change the moduleClassName to opportunities, it should show 1
+            $savedWorkflow->moduleClassName = 'OpportunitiesModule';
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(1, $savedWorkflow->order);
+
+            //Delete the workflow. When creating a new AccountsWorkflow, it should show order 4 since the max
+            //is still 3.
+            $deleted = $savedWorkflow->delete();
+            $this->assertTrue($deleted);
+
+            $savedWorkflow = new SavedWorkflow();
+            $savedWorkflow->name            = 'the name 5';
+            $savedWorkflow->moduleClassName = 'AccountsModule';
+            $savedWorkflow->serializedData  = serialize(array('some data 2'));
+            $savedWorkflow->triggerOn       = Workflow::TRIGGER_ON_NEW;
+            $savedWorkflow->type            = Workflow::TYPE_ON_SAVE;
+            $this->assertNull($savedWorkflow->order);
+            SavedWorkflowsUtil::resolveOrder($savedWorkflow);
+            $this->assertEquals(4, $savedWorkflow->order);
+            $saved = $savedWorkflow->save();
+            $this->assertTrue($saved);
+        }
+
+        /**
+         * @depends testResolveOrder
+         */
+        public function testResolveBeforeSaveByModel()
+        {
+            $this->fail();
+            //resolveBeforeSaveByRedBeanModel($model)
+            //todo: test isActive or not when processing.
+        }
+
+        /**
+         * @depends testResolveBeforeSaveByModel
+         */
+        public function testResolveAfterSaveByModel()
+        {
+            $this->fail();
+            //resolveAfterSaveByRedBeanModel($model)
+        }
+    }
+?>
