4ea032e4a1659976534081d3b81a4eee4952a93c
nn2 <nn2@localhost>
2013-04-05 07:04:34 +0200

    Global Timezone solution ...

---
+++ app/protected/core/components/ThemeManager.php
@@ -0,0 +1,124 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
+     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     ********************************************************************************/
+
+    /**
+     * Extension of CThemeManager to help manage the theme colors and background textures
+     */
+    class ThemeManager extends CThemeManager
+    {
+        const DEFAULT_THEME_COLOR = 'blue';
+
+        public function resolveAndGetThemeColorValue(User $user)
+        {
+            assert('$user instanceOf User && $user->id > 0');
+            if ( null != $themeColor = ZurmoConfigurationUtil::getByUserAndModuleName($user, 'ZurmoModule', 'themeColor'))
+            {
+                return $themeColor;
+            }
+            else
+            {
+                return $this->getDefaultThemeColor();
+            }
+        }
+
+        public function resolveAndGetBackgroundTextureValue(User $user)
+        {
+            assert('$user instanceOf User && $user->id > 0');
+            if ( null != $themeColor = ZurmoConfigurationUtil::getByUserAndModuleName($user, 'ZurmoModule', 'backgroundTexture'))
+            {
+                return $themeColor;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public function getActiveThemeColor()
+        {
+            if (Yii::app()->user->userModel == null)
+            {
+                return $this->getDefaultThemeColor();
+            }
+            else
+            {
+                return $this->resolveAndGetThemeColorValue(Yii::app()->user->userModel);
+            }
+        }
+
+        public function getActiveBackgroundTexture()
+        {
+            if (Yii::app()->user->userModel == null)
+            {
+                return null;
+            }
+            else
+            {
+                return $this->resolveAndGetBackgroundTextureValue(Yii::app()->user->userModel);
+            }
+        }
+
+        public function setThemeColorValue(User $user, $value)
+        {
+            assert('is_string($value)');
+            ZurmoConfigurationUtil::setByUserAndModuleName($user, 'ZurmoModule', 'themeColor', $value);
+        }
+
+        public function setBackgroundTextureValue(User $user, $value)
+        {
+            assert('is_string($value) || $value == null');
+            ZurmoConfigurationUtil::setByUserAndModuleName($user, 'ZurmoModule', 'backgroundTexture', $value);
+        }
+
+        public function getDefaultThemeColor()
+        {
+            return self::DEFAULT_THEME_COLOR;
+        }
+
+        public function getThemeColorNamesAndLabels()
+        {
+            $data = array('blue'       => Zurmo::t('Core', 'Blue'),
+                          'brown'      => Zurmo::t('Core', 'Brown'),
+                          'cherry'     => Zurmo::t('Core', 'Cherry'),
+                          'honey'      => Zurmo::t('Core', 'Honey'),
+                          'lime'       => Zurmo::t('Core', 'Lime'),
+                          'turquoise'  => Zurmo::t('Core', 'Turquoise'),
+                          'violet'     => Zurmo::t('Core', 'Violet'));
+            return $data;
+        }
+
+        public function getBackgroundTextureNamesAndLabels()
+        {
+            $data = array('exclusive-paper'       => Zurmo::t('Core', 'Exclusive Paper'),
+                          'french-stucco'         => Zurmo::t('Core', 'French Stucco'),
+                          'light-noise-diagonal'  => Zurmo::t('Core', 'Light Noise'),
+                          'light-toast'           => Zurmo::t('Core', 'Light Toast'),
+                          'diagonal-noise'        => Zurmo::t('Core', 'Noise'),
+                          'paper'                 => Zurmo::t('Core', 'Paper'));
+            return $data;
+        }
+    }
+?>
