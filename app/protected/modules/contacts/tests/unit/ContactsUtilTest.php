a8dcf5c25759d1d069c6b0add224853b9f3ac599
digimagic <digimagic@localhost>
2013-04-10 12:58:16 -0400

    Adding game badge/rules for reporting and workflow

---
+++ app/protected/modules/contacts/tests/unit/ContactsUtilTest.php
@@ -0,0 +1,136 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2013 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 27 North Wacker Drive
+     * Suite 370 Chicago, IL 60606. or at email address contact@zurmo.com.
+     *
+     * The interactive user interfaces in original and modified versions
+     * of this program must display Appropriate Legal Notices, as required under
+     * Section 5 of the GNU General Public License version 3.
+     *
+     * In accordance with Section 7(b) of the GNU General Public License version 3,
+     * these Appropriate Legal Notices must retain the display of the Zurmo
+     * logo and Zurmo copyright notice. If the display of the logo is not reasonably
+     * feasible for technical reasons, the Appropriate Legal Notices must display the words
+     * "Copyright Zurmo Inc. 2013. All rights reserved".
+     ********************************************************************************/
+
+    class ContactsUtilTest extends ZurmoBaseTest
+    {
+        public static $activateDefaultLanguages = true;
+
+        public static function setUpBeforeClass()
+        {
+            parent::setUpBeforeClass();
+            SecurityTestHelper::createSuperAdmin();
+            SecurityTestHelper::createUsers();
+        }
+
+        public function testResolveContactStateAdapterByModulesUserHasAccessTo()
+        {
+            $super = User::getByUsername('super');
+            Yii::app()->user->userModel = $super;
+            $bobby   = User::getByUsername('bobby');
+            $this->assertEquals(Right::DENY, $bobby->getEffectiveRight('ContactsModule', ContactsModule::RIGHT_ACCESS_CONTACTS));
+            $this->assertEquals(Right::DENY, $bobby->getEffectiveRight('LeadsModule', LeadsModule::RIGHT_ACCESS_LEADS));
+
+            //test Contact model where has no access to either the leads or contacts module.
+            $adapterName = ContactsUtil::resolveContactStateAdapterByModulesUserHasAccessTo('ContactsModule', 'LeadsModule', $bobby);
+            $this->assertFalse($adapterName);
+
+            //test Contact model where user has access to only the leads module
+            $bobby->setRight('LeadsModule', LeadsModule::RIGHT_ACCESS_LEADS);
+            $this->assertTrue($bobby->save());
+            $adapterName = ContactsUtil::resolveContactStateAdapterByModulesUserHasAccessTo('ContactsModule', 'LeadsModule', $bobby);
+            $this->assertEquals('LeadsStateMetadataAdapter', $adapterName);
+
+            //test Contact model where user has access to only the contacts module
+            $bobby->removeRight('LeadsModule', LeadsModule::RIGHT_ACCESS_LEADS);
+            $bobby->setRight('ContactsModule', ContactsModule::RIGHT_ACCESS_CONTACTS);
+            $this->assertTrue($bobby->save());
+            $adapterName = ContactsUtil::resolveContactStateAdapterByModulesUserHasAccessTo('ContactsModule', 'LeadsModule', $bobby);
+            $this->assertEquals('ContactsStateMetadataAdapter', $adapterName);
+
+            //test Contact model where user has access to both the contacts and leads module.
+            $bobby->setRight('LeadsModule', LeadsModule::RIGHT_ACCESS_LEADS);
+            $this->assertTrue($bobby->save());
+            $adapterName = ContactsUtil::resolveContactStateAdapterByModulesUserHasAccessTo('ContactsModule', 'LeadsModule', $bobby);
+            $this->assertNull($adapterName);
+        }
+
+        /**
+         * @depends testResolveContactStateAdapterByModulesUserHasAccessTo
+         */
+        public function testGetContactStateDataFromStartingStateOnAndKeyedById()
+        {
+            $this->assertTrue(ContactsModule::loadStartingData());
+            $this->assertEquals(6, count(ContactState::GetAll()));
+            $contactStates = ContactsUtil::GetContactStateDataFromStartingStateOnAndKeyedById();
+            $this->assertEquals(2, count($contactStates));
+        }
+
+        /**
+         * @depends testGetContactStateDataFromStartingStateOnAndKeyedById
+         */
+        public function testGetContactStateLabelsKeyedByLanguageAndOrder()
+        {
+            $data                        = ContactsUtil::getContactStateLabelsKeyedByLanguageAndOrder();
+            $compareData                 = null;
+            $this->assertEquals($compareData, $data);
+            $states                      = ContactState::getByName('Qualified');
+            $states[0]->serializedLabels = serialize(array('fr' => 'QualifiedFr', 'de' => 'QualifiedDe'));
+            $this->assertTrue($states[0]->save());
+            $data                        = ContactsUtil::getContactStateLabelsKeyedByLanguageAndOrder();
+            $compareData                 = array('fr' => array($states[0]->order => 'QualifiedFr'),
+                                                 'de' => array($states[0]->order => 'QualifiedDe'));
+            $this->assertEquals($compareData, $data);
+        }
+
+        /**
+         * @depends testGetContactStateLabelsKeyedByLanguageAndOrder
+         */
+        public function testResolveStateLabelByLanguage()
+        {
+            $states = ContactState::getByName('Qualified');
+            $this->assertEquals('Qualified',   ContactsUtil::resolveStateLabelByLanguage($states[0], 'en'));
+            $this->assertEquals('QualifiedFr', ContactsUtil::resolveStateLabelByLanguage($states[0], 'fr'));
+            $this->assertEquals('QualifiedDe', ContactsUtil::resolveStateLabelByLanguage($states[0], 'de'));
+        }
+
+        /**
+         * @depends testResolveStateLabelByLanguage
+         */
+        public function testGetContactStateDataFromStartingStateKeyedByIdAndLabelByLanguage()
+        {
+            $qualifiedStates = ContactState::getByName('Qualified');
+            $customerStates = ContactState::getByName('Customer');
+            $data = ContactsUtil::getContactStateDataFromStartingStateKeyedByIdAndLabelByLanguage('en');
+            $compareData = array($qualifiedStates[0]->id => 'Qualified',
+                                $customerStates[0]->id  => 'Customer');
+            $this->assertEquals($compareData, $data);
+            $data = ContactsUtil::getContactStateDataFromStartingStateKeyedByIdAndLabelByLanguage('fr');
+            $compareData = array($qualifiedStates[0]->id => 'QualifiedFr',
+                                $customerStates[0]->id  => 'Client');
+            $this->assertEquals($compareData, $data);
+        }
+    }
+?>
