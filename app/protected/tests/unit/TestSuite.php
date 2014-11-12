ab262165451e1f0eac71eb4706f61002dfe54109
msinghai <msinghai@localhost>
2014-11-12 09:55:03 -0500

    fix for test cases

--- app/protected/tests/unit/TestSuite.php
+++ app/protected/tests/unit/TestSuite.php
@@ -188,8 +188,7 @@
                 $success = preg_match("/;dbname=([^;]+)/", Yii::app()->db->connectionString, $matches); // Not Coding Standard
                 assert('$success == 1');
                 $databaseName = $matches[1];
-                preg_match("/;host=([^;]+)/", Yii::app()->db->connectionString, $matches);
-                assert('$success == 1');
+                preg_match("/mysql:host=([^;]+)/", Yii::app()->db->connectionString, $matches);
                 $host = $matches[1];
                 $systemOutput = system('mysqldump -u' . Yii::app()->db->username .
                                         ' -p' . Yii::app()->db->password .
