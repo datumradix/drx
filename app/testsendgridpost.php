135a9668ab3b02b15ae86d68b12b8bb27d3a0858
msinghai <msinghai@localhost>
2015-01-07 13:09:47 +0530

    Changed test post file

--- app/testsendgridpost.php
+++ app/testsendgridpost.php
@@ -11,6 +11,7 @@ echo "After curl init";
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                                             'Content-Type: application/json',
                                             'Connection: Keep-Alive'
