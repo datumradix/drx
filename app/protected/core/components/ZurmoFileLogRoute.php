d9b2d7612a33903de29bc7476a90d4a242365fe6
Muhammad Shoaib <shoaibi@dotgeek.me>
2015-01-25 16:09:49 +0500

    Added ZurmoFileLogRoute to keep the logPath of application.log dynamic

---
+++ app/protected/core/components/ZurmoFileLogRoute.php
@@ -0,0 +1,52 @@
+<?php
+    /*********************************************************************************
+     * Zurmo is a customer relationship management program developed by
+     * Zurmo, Inc. Copyright (C) 2015 Zurmo Inc.
+     *
+     * Zurmo is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU Affero General Public License version 3 as published by the
+     * Free Software Foundation with the addition of the following permission added
+     * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+     * IN WHICH THE COPYRIGHT IS OWNED BY ZURMO, ZURMO DISCLAIMS THE WARRANTY
+     * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+     *
+     * Zurmo is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+     * details.
+     *
+     * You should have received a copy of the GNU Affero General Public License along with
+     * this program; if not, see http://www.gnu.org/licenses or write to the Free
+     * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+     * 02110-1301 USA.
+     *
+     * You can contact Zurmo, Inc. with a mailing address at 27 North Wacker Drive
+     * Suite 370 Chicago, IL 60606. or at email address contact@zurmo.com.
+     *
+     * The interactive user interfaces in original and modified versions
+     * of this program must display Appropriate Legal Notices, as required under
+     * Section 5 of the GNU Affero General Public License version 3.
+     *
+     * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+     * these Appropriate Legal Notices must retain the display of the Zurmo
+     * logo and Zurmo copyright notice. If the display of the logo is not reasonably
+     * feasible for technical reasons, the Appropriate Legal Notices must display the words
+     * "Copyright Zurmo Inc. 2015. All rights reserved".
+     ********************************************************************************/
+
+    class ZurmoFileLogRoute extends CFileLogRoute
+    {
+        public function init()
+        {
+            // ok, 2 things here:
+            // First, we could have used logPath property on the component inside common.php and hardcoded the path there.
+            // Problem? Yii::app() would not be initialized there and I don't feel like hardcoding the path to
+            // private runtime there.
+            // Second, we call parent's init after setting the log path because if we do it before, CFileLogRoute
+            // would set it to runtime.
+            if($this->getLogPath()===null)
+                $this->setLogPath(Yii::app()->getPrivateRuntimePath());
+            parent::init();
+        }
+    }
+?>
