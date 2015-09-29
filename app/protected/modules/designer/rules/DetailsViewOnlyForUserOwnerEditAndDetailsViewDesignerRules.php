224c5cc4da9b86557ac25e787a91c6d4401cc3b8
Boban Peric <cybperic@gmail.com>
2015-09-29 14:30:53 +0200

    CRM-157 Make Owner optional field value in Detail layout

--- app/protected/modules/designer/rules/DetailsViewOnlyForUserOwnerEditAndDetailsViewDesignerRules.php
+++ app/protected/modules/designer/rules/DetailsViewOnlyForUserOwnerEditAndDetailsViewDesignerRules.php
@@ -42,5 +42,22 @@
             $savableMetadataRules[]     = 'DetailsViewOnlyForUserOwner';
             return $savableMetadataRules;
         }
+        
+        /**
+         * This override is here because the 'owner' field is already placed by default in edit view 
+         * on the right sidebar.
+         * @param string $attributeName
+         */
+        public function isRequiredAttributeExemptFromBeingPlacedInLayout($attributeName)
+        {
+            if ($attributeName == 'owner')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 ?>
