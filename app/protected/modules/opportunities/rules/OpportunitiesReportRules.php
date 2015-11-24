2d94160a3ee0b9a62447c6afe2c111593822ee9c
Ivica Nedeljkovic <ivica.nedeljkovic@gmail.com>
2015-11-24 13:11:22 +0100

    CRM-276 - Exclude notificationSubscribers from workflow and report elements

--- app/protected/modules/opportunities/rules/OpportunitiesReportRules.php
+++ app/protected/modules/opportunities/rules/OpportunitiesReportRules.php
@@ -44,7 +44,12 @@
          */
         public static function getDefaultMetadata()
         {
-            $metadata = array();
+            $metadata = array(
+                'Opportunity' => array(
+                    'nonReportable' =>
+                        array('accounts', 'account', 'notificationSubscribers', 'comments'),
+                )
+            );
             return array_merge(parent::getDefaultMetadata(), $metadata);
         }
     }
